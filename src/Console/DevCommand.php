878ae67c27b89f6cb67280331afaf85dbac33303
OoBook <oguzhan@olmadikprojeler.com>
2023-09-07 20:34:46 +0300

    unusual:dev command added

---
+++ src/Console/DevCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace OoBook\CRM\Base\Console;
+
+class DevCommand extends BaseCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'unusual:dev {--noInstall}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Hot reload unusual assets with custom Vue component, configuration";
+
+    /*
+     * Executes the console command.
+     *
+     * @return mixed
+     */
+    public function handle() :int
+    {
+        $this->call('unusual:build', [
+            '--hot' => true,
+            '--noInstall' => $this->option('noInstall'),
+        ]);
+
+        return 0;
+    }
+}
