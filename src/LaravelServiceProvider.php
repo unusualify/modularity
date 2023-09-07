dbaeeca6d22eb2513097f11bce21c7522f343c4e
OoBook <oguzhan@olmadikprojeler.com>
2023-09-07 20:34:25 +0300

    config publish and merging updates

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -9,10 +9,13 @@ final class LaravelServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->publishConfigs();
+
         $this->publishMigrations();
 
         $this->publishAssets();
 
+        $this->publishViews();
     }
 
     /**
@@ -22,7 +25,7 @@ final class LaravelServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->publishConfigs();
+
     }
 
     private function publishMigrations(): void
@@ -48,8 +51,29 @@ final class LaravelServiceProvider extends ServiceProvider
 
     private function publishConfigs(): void
     {
+        // $this->publishes([__DIR__ . '/config/publishes/translatable.php' => config_path('translatable.php')], 'config');
+        // $this->publishes([__DIR__ . '/config/publishes/translation.php' => config_path('translation.php')], 'config');
+        // $this->publishes([__DIR__ . '/config/publishes/publish.php' => config_path('unusual.php')], 'config');
+        // $this->publishes([__DIR__ . '/config/publishes/navigation-publish.php' => config_path('unusual-navigation.php')], 'config');
+        // $this->publishes([
+        //     base_path('vendor/torann/geoip/config/geoip.php') => config_path('geoip.php'),
+        // ], 'config');
         $this->publishes([
+            __DIR__ . '/config/publishes/translatable.php' => config_path('translatable.php'),
+            __DIR__ . '/config/publishes/translation.php' => config_path('translation.php'),
+            __DIR__ . '/config/publishes/publish.php' => config_path('unusual.php'),
+            __DIR__ . '/config/publishes/navigation-publish.php' => config_path('unusual-navigation.php'),
             base_path('vendor/torann/geoip/config/geoip.php') => config_path('geoip.php'),
         ], 'config');
+
     }
+
+    private function publishViews(): void
+    {
+        $this->publishes([
+            __DIR__ . '/resources/views/vendor/translation' => resource_path('views/vendor/translation')
+        ], 'views');
+    }
+
+
 }
