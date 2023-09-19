0a28d54da9ef69825f3776c007ef41b00b9ff4ec
OoBook <oguzhan@olmadikprojeler.com>
2023-09-19 15:00:39 +0300

    error handling updated on side of vue

--- vue/src/js/utils/errors.js
+++ vue/src/js/utils/errors.js
@@ -1,8 +1,8 @@
 export function globalError (component = null, error = { message: '', value: null }) {
-  __log(
-    error,
-    error.value
-  )
+  // __log(
+  //   error,
+  //   error.value
+  // )
   let prefix = ''
 
   if (component && typeof component === 'string') {
@@ -19,17 +19,17 @@ export function globalError (component = null, error = { message: '', value: nul
     console.error(error.value.response?.data)
   }
 
-  window.vm.config.globalProperties.$notif({
-    message: 'dene',
-    variant: 'warning'
-  })
-
   // Error 401 = session expired / not authenticated
   // Error 419 = CSRF token mismatched
   if (statusCode === 401 || statusCode === 419) {
     window.vm.config.globalProperties.$notif({
       message: 'Your session has expired, please <a href="' + document.location + '" target="_blank">login in another tab</a>. You can then continue working here.',
       variant: 'warning'
+    })
+  } else {
+    window.vm.config.globalProperties.$notif({
+      message: error.message,
+      variant: 'error'
     })
   }
 }
