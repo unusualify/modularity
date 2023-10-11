2fa6a27257bba4abeb96dd8a5d81f3d1bc010746
OoBook <oguzhan@olmadikprojeler.com>
2023-10-11 16:39:16 +0300

    UComponent & UWrapper service classed developed

---
+++ src/Services/View/UComponent.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace OoBook\CRM\Base\Services\View;
+
+use BadMethodCallException;
+use Closure;
+use Exception;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Str;
+use Illuminate\View\Component;
+use OoBook\CRM\Base\Traits\ManageNames;
+use Stringable;
+
+class UComponent extends Component
+{
+    use ManageNames;
+
+    public $tag;
+
+    public $elements;
+
+    public $attributes = [];
+
+    public $slots = [];
+
+    public $directives = [];
+
+    /********************* */
+
+    public static function make(): self
+    {
+        return new self();
+    }
+
+    public function makeComponent($tag, $attributes = [], $elements = '', $slots = [], $directives = []) {
+        return $this->setTag($tag)
+            ->setAttributes($attributes)
+            ->setElements($elements)
+            ->setSlots($slots)
+            ->setDirectives($directives);
+    }
+
+    public function setTag($tag) {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    public function setAttributes($attributes) {
+        $this->attributes = $attributes;
+
+        return $this;
+    }
+
+    public function setSlots($slots) {
+        $this->slots = $slots;
+
+        return $this;
+    }
+
+    public function setDirectives($directives) {
+        $this->directives = $directives;
+
+        return $this;
+    }
+
+    public function setElements($elements) {
+        if($elements !== '')
+            $this->elements = $elements;
+
+        return $this;
+    }
+
+    public function addChildren($element)
+    {
+        if(is_string($element)){
+            $this->elements = ___($element);
+        }else if(is_array($element)){
+            if(!is_array($this->elements))
+                $this->elements = [];
+            $this->elements[] = $element;
+        }else if(get_class($element) === get_class($this)) {
+            if(!is_array($this->elements))
+                $this->elements = [];
+            $this->elements[] = $element->render();
+        }else {
+            $this->elements = $element;
+        }
+
+        return $this;
+    }
+
+    public function __call($method, $args) {
+
+        if(preg_match('/make([V|Ue][A-Za-z]+)/', $method, $match)){
+            $tag = $this->getKebabCase($match[1]);
+
+            return $this->makeComponent($tag, ...$args);
+        }
+        if(preg_match('/addChildren([V|Ue][A-Za-z]+)/', $method, $match)){
+            $tag = $this->getKebabCase($match[1]);
+
+            return $this->addChildren($tag, ...$args);
+        }
+
+        if (!in_array($method, get_class_methods($this))) {
+            throw new BadMethodCallException(sprintf(
+                'Call to undefined method %s->%s()', static::class, $method
+            ));
+        }
+    }
+
+    public static function __callStatic($method, $args) {
+        $instance = new static;
+
+        if(preg_match('/make([V|Ue][A-Za-z]{1,20}|Template|Div)/', $method, $match)){
+            $tag = $instance->getKebabCase($match[1]);
+
+            return $instance->makeComponent($tag, ...$args);
+        }
+
+        if (!in_array($method, get_class_methods($instance))) {
+            throw new BadMethodCallException(sprintf(
+                'Call to undefined method %s::%s()', static::class, $method
+            ));
+        }
+    }
+
+    public function render()
+    {
+        return [
+            'tag' => $this->tag,
+            'attributes' => $this->attributes,
+            'slots' => $this->slots,
+            'directives' => $this->directives,
+            ...($this->elements ? ['elements' => $this->elements] : [])
+        ];
+    }
+}
---
+++ src/Services/View/UWrapper.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace OoBook\CRM\Base\Services\View;
+
+use BadMethodCallException;
+use Closure;
+use Exception;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\View\Component;
+use OoBook\CRM\Base\Traits\ManageNames;
+use Stringable;
+
+class UWrapper
+{
+    use ManageNames;
+
+    /********************* */
+
+    public static function make(): self
+    {
+        return new self();
+    }
+
+    public static function makeGridSection($elements, $attributes = []){
+        $row = UComponent::makeVRow()
+            ->setAttributes(array_merge_recursive_preserve([
+                'class' => '',
+                // 'noGutters' => true
+            ], $attributes));
+
+        foreach ($elements as $key => $element) {
+            $col = UComponent::makeVCol();
+
+            $col_attributes = ['class' => 'd-flex', 'cols' => 12, 'lg' => 6];
+
+            if(is_array($element)){
+                if(isset($element['content']) && is_array($element['content'])){
+                    $col_attributes = array_merge_recursive_preserve($col_attributes, $element['parent_attributes']);
+
+                    foreach($element['content'] as $component){
+                        $col->addChildren($component);
+                    }
+                }else{
+                    foreach($element as $component){
+                        $col->addChildren($component);
+                    }
+                }
+            }else if(get_class($element) === 'OoBook\CRM\Base\Services\View\UComponent' ){
+                $col->addChildren($element);
+            }
+
+            $col->setAttributes($col_attributes);
+
+            $row->addChildren($col);
+        }
+
+        return $row->render();
+    }
+
+    public static function makeProfileWrapper($elements, $attributes = []){
+
+    }
+
+    public static function makeFormWrapper($forms)
+    {
+        // dd($forms);
+        // dd(
+        //     [
+        //         Collection::make($forms)->map(function($form){
+        //             return UComponent::makeUeForm()
+        //                 ->setAttributes($form);
+        //         })->toArray()
+        //     ]
+        // );
+        return static::makeGridSection(
+            Collection::make($forms)->map(function($form){
+                return UComponent::makeUeForm()
+                    ->setAttributes($form);
+            })->toArray()
+        );
+    }
+
+    public function __call($method, $args) {
+
+        if(preg_match('/make([V|Ue][A-Za-z]+)/', $method, $match)){
+            $tag = $this->getKebabCase($match[1]);
+
+            return $this->makeComponent($tag, ...$args);
+        }
+        if(preg_match('/addChildren([V|Ue][A-Za-z]+)/', $method, $match)){
+            $tag = $this->getKebabCase($match[1]);
+
+            return $this->addChildren($tag, ...$args);
+        }
+
+        if (!in_array($method, get_class_methods($this))) {
+            throw new BadMethodCallException(sprintf(
+                'Call to undefined method %s->%s()', static::class, $method
+            ));
+        }
+    }
+
+    public static function __callStatic($method, $args) {
+        $instance = new static;
+
+        if(preg_match('/make([V|Ue][A-Za-z]+)/', $method, $match)){
+            $tag = $instance->getKebabCase($match[1]);
+
+            return $instance->makeComponent($tag, ...$args);
+        }
+
+        if (!in_array($method, get_class_methods($instance))) {
+            throw new BadMethodCallException(sprintf(
+                'Call to undefined method %s::%s()', static::class, $method
+            ));
+        }
+    }
+
+}
