ae690af37ea2c9f184a2101b5a6e46c7336a607d
OoBook <oguzhan@olmadikprojeler.com>
2023-11-27 14:08:36 +0300

    getting inputs and route name on ManageTraits

--- src/Traits/ManageTraits.php
+++ src/Traits/ManageTraits.php
@@ -2,6 +2,7 @@
 namespace Unusualify\Modularity\Traits;
 
 use Illuminate\Support\Collection;
+use Unusualify\Modularity\Facades\Modularity;
 
 trait ManageTraits {
 
@@ -25,4 +26,39 @@ trait ManageTraits {
             return method_exists(get_called_class(), $method);
         });
     }
+
+    public function getInputs() {
+        $moduleName = null;
+        $routeName = null;
+        if( preg_match('/[M|m]{1}odules[\/\\\]([A-Za-z]+)[\/\\\]/', get_class($this), $matches)){
+            $moduleName = $matches[1];
+        }
+        if( preg_match('/(\w+)Repository/', get_class_short_name($this), $matches)){
+            $routeName = snakeCase($matches[1]);
+        }
+
+        if( $moduleName && $routeName){
+            $module = Modularity::find($moduleName);
+            $route_config = $module->getRouteConfig($routeName);
+
+            return $route_config['inputs'];
+        }
+
+        return [];
+    }
+
+    public function getRouteName() {
+        $moduleName = null;
+        $routeName = null;
+        if( preg_match('/[M|m]{1}odules[\/\\\]([A-Za-z]+)[\/\\\]/', get_class($this), $matches)){
+            $moduleName = $matches[1];
+        }
+        if( preg_match('/(\w+)Repository/', get_class_short_name($this), $matches)){
+            $routeName = studlyName($matches[1]);
+        }
+
+        return $routeName;
+    }
+
+
 }
