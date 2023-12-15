d9cd15cb47a2b75ad2a87de6c9edc1f4b115e611
OoBook <oguzhan@olmadikprojeler.com>
2023-12-15 17:51:09 +0300

    UserController superadmin checking on deleting

--- src/UModules/SystemUser/Http/Controllers/UserController.php
+++ src/UModules/SystemUser/Http/Controllers/UserController.php
@@ -49,4 +49,28 @@ class UserController extends BaseController
             $request
         );
     }
+
+    public function destroy($id, $submoduleId = null)
+    {
+        $params = $this->request->route()->parameters();
+
+        $id = last($params);
+
+        $item = $this->repository->getById($id);
+
+        if($item->isSuperAdmin()){
+            return $this->respondWithError(___("listing.delete-superadmin-error"));
+        }
+
+        if ($this->repository->delete($id)) {
+            // $this->fireEvent();
+            activity()->performedOn($item)->log('deleted');
+
+            return $this->respondWithSuccess(___("listing.delete.success", ['modelTitle' => $this->modelTitle]));
+            // return $this->respondWithSuccess(___("$this->baseKey::lang.listing.delete.success", ['modelTitle' => $this->modelTitle]));
+        }
+
+        return $this->respondWithError(___("listing.delete.error", ['modelTitle' => $this->modelTitle]));
+        // return $this->respondWithError(unusualTrans("$this->baseKey::lang.listing.delete.error", ['modelTitle' => $this->modelTitle]));
+    }
 }
