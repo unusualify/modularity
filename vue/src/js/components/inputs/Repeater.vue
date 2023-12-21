58e1c8a4b20df1d4c30f1cf7cce00643c4eed613
OoBook <oguzhan@olmadikprojeler.com>
2023-12-21 10:47:27 +0300

    repeater max min props

--- vue/src/js/components/inputs/Repeater.vue
+++ vue/src/js/components/inputs/Repeater.vue
@@ -4,22 +4,11 @@
       {{ label }}
     </ue-title>
     <div class="repeater__block">
-      <!-- <template v-for="(model, index) in repeaterInputs" :key="`ue-repeater-form-${index}`">
-        <v-custom-form-base
-          :id="`ue-repeater-form-${index}`"
-          v-model="repeaterInputs[index]"
-          :schema="repeaterSchemas[index]"
-          :row="rowAttribute"
-        >
-        </v-custom-form-base>
-      </template> -->
-
       <draggable
         class="content__content"
         v-model="repeaterInputs"
         item-key="id"
         v-bind="dragOptions"
-
         >
         <template #item="itemSlot">
           <div>
@@ -37,52 +26,6 @@
                       <v-btn @click="deleteRepeaterBlock(itemSlot.index)" color="red" variant="text" density="compact" class="" icon="">
                         <v-icon size="x-small" icon="$delete" />
                       </v-btn>
-                    </div>
-                    <div class="content__item--navigation d-none">
-                      <!-- <button
-                        v-if="!isFirst"
-                        v-on:click.stop="$emit('up')"
-                        type="button"
-                        class="nav-button up">
-                        <svg
-                          width="18"
-                          height="18"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 18 18"
-                          role="img"
-                          aria-hidden="true"
-                          focusable="false">
-                          <polygon points="9,4.5 3.3,10.1 4.8,11.5 9,7.3 13.2,11.5 14.7,10.1 "></polygon>
-                        </svg>
-                      </button> -->
-                      <!-- <button type="button" class="nav-anchor">
-                        <svg
-                          width="18"
-                          height="18"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 18 18"
-                          role="img"
-                          aria-hidden="true"
-                          focusable="false">
-                          <path d="M13,8c0.6,0,1-0.4,1-1s-0.4-1-1-1s-1,0.4-1,1S12.4,8,13,8z M5,6C4.4,6,4,6.4,4,7s0.4,1,1,1s1-0.4,1-1S5.6,6,5,6z M5,10 c-0.6,0-1,0.4-1,1s0.4,1,1,1s1-0.4,1-1S5.6,10,5,10z M13,10c-0.6,0-1,0.4-1,1s0.4,1,1,1s1-0.4,1-1S13.6,10,13,10z M9,6 C8.4,6,8,6.4,8,7s0.4,1,1,1s1-0.4,1-1S9.6,6,9,6z M9,10c-0.6,0-1,0.4-1,1s0.4,1,1,1s1-0.4,1-1S9.6,10,9,10z"></path>
-                        </svg>
-                      </button> -->
-                      <!-- <button
-                        v-if="!isLast"
-                        v-on:click.stop="$emit('down')"
-                        type="button"
-                        class="nav-button down">
-                        <svg
-                          width="18"
-                          height="18"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 18 18"
-                          role="img"
-                          aria-hidden="true"
-                          focusable="false">
-                          <polygon points="9,13.5 14.7,7.9 13.2,6.5 9,10.7 4.8,6.5 3.3,7.9 "></polygon>
-                        </svg>
-                      </button> -->
                     </div>
                     <v-custom-form-base
                       :id="`ue-repeater-form-${itemSlot.index}`"
