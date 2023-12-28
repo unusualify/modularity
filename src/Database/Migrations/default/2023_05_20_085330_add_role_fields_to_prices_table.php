88d58afdbf5a273ebcacd0cf492ae90f53b6765a
OoBook <oguzhan@olmadikprojeler.com>
2023-12-28 16:52:53 +0300

    additional_columns to prices table

---
+++ src/Database/Migrations/default/2023_05_20_085330_add_role_fields_to_prices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(config('priceable.tables.prices'), function (Blueprint $table) {
+            $table->after('valid_till', function (Blueprint $table) {
+                $table->string('role')->nullable();
+                $table->string('locale')->nullable();
+            });
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(config('priceable.tables.prices'), function (Blueprint $table) {
+			$table->dropColumn('role');
+			$table->dropColumn('locale');
+        });
+    }
+};
