249bf12bf1a8a35189696e3b76b87b30a33012be
OoBook <oguzhan@olmadikprojeler.com>
2024-01-16 13:36:27 +0300

    prepend sometimes rule on schemaParser

--- src/Support/Decomposers/SchemaParser.php
+++ src/Support/Decomposers/SchemaParser.php
@@ -429,6 +429,7 @@ class SchemaParser extends Parser
         }
 
         if(count($rules) > 0){
+            array_unshift($rules, 'sometimes');
             $extra_options['rules'] = implode(':', $rules);
         }
         return [
