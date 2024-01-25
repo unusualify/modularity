194bd5274598fb4b5e5839b7804f5b348434580b
OoBook <oguzhan@olmadikprojeler.com>
2024-01-25 14:53:17 +0300

    RepeatersTrait redesigned at 25.01.24

--- src/Repositories/Traits/RepeatersTrait.php
+++ src/Repositories/Traits/RepeatersTrait.php
@@ -2,10 +2,11 @@
 
 namespace Unusualify\Modularity\Repositories\Traits;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 /**
  * This trait is used for repeaters that may or may not have files or images in them.
- * If there is a file or image in the repeater, it should be removed from the repeater object, to be able to get detected by the FilesTrait and/or MediasTrait.
+ * If there is a file or image in the repeater, it should be removed from the repeater object, to be able to get detected by the FilesTrait and/or ImagesTrait.
  * as of @version 1.0.0, this trait onl manages Medias.
  * @uses name::function Name
  * @author Hazarcan Doğa
@@ -15,142 +16,107 @@ use Illuminate\Support\Facades\App;
 
 trait RepeatersTrait
 {
-    // use MediasTrait;
-    private $saveToPivotTable = [
-        'image',
-        'file',
-    ];
-    private $staticFieldKeys = [
-        'id',
-        // 'position',
-    ];
-    public $pivotedRepeatersTrait;
-    /**
-     * @param \Unusualify\Modularity\Entities\Model $object
-     * iterate over inputs and check if there is a repeater,
-     * if there is a repeater check if there is a file or image in the repeater
-     * if there is a file or image in the repeater, remove it from the repeater object,
-     * add it to the parent of the repeater aka $fields with the name of [repeatername].[index].[fieldname],
-     */
-    public function prepareFieldsBeforeSaveRepeatersTrait($object,$fields) {
-        $repeaterInputs = $this->getRepeaterFields();
-        $pivotedInputNames = $this->getPivotedInputNames($repeaterInputs);
-        foreach($repeaterInputs as $repeaterInput) {
-            $repeaterName = $repeaterInput['name'] ?? $repeaterInput;
-            $repeaterTranslated = $repeaterInput['translated'] ?? false;
-            // dd($repeaterTranslated, $fields[$repeaterName]);
-            if(isset($fields[$repeaterName])) {
-                if($repeaterTranslated) {
-                    $fields = $this->prepareTranslated($fields, $repeaterName, $repeaterInputs, $pivotedInputNames);
-                } else {
-                    $fields = $this->prepareUntranslated($fields, $repeaterName, $repeaterInputs, $pivotedInputNames);
+    use ImagesTrait, FilesTrait, PricesTrait;
 
-                }
-                // foreach($fields[$repeaterName] as $index => $repeater) {
-                //     $formated_repeater = [];
-                //         foreach($repeater as $key => $value) {
-                //             // dd($key, $value);
-                //             if(in_array($key, $pivotedInputNames[$repeaterName])) {
-                //                 $this->pivotedRepeatersTrait[] = $repeaterName . '.' . $index . '.' . $key;
-                //                 $fields[$repeaterName  . '.' . $index . '.' . $key] = $value;
-                //                 // unset($fields[$repeaterName][$index][$key]);
-                //                 // clears sepearetd from the original repeater that will be formatted
-                //                 unset($repeater[$key]);
-                //             }
-                //         }
-                //     /**
-                //      * Arr only part separates the static fields from the fields that will be asssigned to ($content [castsTo: object] in the repeater model)
-                //      * array_flip() is essential for key-based comparison in array_diff_key().
-                //      * It enables filtering elements based on key presence or absence.
-                //      * This approach is useful for keeping dynamic or non-static elements in $repeater while removing those with specific keys
-                //      * (information from Bard)
-                //      */
-                //     // dd($fields[$repeaterName],$fields[$repeaterName][$index]);
-                //     unset($fields[$repeaterName][$index]);
-                //     dd($repeater, $index);
-                //     $fields[$repeaterName]["content"][$index] = $repeater;
 
-                //     // dd($repeaterTranslated, $fields, $repeater);
-                //     //    if($repeaterTranslated) {
-                //     //        foreach ($locales as $locale) {
-                //     //            $fields[$repeaterName]["content"][$locale][$index] = $repeater;
-                //     //         }
-                //     //     } else {
-                //     //         $fields[$repeaterName]["content"][$index] = $repeater;
-                //     //     }
-                // }
-                // $fields[$repeaterName] += [
-                //     'role' => $repeaterName,
-                // ];
+    public function setColumnsRepeatersTrait($columns, $inputs) {
+
+        $traitName = get_class_short_name(__TRAIT__);
+
+        $columns[$traitName] = collect($this->inputs())->reduce(function($acc, $curr){
+            if(preg_match('/json-repeater/', $curr['type'])){
+                $acc[] = $curr['name'];
             }
-        }
-        // dd($fields);
-        // dd($fields, Arr::get($originalFields,'repeatername.1.participantImage'), $this->inputs());
-        return $fields;
+            return $acc;
+        }, []);
+
+        return $columns;
     }
 
-    private function prepareTranslated(array $fields, string $name, $repeaterInputs, $pivotedInputNames) : array{
-        $repeaterInputs = $this->getRepeaterFields();
-        $pivotedInputNames = $this->getPivotedInputNames($repeaterInputs);
-
-        foreach($fields[$name] as $locale => $repeater) {
-            foreach($repeater as $rii => $repeaterItem) {
-                // dd( $repeaterItem);
-                foreach($repeaterItem as $field => $value) {
-                    // dd($key, $value);
-                    if(in_array($field, $pivotedInputNames[$name])) {
-                        $pivoted_key = $name  . '.' . $locale . '.' . $rii . '.' . $field;
-                        $this->pivotedRepeatersTrait[] = $pivoted_key;
-                        $fields[$pivoted_key] = $value;
-                        unset($repeaterItem[$field]);
-                    }
-                }
-                // unset($fields[$name][$locale][$rii]);
-                $fields[$name][$locale][$rii] = $repeaterItem;
-            }
-            // $fields[$name][$locale] += [
-            //     'role' => $name,
-            // ];
-        }
-        // dd($this->pivotedRepeatersTrait);
-        return $fields;
-    }
-    private function prepareUntranslated(array $fields, string $name, $repeaterInputs, $pivotedInputNames) : array{
-        $repeaterInputs = $this->getRepeaterFields();
-        $pivotedInputNames = $this->getPivotedInputNames($repeaterInputs);
-
-        foreach($fields[$name] as $rii => $repeaterItem) {
-            foreach($repeaterItem as $field => $value) {
-                // dd($key, $value);
-                if(in_array($field, $pivotedInputNames[$name])) {
-                    $this->pivotedRepeatersTrait[] = $name . '.' . $rii . '.' . $field;
-                    $fields[$name  . '.' . $rii . '.' . $field] = $value;
-                    unset($repeaterItem[$field]);
-                }
-            }
-            $fields[$name][$rii] = $repeaterItem;
-            // $fields[$name][$locale] += [
-            //     'role' => $name,
-            // ];
-        }
-        // dd($fields);
-        return $fields;
-    }
     /**
      * @param \Unusualify\Modularity\Entities\Model $object
      * @param array $fields
      * @return \Unusualify\Modularity\Entities\Model
      */
     public function afterSaveRepeatersTrait($object, $fields) {
+
         if ($this->shouldIgnoreFieldBeforeSave('repeaters')) {
             return $object;
         }
-        // dd( $fields);
-        $defaultLocale = unusualConfig('locale');
-        $locales = getLocales();
+        // $defaultLocale = unusualConfig('locale');
+        $defaultLocale = config('app.locale');
+        // $locales = getLocales();
+        $system_locales = getLocales();
 
-        foreach($this->getRepeaterFields() as $repeaterInput) {
+        foreach ($this->getColumns(__TRAIT__) as $name) {
+            $input = $this->inputs()[$name];
+            $isTranslated = $input['translated'] ?? false;
+
+            if(isset($fields[$name])) {
+                $unsetColumns = [];
+                Collection::make($this->traitColumns)->each(function($columns, $traitName) use(&$unsetColumns){
+
+                    $unsetColumns = Collection::make($columns)->reduce(function($unsetColumns, $column){
+                        if(preg_match('/([A-Za-z-_\.]+)\.\*\.([A-Za-z-_\.]+)/', $column, $matches)){
+                            if(!in_array($matches[2], $unsetColumns))
+                                $unsetColumns[] = $matches[2];
+                        }
+
+                        return $unsetColumns;
+                    }, $unsetColumns);
+
+
+                });
+
+                $intersect_locales = array_intersect(array_keys($fields[$name]), $system_locales);
+                $localized = false;
+                $exist_locale = null;
+
+                $existingRepeaters = isset($object->id) ? $object->repeaters()
+                    ->where('repeatable_id', $object->id)
+                    ->where('role', $name)
+                    ->get() : null;
+                // dd($object->id, $existingRepeaters, $existingRepeaters->where('repeatable_id', $object->id));
+                // $repeaterModels = isset($object->id) ? $existingRepeaters->where('repeatable_id', $object->id) : null;
+
+                if(count($intersect_locales) > 1){
+                    $localized = true;
+                    $exist_locale = $intersect_locales[0];
+                }
+
+                foreach($system_locales as $system_locale) {
+                    $content = $fields[$name];
+
+                    if($localized)
+                        $content = isset($fields[$name][$system_locale]) ? $fields[$name][$system_locale] : $fields[$name][$exist_locale];
+
+                    foreach($unsetColumns as $unsetColumn){
+                        foreach ($content as &$item) {
+                            # code...
+                            unset($item[$unsetColumn]);
+                        }
+                    }
+
+                    $data = [
+                        'role' => $name,
+                        'content' => $content,
+                        'locale' => $system_locale,
+                    ];
+
+                    $existingRepeater = $existingRepeaters ? $existingRepeaters->where('locale', $system_locale)->first() : null;
+
+                    $existingRepeater
+                        ? $existingRepeater->update($data)
+                        : $object->repeaters()->create($data);
+
+                }
+            }
+        }
+
+        return;
+        foreach($this->getRepeaterInputs() as $repeaterInput) {
             $repeaterName = $repeaterInput['name'] ?? $repeaterInput;
+
             $repeaterTranslated = $repeaterInput['translated'] ?? false;
 
             if(isset($fields[$repeaterName])) {
@@ -219,113 +185,64 @@ trait RepeatersTrait
         return $object;
     }
 
-    private function saveTranslated($repeater, $name, $existingRepeaters, $object) {
-        $defaultLocale = unusualConfig('locale');
-        $locales = getLocales();
-        $repeaterModel = isset($repeater['id']) ? $existingRepeaters->where('id', $repeater['id'])->first() : null;
-        foreach($locales as $locale) {
-            $saveFormat = [
-                'role' => $name,
-                'locale' => $locale,
-            ];
-            if(isset($repeater[$locale])) {
-                $saveFormat['content'] = $repeater[$locale];
-            } else {
-                $saveFormat['content'] = $repeater[$defaultLocale];
-            }
-            if($repeaterModel) {
-                $repeaterModel->update($saveFormat);
-            } else {
-                $repeaterModel = $object->repeaters()->create($saveFormat);
-            }
-        }
-    }
-    private function saveUnranslated($repeater, $name, $existingRepeaters, $object) {
-        $locales = getLocales();
-        $repeaterModel = isset($repeater['id']) ? $existingRepeaters->where('id', $repeater['id'])->first() : null;
-        foreach($locales as $locale) {
-            $saveFormat = [
-                'role' => $name,
-                'locale' => $locale,
-                'content' => $repeater
-            ];
-            if($repeaterModel) {
-                $repeaterModel->update($saveFormat);
-            } else {
-                $repeaterModel = $object->repeaters()->create($saveFormat);
-            }
-        }
-    }
     /**
      * Clear the relationship between the repeater and the parent(module) aka object.
+     *
      * @param \Unusualify\Modularity\Entities\Model $object
+     *
      * @return void
      */
-    public function afterDelete($object)
-    {
+    public function afterDelete($object){
 
     }
-
 
     /**
      * From objects input
      */
-    public function getRepeaterFields() {
-
+    public function getRepeaterInputs() {
         return collect($this->inputs())->reduce(function($acc, $curr){
-            return collect($this->inputs())->reduce(function($acc, $curr){
-                if(preg_match('/repeater/', $curr['type']) || preg_match('/custom-input-repeater/', $curr['type']))
-                {
-                    $acc[] = [ 'name' =>$curr['name'], 'translated' => $curr['translated'] ?? false ];
-                }
-                return $acc;
-            }, []);
+            if(isset($curr['name']) && preg_match('/json-repeater/', $curr['type'])){
+                $acc[] = $curr + ['translated' => $curr['translated'] ?? false ];
+            }
+
             return $acc;
         }, []);
     }
 
-
-    public function getPivotedInputNames($repeaterInputs) {
-        $pivotedInputNames = [];
-        // dd($repeaterNames);
-        // dd($this->inputs(),Arr::get($this->inputs(), 'repeatername.*.participantCompany'));
-        // dd($this->inputs(),Arr::hasAny($this->inputs(), 'repeatername'));
-        foreach($repeaterInputs as $repeaterData) {
-            $pivotedInputNames[$repeaterData['name']] = collect($this->inputs()[$repeaterData['name']]["schema"])->reduce(function($acc, $curr){
-                // dd($acc, $curr, $this->saveToPivotTable);
-                if(in_array($curr['type'], $this->saveToPivotTable)) {
-                    $acc[] = $curr['name'];
-                }
-                return $acc;
-            }, []);
-        }
-        // foreach($repeaterNames as $repeaterName) {
-        //     // Ensure $repeaterName is a string or an integer
-        //     $key = is_object($repeaterName) ? $repeaterName->property : (is_array($repeaterName) ? $repeaterName[0] : $repeaterName);
-        //     $pivotedInputNames[$key] = collect($this->inputs()[$key]["schema"])->reduce(function($acc, $curr){
-        //         if(in_array($curr['type'], $this->saveToPivotTable)) {
-        //             $acc[] = $curr['name'];
-        //         }
-        //         return $acc;
-        //     }, []);
-        // }
-        return $pivotedInputNames;
-    }
     public function getFormFieldsRepeatersTrait($object, $fields, $schema)
     {
-        foreach($object->repeaters->groupBy('locale') as $repeatersByLocale) {
-            foreach($repeatersByLocale as $repeater) {
-                // dd($repeater->content);
-                if($schema[$repeater->role]['translated'] ?? false) {
-                    // dd($repeater->role, $repeater->locale, $repeater->content);
-                    Arr::set($fields, $repeater->role . '.' . $repeater->locale, $repeater->content);
-                } else {
-                    Arr::set($fields, $repeater->role, $repeater->content);
+        // not possess any repeater data
+        if($object->repeaters->isEmpty()){
+            $fields += Arr::mapWithKeys($this->getRepeaterInputs(), function($input){
+                return [
+                    $input['name'] => ($input['translated'] ?? false) ? Arr::mapWithKeys(getLocales(), function($locale){
+                        return [ $locale => [] ];
+                    }): []
+                ];
+            });
+        }else{
+
+            foreach($object->repeaters->groupBy('locale') as $repeatersByLocale) {
+
+                foreach($repeatersByLocale as $repeater) {
+                    // dd($repeater->content);
+                    if($this->inputs()[$repeater->role]['translated'] ?? false) {
+                        $name = $repeater->role . '.' . $repeater->locale;
+
+                        foreach (Arr::dot($repeater->content) as $notation => $value) {
+                            Arr::set($fields, "{$name}.{$notation}", $value);
+                        }
+
+                    } else {
+                        $name = $repeater->role;
+                        foreach (Arr::dot($repeater->content) as $notation => $value) {
+                            Arr::set($fields, "{$name}.{$notation}", $value);
+                        }
+                    }
                 }
+                // $fields[$repeater->role] = $repeater->content;
             }
-            // $fields[$repeater->role] = $repeater->content;
         }
-        // dd($fields);
         return $fields;
     }
 }
