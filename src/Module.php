dabc02fc6d179c75e9d30c63d220ce857505feea
OoBook <oguzhan@olmadikprojeler.com>
2024-01-31 13:42:50 +0300

    register methods overwrite on Module

--- src/Module.php
+++ src/Module.php
@@ -48,6 +48,40 @@ class Module extends NwidartModule
         // dd($this->moduleActivator, $app);
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function getCachedServicesPath(): string
+    {
+        // This checks if we are running on a Laravel Vapor managed instance
+        // and sets the path to a writable one (services path is not on a writable storage in Vapor).
+        if (!is_null(env('VAPOR_MAINTENANCE_MODE', null))) {
+            return Str::replaceLast('config.php', $this->getSnakeName() . '_module.php', $this->app->getCachedConfigPath());
+        }
+
+        return Str::replaceLast('services.php', $this->getSnakeName() . '_module.php', $this->app->getCachedServicesPath());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerProviders(): void
+    {
+        (new ProviderRepository($this->app, new Filesystem(), $this->getCachedServicesPath()))
+            ->load($this->get('providers', []));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerAliases(): void
+    {
+        $loader = AliasLoader::getInstance();
+        foreach ($this->get('aliases', []) as $aliasName => $aliasClass) {
+            $loader->alias($aliasName, $aliasClass);
+        }
+    }
+
     public function setModuleActivator($name)
     {
         $this->moduleActivator->setModule($name);
