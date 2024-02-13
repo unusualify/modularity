e18cec59be1765d2b02ce9d875d3e32f71727d9d
ilkerciblak <ilkerciblak@gmail.com>
2024-02-13 16:42:08 +0300

    root clear

--- vue/src/js/mixins/root.js
+++ vue/src/js/mixins/root.js
@@ -15,68 +15,13 @@ export default {
     }
   },
   computed: {
-    // isHoverable () {
-    //   return (this.$store.state.config.sideBarOpt.isMini && this.isLgAndUp) || this.railMode
-    // },
-    // isMini: {
-    //   get () {
-    //     return this.miniStatus = this.$store.state.config.sideBarOpt.isMini && this.isLgAndUp
-    //   },
-    //   set (val) {
-    //     this.miniStatus = val
-    //   }
-    // },
-    // railMode: {
-    //   get(){
-    //     return this.hasRailMode = this.$vuetify.display.lgAndUp && this.$store.state.config.sideBarOpt.rail;;
-    //   },
-    //   // set(miniStatus){
-    //   //   this.hasRailMode = miniStatus && this.$store.state.config.sideBarOpt.rail;
-    //   // }
-    // },
-    // showToggleButton () {
-    //   return !this.isLgAndUp || (!this.$store.state.config.sideBarOpt.isMini)
-    // },
-    // isLgAndUp () {
-    //   return this.$vuetify.display.lgAndUp
-    // },
-    // isXlAndUp () {
-    //   return this.$vuetify.display.xlAndUp
-    // },
-    // isSmAndDown () {
-    //   return this.$vuetify.display.smAndDown
-    // },
-    // ...mapState({
-    //   isMiniSidebar: state => state.config.sideBarOpt.isMini
-    // })
-
   },
   watch: {
-    // isLgAndUp (val) {
-    //   this.isMini = this.isMiniSidebar && val
-    // }
   },
   methods: {
-    // toggleSidebar () {
-    //   this.sidebarToggle = !this.sidebarToggle
-    // },
-    // handleMethodCall (functionName, ...val) {
-    //   return this[functionName](...val)
-    // },
-    // handleVmFunctionCall (functionName, ...val) {
-
-    //   return this[functionName](...val)
-    // }
   },
   mounted () {
-    // if (this.isMiniSidebar && this.isLgAndUp) {
-    //   this.sidebarToggle = true
-    // } else if (!this.$store.state.config.sideBarOpt.isMini) {
-    //   this.sidebarToggle = true
-    // }
   },
   created () {
-
-    // openMediaLibrary()
   }
 }
