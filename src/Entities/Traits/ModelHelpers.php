b2f48ad8ed30a3fc0bd9a6a45f1412aadbce93a7
OoBook <oguzhan@olmadikprojeler.com>
2024-02-16 11:49:43 +0300

    refactoring definedRelationships

--- src/Entities/Traits/ModelHelpers.php
+++ src/Entities/Traits/ModelHelpers.php
@@ -6,7 +6,26 @@ use Illuminate\Support\Arr;
 
 trait ModelHelpers
 {
+    protected static $definedRelationships = [];
 
+    public static function bootModelHelpers()
+    {
+        $relationClassesPattern = app('model.relation.pattern');
+
+        $reflector = new \ReflectionClass(get_called_class());
+
+        $modelName = get_class_short_name(get_called_class());
+
+        static::$definedRelationships[$modelName] = collect($reflector->getMethods(\ReflectionMethod::IS_PUBLIC))
+            // ->filter(fn(\ReflectionMethod $method) =>  $method->hasReturnType() && preg_match("{$relationClassesPattern}", $method->getReturnType()) )
+            ->reduce(function($carry, \ReflectionMethod $method) use($relationClassesPattern) {
+                if($method->hasReturnType() && preg_match("{$relationClassesPattern}", ($returnType = $method->getReturnType()) )){
+                    $carry[$method->name] = get_class_short_name((string) $returnType);
+                }
+
+                return $carry;
+            });
+    }
     /**
      * Checks if this model is soft deletable.
      *
@@ -28,7 +47,44 @@ trait ModelHelpers
         return $this->getConnection()->getSchemaBuilder()->hasColumn($this->getTable(), $column);
     }
 
+
     public function definedRelations($relations = null): array
+    {
+        $modelName = get_class_short_name(get_called_class());
+
+        $definedRelationships = static::$definedRelationships[$modelName];
+
+        if($relations){
+            if(is_array($relations)){
+                return array_keys(Arr::where($definedRelationships, fn($val, $key) => in_array($val, $relations)));
+
+            }else if(is_string($relations)){
+                return array_keys(Arr::where($definedRelationships, fn($val, $key) => $val == studlyName($relations)));
+            }
+        }
+
+        return array_keys($definedRelationships);
+    }
+
+    public function hasRelation($relationName): bool
+    {
+        $modelName = get_class_short_name(get_called_class());
+
+        $definedRelationships = static::$definedRelationships[$modelName];
+
+        return array_key_exists($relationName, $definedRelationships);
+    }
+
+    public function getRelationType($relationName): string
+    {
+        $modelName = get_class_short_name(get_called_class());
+
+        $definedRelationships = static::$definedRelationships[$modelName];
+
+        return array_key_exists($relationName, $definedRelationships) ? $definedRelationships[$relationName] : false;
+    }
+
+    public function _definedRelations($relations = null): array
     {
         $relationNamespace = "Illuminate\Database\Eloquent\Relations";
 
