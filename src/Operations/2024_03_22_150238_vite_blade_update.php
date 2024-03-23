f308e29bd5125c1fe084239c3232ce8384cb4e01
OoBook <oguzhan@olmadikprojeler.com>
2024-03-23 04:34:57 +0300

    chore(operation): :adhesive_bandage: add vite blade update operation

---
+++ src/Operations/2024_03_22_150238_vite_blade_update.php
@@ -0,0 +1,52 @@
+<?php
+
+use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
+use Illuminate\Console\Concerns\InteractsWithIO;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Unusualify\Modularity\Facades\Modularity;
+
+return new class extends OneTimeOperation
+{
+    use InteractsWithIO;
+
+    public function __construct()
+    {
+        $this->output = new ConsoleOutput();
+    }
+    /**
+     * Determine if the operation is being processed asynchronously.
+     */
+    protected bool $async = true;
+
+    /**
+     * The queue that the job will be dispatched to.
+     */
+    protected string $queue = 'default';
+
+    /**
+     * A tag name, that this operation can be filtered by.
+     */
+    protected ?string $tag = null;
+
+    /**
+     * Process the operation.
+     */
+    public function process(): void
+    {
+        foreach (Modularity::getModules() as $key => $module) {
+            Artisan::call('unusual:make:module', [
+                'module' => $module->getName(),
+                '--just-stubs' => true,
+                '--stubs-only' => 'views/index,views/form'
+            ]);
+        }
+        $this->output->writeln('');
+        $this->output->writeln('');
+
+        $this->info("\tIndex and form blade files converted to vite format.");
+
+        $this->output->writeln('');
+    }
+};
