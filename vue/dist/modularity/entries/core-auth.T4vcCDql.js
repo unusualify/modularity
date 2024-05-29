ca72abe4bd09380026fdd3fe1c3e7622f71224f1
OoBook <oguzhan@olmadikprojeler.com>
2024-05-29 14:45:04 +0300

    fix(vue): :green_heart: add dist folder and frontend build with ModalMedia fix

--- vue/dist/modularity/entries/core-auth.T4vcCDql.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"../js/vendor.DMXVB7UE.js";import{f as s,U as m,s as i,r as p}from"../js/form.D2fxSfix.js";import"../js/awesome-phonenumber.C8KclVch.js";import"../js/fine-uploader.C_FDpdrq.js";import"../js/vuetify.Bo3hDh75.js";const o=i;o.registerModule("form",s);const r=t({store:o,mixins:[p]});r.use(m);r.mount("#auth");
-//# sourceMappingURL=core-auth.T4vcCDql.js.map
--- vue/dist/modularity/entries/core-form.BBBNTT4f.js
+++
@@ -1,2 +0,0 @@
-import{_ as s}from"../js/vendor.DMXVB7UE.js";import{f as t,U as m,s as i,r as p}from"../js/form.D2fxSfix.js";import"../js/awesome-phonenumber.C8KclVch.js";import"../js/fine-uploader.C_FDpdrq.js";import"../js/vuetify.Bo3hDh75.js";const o=i;o.registerModule("form",t);const r=s({store:o,mixins:[p]});r.use(m);r.mount("#admin");
-//# sourceMappingURL=core-form.BBBNTT4f.js.map
