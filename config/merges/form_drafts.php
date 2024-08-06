d42793f1b6103fa5dd06e1f3375494e22375e5ca
OoBook <oguzhan@olmadikprojeler.com>
2024-08-06 18:49:06 +0300

    feat: update inputs of form_draft wrt input_types structure

--- config/merges/form_drafts.php
+++ config/merges/form_drafts.php
@@ -3,74 +3,61 @@
 return [
     'user' => [
         'name' => [
-            "type" => "text",
-            "name" => "name",
-            "label" => "Name",
-            "default" => "",
+            'type' => '_name',
             'col' => [
                 'sm' => 6,
             ],
             'rules' => 'min:3|unique_table'
         ],
         'surname' => [
-            "type" => "text",
-            "name" => "surname",
-            "label" => "Surname",
-            "default" => "",
+            'type' => '_name',
+            'name' => 'surname',
+            'label' => 'Surname',
             'col' => [
                 'sm' => 6,
             ],
             'rules' => 'min:2'
         ],
         'job_title' => [
-            "type" => "text",
-            "name" => "job_title",
-            "label" => "Job Title",
-            "default" => "",
+            'type' => '_name',
+            'name' => 'job_title',
+            'label' => 'Job Title',
             'col' => [
                 'sm' => 6,
             ],
             'rules' => 'min:2'
         ],
         'email' => [
-            "type" => "text",
-            "name" => "email",
-            "label" => "E-mail",
-            "default" => "",
+            'type' => '_email',
+            'default' => '',
             'col' => [
                 'sm' => 6,
             ],
-            'rules' => 'email'
         ],
         'phone' => [
-            "type" => "custom-input-phone",
-            "name" => "phone",
-            "label" => "Phone Number",
-            "default" => "",
+            'type' => '_phone',
             'col' => [
                 'sm' => 6,
             ],
-            'clearable' => false
         ],
         'country' => [
-            "type" => "text",
-            "name" => "country",
-            "label" => "Country",
-            "default" => "",
+            'type' => '_name',
+            'name' => 'country',
+            'label' => 'Country',
             'col' => [
                 'sm' => 6,
             ],
             'rules' => 'min:3'
         ],
         'language' => [
-            "type" => "language",
-            "name" => "language",
-            "label" => "Preferred Language",
+            'type' => '_language',
+            'name' => 'language',
+            'label' => 'Preferred Language',
             'col' => [ 'sm' => 6]
         ],
         'timezone' => [
-            "type" => "timezone",
-            "name" => "timezone",
+            'type' => '_timezone',
+            'name' => 'timezone',
             'col' => ['sm' => 6],
             'name' => 'time',
             'rules' => 'sometimes|required',
@@ -78,17 +65,12 @@ return [
     ],
     'user_password' => [
         'current_password' => [
-            "type" => "password",
-            // "ext" => "password",
-            "name" => "current_password",
-            "label" => "Current Password",
-            "default" => "",
+            'type' => '_password',
+            'name' => 'current_password',
+            'label' => 'Current Password',
+            'default' => '',
             'col' => [ 'sm' => 6],
-            "appendInnerIcon" => '$non-visibility',
-            "slotHandlers" => [
-                'appendInner' => 'password',
-            ],
-            "rules" => "sometimes|required|current_password",
+            'rules' => 'sometimes|required|current_password',
         ],
         'gap-1' => [
             'type' => 'v-sheet',
@@ -101,129 +83,87 @@ return [
             ]
         ],
         'password_confirmation' => [
-            "type" => "password",
-            // "ext" => "password",
-            "name" => "password_confirmation",
-            "label" => "New Password",
-            "default" => "",
+            'type' => '_password',
+            'name' => 'password_confirmation',
+            'label' => 'New Password',
             'col' => [ 'sm' => 6],
-            "appendInnerIcon" => '$non-visibility',
-            "slotHandlers" => [
-                'appendInner' => 'password',
-            ],
             'rules' => 'sometimes|min:6'
         ],
         'password' => [
-            "type" => "password",
-            // "ext" => "password",
-            "name" => "password",
-            "label" => "Confirm Password",
-            "default" => "",
+            'type' => '_password',
+            'name' => 'password',
+            'label' => 'Confirm Password',
             'col' => [ 'sm' => 6],
-            "appendInnerIcon" => '$non-visibility',
-            "slotHandlers" => [
-                'appendInner' => 'password',
-            ],
             'rules' => 'sometimes|min:6|confirmed'
-            // 'rules' => [
-            //     'min:6',
-            //     ['confirmation', 'new_password'],
-            // ]
         ],
     ],
     'company' => [
         'name' => [
-            "type" => "text",
-            "name" => "name",
-            "label" => "Company",
-            "default" => "",
+            'type' => '_name',
+            'label' => 'Company',
             'col' => [ 'sm' => 6],
-            'rules' => [
-                ['min', 3]
-            ]
+            'rules' => 'sometimes|min:3'
         ],
         'address' => [
-            "type" => "text",
-            "name" => "address",
-            "label" => "Address",
-            "default" => "",
+            'type' => 'text',
+            'name' => 'address',
+            'label' => 'Address',
             'col' => [
                 'cols' => 12,
                 'sm' => 12,
             ],
-            'rules' => [
-                // ['email']
-            ]
         ],
         'city' => [
-            "type" => "text",
-            "name" => "city",
-            "label" => "City",
-            "default" => "",
+            'type' => 'text',
+            'name' => 'city',
+            'label' => 'City',
+            'default' => '',
             'col' => [ 'sm' => 6],
-
-            'rules' => [
-                ['min', 3]
-            ]
+            'rules' => 'sometimes|min:3'
         ],
         'state' => [
-            "type" => "text",
-            "name" => "state",
-            "label" => "State/Province",
-            "default" => "",
+            'type' => 'text',
+            'name' => 'state',
+            'label' => 'State/Province',
+            'default' => '',
             'col' => [ 'sm' => 6],
-            'rules' => [
-                ['min', 3]
-            ]
+            'rules' => 'sometimes|min:3'
         ],
         'country' => [
-            "type" => "text",
-            "name" => "country",
-            "label" => "Country",
-            "default" => "",
+            'type' => 'text',
+            'name' => 'country',
+            'label' => 'Country',
+            'default' => '',
             'col' => [ 'sm' => 6],
-            'rules' => [
-                ['min', 3]
-            ]
+            'rules' => 'sometimes|min:3'
         ],
         'zip_code' => [
-            "type" => "text",
-            "name" => "zip_code",
-            "label" => "ZIP/Postal Code",
-            "default" => "",
+            'type' => 'text',
+            'name' => 'zip_code',
+            'label' => 'ZIP/Postal Code',
+            'default' => '',
             'col' => [ 'sm' => 6],
-            'rules' => [
-                ['min', 3]
-            ]
+            'rules' => 'sometimes|min:3'
         ],
         'phone' => [
-            "type" => "custom-input-phone",
-            "name" => "phone",
-            "label" => "Phone Number",
-            "default" => "",
+            'type' => '_phone',
             'col' => [ 'sm' => 6],
-            'rules' => 'min:20',
-            'clearable' => false
         ],
         'vat_number' => [
-            "type" => "text",
-            "name" => "vat_number",
-            "label" => "VAT Number",
-            "default" => "",
+            'type' => 'text',
+            'name' => 'vat_number',
+            'label' => 'VAT Number',
+            'default' => '',
             'col' => [ 'sm' => 6],
-            'rules' => [
-                ['min', 3]
-            ]
+            'rules' => 'sometimes|min:3'
         ],
         'tax_id' => [
-            "type" => "text",
-            "name" => "tax_id",
-            "label" => "Tax ID",
-            "default" => "",
+            'type' => 'text',
+            'name' => 'tax_id',
+            'label' => 'Tax ID',
+            'default' => '',
             'col' => [ 'sm' => 6],
-            'rules' => [
-                ['min', 3]
-            ]
+            'rules' => 'sometimes|min:3'
         ],
     ]
 ];
