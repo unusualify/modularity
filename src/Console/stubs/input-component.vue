f4987f42ff45b4abf3b1eda1a6807d5a1a1d6776
OoBook <oguzhan@olmadikprojeler.com>
2024-08-09 10:35:57 +0300

    feat(command): :art: add vue input component generator command

---
+++ src/Console/stubs/input-component.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-input
+    v-model="input"
+    hideDetails="auto"
+    :variant="boundProps.variant"
+    class="$NAME$"
+    >
+    <template v-slot:default="defaultSlot">
+
+    </template>
+  </v-input>
+</template>
+
+<script>
+import { useInput, makeInputProps, makeInputEmits } from '@/hooks'
+
+export default {
+  name: '$NAME$',
+  emits: [...makeInputEmits],
+  components: {
+
+  },
+  props: {
+    ...makeInputProps(),
+  },
+  setup (props, context) {
+    return {
+      ...useInput(props, context)
+    }
+  },
+  data: function () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  methods: {
+
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style lang="sass">
+  .$NAME$
+
+
+</style>
+
+<style lang="scss">
+
+</style>
