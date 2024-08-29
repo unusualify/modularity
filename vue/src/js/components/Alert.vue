b69929b61a40371f00ab4d52a1f7599d5d9f214f
OoBook <oguzhan@olmadikprojeler.com>
2024-08-30 01:40:40 +0300

    feat: :sparkles: add dialog alert first version

--- vue/src/js/components/Alert.vue
+++ vue/src/js/components/Alert.vue
@@ -23,14 +23,18 @@
         />
       </v-btn>
     </template>
-    {{ text }}
+    <!-- {{ text }} -->
+    <div v-html="text"/>
   </v-snackbar>
 </template>
 
 <script>
-import { AlertMixin } from '@/mixins'
+// import { AlertMixin } from '@/mixins'
+import { ALERT } from '@/store/mutations/index'
+import { mapState, mapGetters } from 'vuex'
+
 export default {
-  mixins: [AlertMixin],
+  // mixins: [AlertMixin],
   data: () => ({
     timeout: 3000
   }),
@@ -51,7 +55,23 @@ export default {
     },
     defaultMessage () {
       return this.$t('messages.' + this.type)
-    }
+    },
+    show: {
+      get () {
+        return this.$store.state.alert.show
+      },
+      set (val) {
+        this.$store.commit(ALERT.SET_ALERT_SHOW, val)
+      }
+    },
+    ...mapState({
+      type: state => state.alert.type,
+      message: state => state.alert.message,
+      location: state => state.alert.location
+    }),
+    ...mapGetters([
+    //   'defaultItem',
+    ])
   },
   methods: {
     info (message = null, timeout = 3000) {
@@ -65,6 +85,26 @@ export default {
     },
     error (message = null, timeout = 3000) {
       this.open('error', message, timeout)
+    },
+    activate () {
+      this.show = true
+    },
+    deactivate () {
+      this.show = true
+    },
+    open (type = 'info', message = null, timeout = 3000) {
+      this.type = type
+      this.message = message ?? this.message
+      this.timeout = timeout
+      this.activate()
+    },
+    close () {
+      this.deactivate()
+      this.$nextTick(() => {
+        this.type = 'info'
+        this.message = null
+        this.timeout = 3000
+      })
     }
   }
 }
