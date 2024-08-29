314491173beca8c96c91616223501e4dbd5f9249
gunesbizim <gnsbzm@gmail.com>
2024-08-29 14:07:30 +0300

    refactor: :fire: inside of paymentController cleaned

--- umodules/SystemPayment/Http/Controllers/PaymentController.php
+++ umodules/SystemPayment/Http/Controllers/PaymentController.php
@@ -3,6 +3,7 @@
 namespace Modules\SystemPayment\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use Modules\Package\Entities\PackageCountry;
 use Modules\SystemPayment\Entities\PaymentService;
 use Unusualify\Modularity\Http\Controllers\BaseController;
@@ -27,72 +28,5 @@ class PaymentController extends BaseController
     /**
      *
      */
-    public function pay(Request $request, Price $price)
-    {
-        $params = $request->all();
-        $paymentService = PaymentService::find($params['payment_service_id']);
-        $payment = new Payable($paymentService->name);
-        $payment->service->pay($params);
-    }
 
-    public function paypalResponse(Request $request)
-    {
-        $allParams = $request->query();
-
-        if ($allParams['success'] == true) {
-            $paypal = new Payable('paypal');
-            $resp = $paypal->service->capturePayment($allParams['token']);
-            // dd($resp);
-            $paypal->service->updateRecord(
-                $allParams['token'],
-                'COMPLETED',
-                json_encode($resp)
-            );
-        }
-        dd('here');
-    }
-    public function garantiResponse(Request $request)
-    {
-        //Not testable at the moment
-        dd($request);
-    }
-
-    public function tebResponse(Request $request)
-    {
-        //Not testable at the moment
-
-        dd($request);
-    }
-
-    public function tebCommonResponse(Request $request)
-    {
-        $payment = new Payable('teb-common-pos');
-        if ($request->BankResponseCode == "00") {
-            // dd($request, $request->BankResponseCode);
-            $payment->service->updateRecord($request->OrderId, 'COMPLETED', $request->all());
-            //Update payment model with the response field and remove parameters
-            // return view()
-            dd('success');
-        } else {
-            $payment->service->updateRecord($request->OrderId, 'CANCELED', $request->all());        }
-        dd($request);
-    }
-
-    public function iyzicoResponse(Request $request)
-    {
-        $payment = new Payable('iyzico');
-        // dd($request->all());
-        if ($request->status == 'success') {
-            $params = [
-                'payment_id' => $request->paymentId,
-                'conversation_id' => $request->conversationId,
-                'conversation_data' => $request->conversationData
-            ];
-            // dd('here');
-            $payment->service->completePayment($params);
-
-            dd('finished');
-        }
-        dd($request);
-    }
 }
