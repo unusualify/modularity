83b13193ca435658928ea871872c68a3baf82a2a
gunesbizim <gnsbzm@gmail.com>
2024-09-20 09:33:05 +0300

    fix: :bug: where module is undefined on ManageForm in some cases

--- src/Traits/ManageForm.php
+++ src/Traits/ManageForm.php
@@ -393,7 +393,7 @@ trait ManageForm {
             if(@class_exists($hydrateClass)){
                 $input = App::make($hydrateClass, [
                     'input' => $input,
-                    'module' => $this->module
+                    'module' => $this->module ?? null
                 ])->render();
             }
 
