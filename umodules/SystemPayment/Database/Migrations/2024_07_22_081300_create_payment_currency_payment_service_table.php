a40820ad37d733f34e2c25c96fce38d2c2d9c303
OoBook <oguzhan@olmadikprojeler.com>
2024-09-27 10:17:00 +0300

    chore(psr-4): fix psr-4 issues

--- umodules/SystemPayment/Database/Migrations/2024_07_22_081300_create_payment_currency_payment_service_table.php
+++ umodules/SystemPayment/Database/Migrations/2024_07_22_081300_create_payment_currency_payment_service_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCurrencyPaymentServiceTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -28,4 +28,4 @@ class CreateCurrencyPaymentServiceTable extends Migration
     {
         Schema::dropIfExists('payment_currency_payment_service');
     }
-}
+};
