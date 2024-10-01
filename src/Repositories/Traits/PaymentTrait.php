c8f38b6912557281cb6da698fbce5f86a2a1aa24
OoBook <oguzhan@olmadikprojeler.com>
2024-10-01 11:53:09 +0300

    fix: add if Snapshot model has priceable

--- src/Repositories/Traits/PaymentTrait.php
+++ src/Repositories/Traits/PaymentTrait.php
@@ -2,26 +2,74 @@
 
 namespace Unusualify\Modularity\Repositories\Traits;
 
+use Illuminate\Support\Facades\Session;
+
 trait PaymentTrait
 {
+    /**
+     * paymentTraitRelationName
+     *
+     * @var undefined
+     */
     public $paymentTraitRelationName = null;
+
+    /**
+     * paymentTraitDefaultCurrencyId
+     *
+     * @var int
+     */
     public $paymentTraitDefaultCurrencyId = 2;
+
+    /**
+     * requiredTrait
+     *
+     * @var string
+     */
     protected $requiredTrait = 'Oobook\Priceable\Traits\HasPriceable';
 
+    /**
+     * snapshotTrait
+     *
+     * @var string
+     */
+    protected $snapshotTrait = 'Oobook\Snapshot\Traits\HasSnapshot';
+
+    /**
+     * @param \Unusualify\Modularity\Models\Model $object
+     * @param array $fields
+     * @return void
+     */
     protected function afterSavePaymentTrait($object, $fields)
     {
-        $currencyId = isset($fields['currency_id']) ? $fields['currency_id'] : $this->paymentTraitDefaultCurrencyId;
+        $session_currency = Session::get('currency_id');
 
-        if ($this->paymentTraitRelationName) {
+        $currencyId = isset($fields['currency_id'])
+            ? $fields['currency_id']
+            : $session_currency ?? $this->paymentTraitDefaultCurrencyId;
+
+
+        if ( $this->paymentTraitRelationName ) {
             $relatedClass = $object->{$this->paymentTraitRelationName}()->getRelated();
-            if (classHasTrait($relatedClass, $this->requiredTrait)) {
 
+            $requirementMet = false;
+
+            if ( classHasTrait($relatedClass, $this->requiredTrait) ) {
+                $requirementMet = true;
+            } else if( classHasTrait($relatedClass, $this->snapshotTrait)
+                && classHasTrait($relatedClass->source()->getRelated(), $this->requiredTrait)
+            ){
+                $requirementMet = true;
+            }
+
+            if ($requirementMet) {
+                // dd($object->{$this->paymentTraitRelationName});
                 $records = $object->{$this->paymentTraitRelationName};
                 $totalPrice = 0;
 
                 if ($records instanceof \Illuminate\Database\Eloquent\Collection) {
                     foreach ($records as $record) {
                         $price = $record->prices()->where('currency_id', $currencyId)->first();
+
                         if (!is_null($price))
                             $totalPrice += $price->display_price;
                     }
@@ -46,6 +94,11 @@ trait PaymentTrait
         }
     }
 
+    /**
+     * Form Schema to use on vue frontend side
+     *
+     * @return void
+     */
     public function getPaymentFormSchema()
     {
         return [
