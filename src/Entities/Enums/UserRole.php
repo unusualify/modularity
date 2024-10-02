301036d8da97a708b82db54d7efd3cc11ffb1baa
OoBook <oguzhan@olmadikprojeler.com>
2024-10-02 20:56:25 +0300

    fix: enum for php >=8.1

--- src/Entities/Enums/UserRole.php
+++ src/Entities/Enums/UserRole.php
@@ -2,23 +2,29 @@
 
 namespace Unusualify\Modularity\Entities\Enums;
 
-if ( version_compare(PHP_VERSION, '8.1.0', '<') ) {
-  use MyCLabs\Enum\Enum;
-  class UserRole extends Enum
-  {
-      const VIEWONLY = 'View only';
-      const PUBLISHER = 'Publisher';
-      const ADMIN = 'Admin';
-  }
-}else {
-  enum UserRole:string {
+enum UserRole:string {
     case SUPERADMIN = 'Superadmin';
     case ADMIN = 'Admin';
     case PUBLISHER = 'Publisher';
     case VIEWONLY = 'View Only';
-  }
-
 }
+// if ( version_compare(PHP_VERSION, '8.1.0', '<') ) {
+//   use MyCLabs\Enum\Enum;
+//   class UserRole extends Enum
+//   {
+//       const VIEWONLY = 'View only';
+//       const PUBLISHER = 'Publisher';
+//       const ADMIN = 'Admin';
+//   }
+// }else {
+//   enum UserRole:string {
+//     case SUPERADMIN = 'Superadmin';
+//     case ADMIN = 'Admin';
+//     case PUBLISHER = 'Publisher';
+//     case VIEWONLY = 'View Only';
+//   }
+
+// }
 // if(phpversion())
 // use MyCLabs\Enum\Enum;
 
