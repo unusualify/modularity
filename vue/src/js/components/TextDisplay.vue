21cdffd750433bb2ac351f081a0e89f962845fe4
OoBook <oguzhan@olmadikprojeler.com>
2024-11-04 09:31:38 +0300

    feat: add ue-text-display component

---
+++ vue/src/js/components/TextDisplay.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex align-baseline">
+    <span class="font-weight-bold">{{ text }}</span>
+    <span v-if="subText" class="text-medium-emphasis">{{ subText }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ue-text-display',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    subText: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
