03b463f073dd800b64af1c3fde34a26c2dc5c18d
OoBook <oguzhan@olmadikprojeler.com>
2024-11-04 10:51:23 +0300

    build(vue): :building_construction: add new v0.16.0 front build

--- vue/dist/modularity/entries/core-auth.C1Uf4GT6.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"../js/vendor.DZ890I12.js";import{f as s,U as m,s as i,r as p}from"../js/form.BNiSTDHg.js";import"../js/awesome-phonenumber.C8KclVch.js";import"../js/fine-uploader.C_FDpdrq.js";import"../js/vuetify.C0XZ2-BZ.js";const o=i;o.registerModule("form",s);const r=t({store:o,mixins:[p]});r.use(m);r.mount("#auth");
-//# sourceMappingURL=core-auth.C1Uf4GT6.js.map
