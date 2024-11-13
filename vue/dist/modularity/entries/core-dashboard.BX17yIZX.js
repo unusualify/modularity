bcbc913d7c91ce5a8f0cd5ca6b68ac0aba85b1b1
gunesbizim <gnsbzm@gmail.com>
2024-11-13 17:33:26 +0300

    build: :bookmark: gunes test build

---
+++ vue/dist/modularity/entries/core-dashboard.BX17yIZX.js
@@ -0,0 +1,2 @@
+import{Z as a}from"../js/vendor.xB9OFvt5.js";import{f as r,U as e,s,r as m}from"../js/form.BLXARi84.js";import{d as i,U as p}from"../js/datatable.CGFKUf_o.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.B9H95_Ym.js";const t=s;t.registerModule("datatable",i);t.registerModule("form",r);const o=a({store:t,mixins:[m]});o.use(e);o.component("ue-datatable",p);o.mount("#admin");
+//# sourceMappingURL=core-dashboard.BX17yIZX.js.map
