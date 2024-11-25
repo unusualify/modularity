8e948a77ad59a811930d73a4cb1f8d30d2f32e63
gunesbizim <gnsbzm@gmail.com>
2024-11-25 14:22:19 +0300

    refactor: :recycle: refactor dashboard to use connector instead of controller

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -60,236 +60,26 @@ class DashboardController extends BaseController
 
         // );
         $blocks = app()->config->get(unusualBaseKey() . '.ui_settings.dashboard.blocks');
-        // $blocks = $blocks +
-        // [
-        //     1 => [
-        //         'component' => 'new-table',
-        //         'col' => [
-        //             'cols' => 12,
-        //             'xxl' => 6,
-        //             'xl' => 6,
-        //             'lg' => 6,
-        //             's' => 12,
-        //             'class' => 'pl-theme-semi pb-theme-semi'
-        //         ],
-        //         'attributes' => [
-        //             'customTitle' => 'ANNOUNCEMENTS',
-        //             'tableSubtitle' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lobortis.',
-        //             'hide-headers' => true,
-        //             'fullWidthWrapper' => false,
-        //             'hideSearchField' => true,
-        //             'tableType' => 'dashboard',
-        //             'style' => '',
-        //             'items' =>  App::make(SurveyRepository::class)->get([], [], [
-        //                 'created_at' => 'desc'
-        //             ], 2)->items(),
-        //             'columns' => [
-        //                 [
-        //                     'title' => 'name',
-        //                     'key' => 'created_at',
-        //                     'align' => 'start',
-        //                     'sortable' => true,
-        //                     'filterable' => false,
-        //                     'groupable' => false,
-        //                     'divider' => false,
-        //                     'class' => '',
-        //                     'cellClass' => '',
-        //                     'width' => '',
-        //                     // 'max-width' => 'max-content',
-        //                     'searchable' => true,
-        //                     'isRowEditable' => true,
-        //                     'isColumnEditable' => false,
-        //                     'formatter' => [
-        //                         'date',
-        //                         'numeric'
-        //                     ]
-        //                     // 'formatter' => ['date', 'numeric'],
-        //                 ],
-        //                 [
-        //                     'title' => 'Name',
-        //                     'key' => 'name',
-        //                     'align' => 'start',
-        //                     'sortable' => false,
-        //                     'filterable' => false,
-        //                     'groupable' => false,
-        //                     'divider' => false,
-        //                     'class' => '',
-        //                     'cellClass' => '',
-        //                     'width' => '',
-        //                     'searchable' => true,
-        //                     'isRowEditable' => true,
-        //                     'isColumnEditable' => true,
-        //                     'formatter' => [
 
-        //                     ],
-        //                 ],
-        //                 [
-        //                     'title' => 'Actions',
-        //                     'key' => 'actions',
-        //                     'sortable' => false,
-
-        //                 ],
-        //             ],
-        //             'tableOptions' => [
-        //                 'page'          => 1,
-        //                 'itemsPerPage'  => 1,
-        //                 'sortBy'        => [],
-        //                 'multiSort'     => false,
-        //                 'mustSort'      => false,
-        //                 'groupBy'       => [],
-        //             ],
-        //             'slots' => [
-        //                 'headerBtn' => [
-        //                     'elements' => [
-        //                         [
-        //                             'tag' => 'div',
-        //                             'attributes' => [
-        //                                 'class' => 'text-right pa-8',
-        //                             ],
-        //                             'elements' => [
-        //                                 [
-        //                                     'tag' => 'v-btn-tertiary',
-        //                                     'elements' => 'MANAGE RELEASES',
-        //                                 ]
-        //                             ]
-        //                         ]
-        //                     ]
-        //                 ]
-        //             ]
-        //         ],
-        //     ],
-        //     2 => [
-        //         'component' => 'new-table',
-        //         'col' => [
-        //             'cols' => 12,
-        //             'xxl' => 12,
-        //             'xl' => 12,
-        //             'lg' => 12,
-        //             'class' => ''
-        //         ],
-        //         'attributes' => [
-
-        //             'customTitle' => 'Vimeo Webinars',
-        //             'tableSubtitle' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lobortis.',
-        //             'tableType' => 'dashboard',
-        //             'hide-headers' => false,
-        //             'fullWidthWrapper' => true,
-        //             'hideSearchField' => true,
-        //             'fillHeight' => true,
-        //             'style' => '',
-        //             'items' =>  App::make(VimeoWebinarRepository::class)->get([], [], [
-        //                 'created_at' => 'desc'
-        //             ], 2)->items(),
-        //             'columns' => [
-        //                 [
-        //                     'title' => 'Date',
-        //                     'key' => 'start_date',
-        //                     'align' => 'start',
-        //                     'sortable' => true,
-        //                     'filterable' => false,
-        //                     'groupable' => false,
-        //                     'divider' => false,
-        //                     'class' => '',
-        //                     'cellClass' => '',
-        //                     'width' => '',
-        //                     // 'max-width' => 'max-content',
-        //                     'searchable' => true,
-        //                     'isRowEditable' => true,
-        //                     'isColumnEditable' => false,
-        //                     'formatter' => [
-        //                         'date',
-        //                         'numeric'
-        //                     ]
-        //                     // 'formatter' => ['date', 'numeric'],
-        //                 ],
-        //                 [
-        //                     'title' => 'Name',
-        //                     'key' => 'name',
-        //                     'align' => 'start',
-        //                     'sortable' => false,
-        //                     'filterable' => false,
-        //                     'groupable' => false,
-        //                     'divider' => false,
-        //                     'class' => '',
-        //                     'cellClass' => '',
-        //                     'width' => '',
-        //                     'searchable' => true,
-        //                     'isRowEditable' => true,
-        //                     'isColumnEditable' => true,
-        //                     'formatter' => [
-        //                     ],
-        //                 ],
-        //                 [
-        //                     'title' => 'Published',
-        //                     'key' => 'published',
-        //                     'align' => 'start',
-        //                     'sortable' => false,
-        //                     'filterable' => false,
-        //                     'groupable' => false,
-        //                     'divider' => false,
-        //                     'class' => '',
-        //                     'cellClass' => '',
-        //                     'width' => '',
-        //                     'searchable' => true,
-        //                     'isRowEditable' => true,
-        //                     'isColumnEditable' => true,
-        //                     'formatter' => [
-        //                         'status',
-        //                         [
-        //                             'Not Published',
-        //                             'Published'
-        //                         ],
-        //                         [
-        //                             'blue',
-        //                             'red',
-        //                         ]
-        //                     ],
-        //                 ],
-        //                 [
-        //                     'title' => 'Actions',
-        //                     'key' => 'actions',
-        //                     'sortable' => false,
-
-        //                 ],
-        //             ],
-        //             'tableOptions' => [
-        //                 'page'          => 1,
-        //                 'itemsPerPage'  => 1,
-        //                 'sortBy'        => [],
-        //                 'multiSort'     => false,
-        //                 'mustSort'      => false,
-        //                 'groupBy'       => [],
-        //             ],
-        //             'slots' => [
-        //                 'bottom' => [
-        //                     'elements' => [
-        //                         [
-        //                             'tag' => 'div',
-        //                             'attributes' => [
-        //                                 'class' => 'text-right pa-8',
-        //                             ],
-        //                             'elements' => [
-        //                                 [
-        //                                     'tag' => 'v-btn-tertiary',
-        //                                     'elements' => 'MANAGE RELEASES',
-        //                                 ]
-        //                             ]
-        //                         ]
-        //                     ]
-        //                 ]
-        //             ]
-        //         ],
-        //     ],
-        // ] ;
+        // dd('here');
 
         foreach ($blocks as $index => $block) {
             switch ($block['component']) {
                 case 'table':
-                    $controller = App::make($block['controller'])->setTableAttributes(tableOptions: $block['attributes']['tableOptions'],
-                    );
-                    $block['attributes']['items'] = $controller->getIndexData()['initialResource']->resource['data'];
-                    $block['attributes']['endpoints'] = $controller->getIndexData()['endpoints'];
-                    $block['attributes']['rowActions'] = $controller->getTableActions();
+                    // $controller = App::make($block['controller'])->setTableAttributes(tableOptions: $block['attributes']['tableOptions'],
+                    // );
+                    $data = $this->initConnector($block['connector']);
+                    // dd($data);
+                    // dd(
+                    //     $controller->getIndexData()['initialResource']->resource['data'],
+                    //     $data
+                    // );
+                    // $block['attributes']['items'] = $controller->getIndexData()['initialResource']->resource['data'];
+                    $block['attributes']['items'] = $data['items']->toArray();
+                    $block['attributes']['endpoints'] = array_merge($this->transformRoutes($data['module']->getRouteUris($data['route'])), $this->getUrls());
+                    // dd(array_merge($block['attributes']['endpoints'], $this->getUrls()));
+
+                    // $block['attributes']['rowActions'] = $controller->getTableActions();
                     // in order to keep url as default home url
 
                     $block['attributes']['tableOptions']['search'] = null;
@@ -298,23 +88,31 @@ class DashboardController extends BaseController
                     break;
                 case 'board-information-plus':
                     $cards = $block['cards'] ?? [];
-                    foreach ($cards as $key => $card) {
-                        try {
-                            $repository = App::make($card['repository']);
-                            $data = array_reduce(explode('|', $card['method']), function ($s, $a) use ($repository) {
-                                [$methodName, $args] = array_pad(explode(':', $a, 2), 2, null);
-                                if ($args) {
-                                    $s = empty($s) ? $repository->{$methodName}(...explode(':', $args))->get() : $s->{$methodName}(...explode(':', $args))->get();
-                                } else {
-                                    $s = empty($s) ? $repository->{$methodName}() : $s->$methodName();
-                                }
+                    // dd($cards);
 
-                                return $s;
-                            });
+                    foreach ($cards as $key => $card) {
+                        $data = $this->initConnector($card['connector']);
+                        try {
+                            $data = $this->initConnector($card['connector']);
+                            // dd($data);
+                            // $repository = App::make($card['repository']);
+                            // $data = array_reduce(explode('|', $card['method']), function ($s, $a) use ($repository) {
+                            //     [$methodName, $args] = array_pad(explode(':', $a, 2), 2, null);
+                            //     if ($args) {
+                            //         $s = empty($s) ? $repository->{$methodName}(...explode(':', $args))->get() : $s->{$methodName}(...explode(':', $args))->get();
+                            //     } else {
+                            //         $s = empty($s) ? $repository->{$methodName}() : $s->$methodName();
+                            //     }
+
+                            //     return $s;
+                            // });
+                            // dd($data);
+                            // dd($data);
                         } catch (\Throwable $th) {
                             $data = '-';
+                            // dd('here');
                         }
-                        $card['data'] = $data;
+                        $card['data'] = $data['items'];
                         $cards[$key] = $card;
                     }
                     $blocks[$index]['attributes']['cards'] = $cards;
@@ -335,4 +133,28 @@ class DashboardController extends BaseController
 
         return View::make($view, $options);
     }
+
+    protected function transformRoutes($routes) {
+        $result = [];
+        foreach ($routes as $key => $value) {
+            // Skip if value is an array
+            if (is_array($value)) {
+                continue;
+            }
+
+            // Transform the key - remove everything before last dot
+            $newKey = preg_replace('/^.*\.([^.]+)$/', '$1', $key);
+
+            // Transform the value - replace {something} with :id
+            $newValue = preg_replace('/\{[^}]+\}/', ':id', $value);
+
+            // Add base url to the path using Laravel's url helper
+            $newValue = url($newValue);
+
+            $result[$newKey] = $newValue;
+        }
+        return $result;
+    }
+
+    // Usage example:
 }
