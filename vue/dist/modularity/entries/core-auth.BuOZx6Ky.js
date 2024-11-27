8dd79f5781e5f5d4f0f13586cb41a9c5474f0787
OoBook <oguzhan@olmadikprojeler.com>
2024-11-27 15:54:02 +0300

    build(vue): :building_construction: add new v0.20.1 build

--- vue/dist/modularity/entries/core-auth.BuOZx6Ky.js
+++
@@ -1,2 +0,0 @@
-import{Z as t}from"../js/vendor.D6_4FyNf.js";import{f as s,U as m,s as i,r as p}from"../js/form.CMFK0By0.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.jJgfTs1s.js";const o=i;o.registerModule("form",s);const r=t({store:o,mixins:[p]});r.use(m);r.mount("#auth");
-//# sourceMappingURL=core-auth.BuOZx6Ky.js.map
--- vue/dist/modularity/entries/core-form.CEMDkkra.js
+++
@@ -1,2 +0,0 @@
-import{Z as s}from"../js/vendor.D6_4FyNf.js";import{f as t,U as m,s as i,r as p}from"../js/form.CMFK0By0.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.jJgfTs1s.js";const o=i;o.registerModule("form",t);const r=s({store:o,mixins:[p]});r.use(m);r.mount("#admin");
-//# sourceMappingURL=core-form.CEMDkkra.js.map
