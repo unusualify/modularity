9c7d604c84144c24f409c04152f0bfc4cdd61e88
OoBook <oguzhan@olmadikprojeler.com>
2024-12-25 14:33:05 +0300

    fix(tests): correct import paths in v-input-chat test file
    - Updated import statements in the v-input-chat.test.js file to reflect the correct relative paths for vuetify and the VInputChat component.
    - This change ensures proper module resolution, enhancing the reliability of the tests.

--- vue/test/components/v-input-chat.test.js
+++ vue/test/components/v-input-chat.test.js
@@ -2,9 +2,9 @@
 import { describe, expect, test, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 
-import vuetify from '../src/js/plugins/vuetify'
+import vuetify from '../../src/js/plugins/vuetify'
 
-import VInputChat from '../src/js/components/VInputChat.vue'
+import VInputChat from '../../src/js/components/inputs/Chat.vue'
 
 let getModel = () => {}
 
