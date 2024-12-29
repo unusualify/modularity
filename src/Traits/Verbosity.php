248bc5e80f8e73029f263f0efcc22b107a539430
OoBook <oguzhan@olmadikprojeler.com>
2024-12-29 22:46:55 +0300

    feat: :sparkles: add Verbosity trait for enhanced output control
    Introduced a new `Verbosity` trait that provides methods to manage verbosity levels in the application. This trait includes a mapping of human-readable verbosity levels to Symfony's `OutputInterface` constants, allowing for flexible output control. The methods `setVerbosity`, `getVerbosity`, and various checks for verbosity levels (quiet, verbose, very verbose, debug) enhance the application's output handling capabilities.

---
+++ src/Traits/Verbosity.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Unusualify\Modularity\Traits;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+trait Verbosity
+{
+    /**
+     * The mapping between human readable verbosity levels and Symfony's OutputInterface.
+     *
+     * @var array
+     */
+    protected $verbosityMap = [
+        'v' => OutputInterface::VERBOSITY_VERBOSE,
+        'vv' => OutputInterface::VERBOSITY_VERY_VERBOSE,
+        'vvv' => OutputInterface::VERBOSITY_DEBUG,
+        'quiet' => OutputInterface::VERBOSITY_QUIET,
+        'normal' => OutputInterface::VERBOSITY_NORMAL,
+    ];
+
+    protected $verbosity = OutputInterface::VERBOSITY_NORMAL;
+
+    public function setVerbosity($verbosity)
+    {
+        if (isset($this->verbosityMap[$verbosity])) {
+            $verbosity = $this->verbosityMap[$verbosity];
+        } elseif (! is_int($verbosity)) {
+            $verbosity = $this->verbosity;
+        } elseif (is_int($verbosity)) {
+            $verbosity = $verbosity;
+        }
+
+        $this->verbosity = $verbosity;
+
+        return $this;
+    }
+
+    public function getVerbosity()
+    {
+        return $this->verbosity;
+    }
+
+    public function isQuiet()
+    {
+        return $this->verbosity === OutputInterface::VERBOSITY_QUIET;
+    }
+
+    public function isVerbose()
+    {
+        return OutputInterface::VERBOSITY_VERBOSE <= $this->verbosity;
+    }
+
+    public function isVeryVerbose()
+    {
+        return OutputInterface::VERBOSITY_VERY_VERBOSE <= $this->verbosity;
+    }
+
+    public function isDebug()
+    {
+        return OutputInterface::VERBOSITY_DEBUG <= $this->verbosity;
+    }
+}
