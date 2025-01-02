45f1ee2c7624e5b870958cfd6f8b4b649387801d
OoBook <oguzhan@olmadikprojeler.com>
2025-01-02 12:29:05 +0300

    refactor: :recycle: simplify Vuex store configuration by removing unused state and mutations
    This commit refactors the Vuex store configuration by eliminating unnecessary state properties and mutations related to user profile management. Key changes include:
    
    - Removed `currentUser`, `systemPackageVersions`, and other related state properties from the config module.
    - Deleted the `SET_PROFILE_DATA` mutation and its associated getter.
    - Introduced new state properties to track asynchronous requests, enhancing the store's functionality.
    
    These updates streamline the Vuex store, improving maintainability and focusing on essential state management features.

--- vue/src/js/store/mutations/config.js
+++ vue/src/js/store/mutations/config.js
@@ -4,13 +4,11 @@ export const SET_SIDEBAR = '__setSidebar'
 export const SET_REQUEST_IN_PROGRESS = '__setRequestInProgress'
 export const INCREASE_AXIOS_REQUEST = '__increaseAxiosRequest'
 export const DECREASE_AXIOS_REQUEST = '__decreaseAxiosRequest'
-export const SET_PROFILE_DATA = '__setProfileData'
 
 export default {
   SIDEBAR_TOGGLE,
   SET_SIDEBAR,
   SET_REQUEST_IN_PROGRESS,
   INCREASE_AXIOS_REQUEST,
-  DECREASE_AXIOS_REQUEST,
-  SET_PROFILE_DATA,
+  DECREASE_AXIOS_REQUEST
 }
