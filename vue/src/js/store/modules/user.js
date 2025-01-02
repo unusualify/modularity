2a1379b71ffd7701ccbccca31660f022089e3327
OoBook <oguzhan@olmadikprojeler.com>
2025-01-02 12:28:21 +0300

    feat: :sparkles: add user profile management to Vuex store
    This commit enhances the Vuex store by introducing user profile management features. Key changes include:
    
    - Added new state properties for user profile data, profile route, and profile shortcut models.
    - Implemented a getter to retrieve the user profile.
    - Introduced a mutation to set profile data using the new USER mutation type.
    - Created a new mutations file for user-related mutations.
    
    These updates improve the application's ability to manage user profiles effectively, enhancing the overall user experience.

--- vue/src/js/store/modules/user.js
+++ vue/src/js/store/modules/user.js
@@ -1,10 +1,21 @@
+import { USER } from '../mutations'
+
+
 const state = {
   locale: window[import.meta.env.VUE_APP_NAME]?.LOCALE ?? 'en',
   timezone: window[import.meta.env.VUE_APP_NAME]?.TIMEZONE ?? 'Europe/London',
-  authorization: window[import.meta.env.VUE_APP_NAME]?.AUTHORIZATION ?? {}
+  authorization: window[import.meta.env.VUE_APP_NAME]?.AUTHORIZATION ?? {},
+
+  profile: window[import.meta.env.VUE_APP_NAME]?.STORE?.user?.profile ?? {},
+  profileRoute: window[import.meta.env.VUE_APP_NAME]?.STORE?.user?.profileRoute ?? '',
+  profileShortcutModel: window[import.meta.env.VUE_APP_NAME]?.STORE?.user?.profileShortcutModel ?? {},
+  profileShortcutSchema: window[import.meta.env.VUE_APP_NAME]?.STORE?.user?.profileShortcutSchema ?? {}
 }
 
 const getters = {
+  userProfile: state => {
+    return state.profile
+  },
   isSuperAdmin: state => {
     return state.authorization.isSuperAdmin ?? false
   },
@@ -14,7 +25,6 @@ const getters = {
   userLocale: state => {
     return state.locale
   },
-
   hasRestorable: state => {
     return state.authorization.hasRestorable ?? false
   },
@@ -31,7 +41,9 @@ const actions = {
 }
 
 const mutations = {
-
+  [USER.SET_PROFILE_DATA] (state, data) {
+    state.profile = data
+  },
 }
 
 export default {
