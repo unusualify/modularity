69a6988298040a6ed5e95c5a634a6becf43424aa
OoBook <oguzhan@olmadikprojeler.com>
2025-01-21 16:04:17 +0300

    build(vue): update build artifacts for v0.24.1

--- vue/dist/modularity/entries/core-form.DfHjHZe3.js
+++
@@ -1,2 +0,0 @@
-import{Z as s}from"../js/vendor.Tzo5HDY2.js";import{f as t,U as m,s as i,r as p}from"../js/form.COWZDORF.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.CGhZtPog.js";const o=i;o.registerModule("form",t);const r=s({store:o,mixins:[p]});r.use(m);r.mount("#admin");
-//# sourceMappingURL=core-form.DfHjHZe3.js.map
