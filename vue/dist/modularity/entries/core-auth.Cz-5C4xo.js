0b57543880207984eae812486a6152b1c53a37fd
OoBook <oguzhan@olmadikprojeler.com>
2025-01-22 23:02:12 +0300

    build(vue): update build artifacts for v0.25.0

--- vue/dist/modularity/entries/core-auth.Cz-5C4xo.js
+++
@@ -1,2 +0,0 @@
-import{Z as t}from"../js/vendor.Tzo5HDY2.js";import{f as s,U as m,s as i,r as p}from"../js/form.CtJWdKHn.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.CGhZtPog.js";const o=i;o.registerModule("form",s);const r=t({store:o,mixins:[p]});r.use(m);r.mount("#auth");
-//# sourceMappingURL=core-auth.Cz-5C4xo.js.map
