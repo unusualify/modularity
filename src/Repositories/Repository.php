b72f8c0989429e0bc58bdfcb05f2f629bb13751a
OoBook <oguzhan@olmadikprojeler.com>
2025-01-31 16:52:30 +0300

    fix(repository): remove PaymentTrait and add debug statements for search functionality
    Modify Repository base class:
    - Remove PaymentTrait from used traits
    - Add debug statements (dd) to investigate search and translated attributes handling

--- src/Repositories/Repository.php
+++ src/Repositories/Repository.php
@@ -22,7 +22,7 @@ use Unusualify\Modularity\Traits\ManageNames;
 
 abstract class Repository
 {
-    use DatesTrait, ManageNames, MethodTransformers, RelationTrait, PaymentTrait;
+    use DatesTrait, ManageNames, MethodTransformers, RelationTrait;
 
     /**
      * @var \Unusualify\Modularity\Models\Model
@@ -70,15 +70,17 @@ abstract class Repository
     public function get($with = [], $scopes = [], $orders = [], $perPage = 20, $forcePagination = false)
     {
         $query = $this->model->query();
-
         $query = $this->model->with($this->formatWiths($query, $with));
 
         if (isset($scopes['searches']) && isset($scopes['search']) && is_array($scopes['searches'])) {
             $translatedAttributes = $this->model->translatedAttributes ?? [];
 
+            dd($scopes['searches'], $translatedAttributes);
             $searches = Arr::where($scopes['searches'], function (string|int $value, int $key) use ($translatedAttributes) {
                 return ! in_array($value, $translatedAttributes);
             });
+
+            dd($searches, $translatedAttributes, $scopes, );
 
             $this->searchIn($query, $scopes, 'search', $searches);
 
