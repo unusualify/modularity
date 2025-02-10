689a0c6bda29740e3e5b8a29ad50ad01c654eea8
OoBook <oguzhan@olmadikprojeler.com>
2025-02-10 12:33:55 +0300

    feat: enhance EventMakeCommand with abstract event class selection and self-module support
    Update EventMakeCommand to:
    - Add '--self' flag for creating events in the Modularity vendor path
    - Implement dynamic abstract event class selection
    - Improve namespace and file path generation for events
    - Add production environment check for vendor path event creation

--- src/Console/EventMakeCommand.php
+++ src/Console/EventMakeCommand.php
@@ -4,14 +4,15 @@ namespace Unusualify\Modularity\Console;
 
 use Illuminate\Support\Str;
 use Nwidart\Modules\Support\Stub;
+use Symfony\Component\Finder\Finder;
 use Unusualify\Modularity\Facades\Modularity;
-use function Laravel\Prompts\{select, text};
+use function Laravel\Prompts\{select, text, confirm};
 
 class EventMakeCommand extends BaseCommand
 {
     protected $hidden = true;
 
-    	/**
+    /**
 	 * The name and signature of the console command.
 	 *
 	 * @var string
@@ -19,6 +20,7 @@ class EventMakeCommand extends BaseCommand
 	protected $signature = 'modularity:make:event
 		{name : The name of event}
 		{module? : The name of module}
+        {--self : The path of the modularity}
         {--f|force : Overwrite existing file}
         {--should-broadcast : Should the event broadcast}
         {--should-broadcast-now : Should the event broadcast now}
@@ -44,20 +46,6 @@ class EventMakeCommand extends BaseCommand
         parent::__construct();
     }
 
-    /*
-     * Executes the console command.
-     *
-     * @return mixed
-     */
-    public function _handle(): int
-    {
-        $name = $this->argument('name');
-        $module = $this->argument('module');
-        // handle command
-        $this->info('Event created');
-        return 0;
-    }
-
     /**
      * @return mixed
      */
@@ -65,18 +53,54 @@ class EventMakeCommand extends BaseCommand
     {
         $moduleName = $this->getModuleName();
         $className = $this->getFileName();
-
+        $self = $this->option('self');
         $namespace = 'App\Events';
 
-        if($moduleName) {
+        $implements = [];
+        $extend = null;
+        $namespaces = [];
+        $attributes = [];
+        $methods = [];
+
+        if($self){
+            $namespace = 'Unusualify\Modularity\Events';
+
+
+        }else if($moduleName) {
             $module = Modularity::findOrFail($moduleName);
             $namespace = $module->getTargetClassNamespace('event');
         }
 
-        $implements = [];
-        $namespaces = [];
-        $attributes = [];
-        $methods = [];
+
+        $abstractEventPaths = [
+            base_path('app/Events'),
+            Modularity::getModulePath('/**/Events'),
+            Modularity::getVendorPath('/src/Events'),
+        ];
+        $abstractEventClasses = collect(Finder::create()->files()->depth(0)->in($abstractEventPaths))->reduce(function($carry, $file) {
+            $className = get_file_class($file->getRealPath());
+
+            if($className) {
+                $reflector = new \ReflectionClass(get_file_class($file->getRealPath()));
+                if($reflector->isAbstract()) {
+                    $carry[$className] = $file;
+                }
+            }
+
+            return $carry;
+        }, collect());
+
+        if($abstractEventClasses->count() > 0 && confirm('Do you want use a specific abstract event class?', default: false)) {
+            $abstractEventClass = select(
+                label: 'Select the abstract event class',
+                options: $abstractEventClasses->keys()->toArray(),
+                default: $abstractEventClasses->keys()->first(),
+            );
+            $abstractEventClassShortName = get_class_short_name($abstractEventClass);
+            $namespaces[] = $abstractEventClass;
+            $extend = $abstractEventClassShortName;
+        }
+
 
         if($this->option('should-dispatch-after-commit')) {
             $implements[] = 'ShouldDispatchAfterCommit';
@@ -125,6 +149,10 @@ class EventMakeCommand extends BaseCommand
             $broadcastOn = "new {$channelType}('{$channelName}')";
         }
 
+        if($extend) {
+            $className = $className . ' extends ' . $extend;
+        }
+
         if(count($implements) > 0) {
             $className = $className . ' implements ' . implode(', ', $implements);
         }
@@ -147,15 +175,22 @@ class EventMakeCommand extends BaseCommand
     protected function getDestinationFilePath()
     {
         $moduleName = $this->getModuleName();
-        $fileName = $this->getFileName();
+        $fileName = $this->getFileName() . '.php';
+        $self = $this->option('self');
 
-        if (!$moduleName) {
-            return base_path("app/Events/{$fileName}.php");
+        if($self){
+            if(Modularity::isProduction()) {
+                throw new \Exception('You cannot create an event in the vendor path in production');
+            }
+            return Modularity::getVendorPath('src/Events/') . "{$fileName}";
+
+        }else if (!$moduleName){
+            return base_path("app/Events/{$fileName}");
         }
 
         $module = Modularity::findOrFail($moduleName);
 
-        $targetClassPath = $module->getTargetClassPath('event', $fileName . ".php");
+        $targetClassPath = $module->getTargetClassPath('event', $fileName);
 
         return $targetClassPath;
     }
