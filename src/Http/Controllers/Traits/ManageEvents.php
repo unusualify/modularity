cb4327df8225dd6e466f915d3b46ee32d2790dca
OoBook <oguzhan@olmadikprojeler.com>
2025-02-14 15:38:00 +0300

    refactor: update event handling in ManageEvents trait
    Modify the ManageEvents trait to:
    - Update event class imports from Notification to SystemNotification module
    - Enable all event types (create, update, delete, restore)
    - Simplify event firing mechanism using static dispatch method
    - Remove debug statements and commented code

--- src/Http/Controllers/Traits/ManageEvents.php
+++ src/Http/Controllers/Traits/ManageEvents.php
@@ -2,30 +2,32 @@
 
 namespace Unusualify\Modularity\Http\Controllers\Traits;
 
-use Modules\Notification\Events\ModelCreatedEvent;
+use Modules\SystemNotification\Events\ModelCreated;
+use Modules\SystemNotification\Events\ModelUpdated;
+use Modules\SystemNotification\Events\ModelDeleted;
+use Modules\SystemNotification\Events\ModelForceDeleted;
+use Modules\SystemNotification\Events\ModelRestored;
 
 trait ManageEvents
 {
     protected $events = [
-        'create' => ModelCreatedEvent::class,
-        'store' => ModelCreatedEvent::class,
-        // 'edit' => ModelUpdatedEvent::class,
-        // 'update' => ModelUpdatedEvent::class,
-        // 'destroy' => ModelDeletedEvent::class,
-        // 'forceDelete' => ModelDeletedEvent::class,
-        // 'restore' => ModelRestoredEvent::class,
+        'create' => ModelCreated::class,
+        'store' => ModelCreated::class,
+        'edit' => ModelUpdated::class,
+        'update' => ModelUpdated::class,
+        'destroy' => ModelDeleted::class,
+        'forceDelete' => ModelForceDeleted::class,
+        'restore' => ModelRestored::class,
     ];
 
     public function fireEvent($event, $data = [])
     {
-        event($event, $data);
+        // event($event, $data);
+        $event::dispatch($data);
     }
 
     public function fireEventIf($condition, $event, $data = [])
     {
-        event(new ModelCreatedEvent($model));
-        ModelCreatedEvent::dispatch($model);
-        ModelCreatedEvent::dispatchIf($this->request->ajax(), $model);
         if ($condition) {
             $this->fireEvent($event, $data);
         }
@@ -33,17 +35,15 @@ trait ManageEvents
 
     public function handleActionEvent($model, $action)
     {
+        // dd($model->getChanges(), $model->isDirty(), $model->getDirty());
+
+        // return;
         if (!isset($this->events[$action])) {
             return;
         }
 
         $event = $this->events[$action];
 
-        dd($action, $model, $event);
-
         $this->fireEvent($event, $model);
     }
-
-
-
 }
