b7d83ea5b58c1b857a737df60a5097ad1ae4d866
OoBook <oguzhan@olmadikprojeler.com>
2025-02-18 23:48:03 +0300

    refactor: add returnObject configuration to input hydrate classes
    Add support for:
    - Introducing returnObject option in Autocomplete, Combobox, Select, and SelectScroll hydrate classes
    - Defaulting returnObject to false for consistent behavior

--- src/Hydrates/Inputs/SelectScrollHydrate.php
+++ src/Hydrates/Inputs/SelectScrollHydrate.php
@@ -17,6 +17,7 @@ class SelectScrollHydrate extends InputHydrate
         'itemTitle' => 'name',
         'default' => [],
         'cascadeKey' => 'items',
+        'returnObject' => false,
     ];
 
     /**
