ef9f899a45a6503a0676fa6206b8672616694f9a
OoBook <oguzhan@olmadikprojeler.com>
2025-03-18 15:23:30 +0300

    feat(useGenerate): add utility hook for generating button properties
    - Introduce useGenerate hook to create standardized button properties for action components.
    - Implement generateButtonProps function to handle button attributes based on action configuration, including support for links and conditional rendering of icons and labels.
    - Enhance reusability and maintainability of button components within the application.

---
+++ vue/src/js/hooks/utils/useGenerate.js
@@ -0,0 +1,31 @@
+// hooks/utils/useGenerate.js
+import { computed } from 'vue'
+
+export default function useGenerate(props, context) {
+
+  const generateButtonProps = (action) => {
+
+    let extraProps = {}
+    if(action.href){
+      extraProps['onClick'] = (e) => {
+        e.preventDefault()
+        window.open(action.href, action.target ?? '_blank')
+      }
+    }
+    return {
+      ...(action.componentProps ?? {}),
+      ...extraProps,
+      icon: !action.forceLabel ? action.icon : null,
+      text: action.forceLabel ? action.label : null,
+      color: action.color,
+      variant: action.variant,
+      density: action.density ?? 'comfortable',
+      size: action.size ?? 'default',
+      rounded: action.forceLabel ? null : true,
+    }
+  }
+
+  return {
+    generateButtonProps
+  }
+}
