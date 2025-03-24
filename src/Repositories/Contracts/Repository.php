12299853372e4a3b206381b1a346914cd8f0b6ce
OoBook <oguzhan@olmadikprojeler.com>
2025-03-24 12:15:05 +0300

    feat(Repository): add Repository interface with essential methods
    - Introduce a new Repository interface defining methods for retrieving the like operator, reserved fields, and the model.
    - This addition establishes a contract for repository implementations, enhancing code organization and maintainability.

---
+++ src/Repositories/Contracts/Repository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Unusualify\Modularity\Repositories\Contracts;
+
+interface Repository
+{
+    /**
+     * Get the like operator for the repository.
+     *
+     * @return string
+     */
+    public function getLikeOperator();
+
+    /**
+     * Get the reserved fields for the repository.
+     *
+     * @return array
+     */
+    public function getReservedFields();
+
+    /**
+     * Get the model for the repository.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getModel();
+
+}
