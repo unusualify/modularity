4aba64ceb487a6810513194eed07630b4da404cc
OoBook <oguzhan@olmadikprojeler.com>
2025-03-27 15:41:43 +0300

    refactor(RecursiveStuff): enhance component structure and improve attribute handling
    - Update component rendering logic to include key attributes for better performance.
    - Refactor attribute binding to streamline data handling and improve readability.
    - Introduce error handling for dynamic function calls to enhance robustness.
    - Clean up unused code and comments for improved maintainability.

--- vue/src/js/components/RecursiveStuff.vue
+++ vue/src/js/components/RecursiveStuff.vue
@@ -3,19 +3,23 @@
     {{ configuration.elements }}
   </template>
   <template v-else-if="!configuration.tag && isArray(configuration.elements)">
-
     <component
-    v-for="(element, index) in configuration.elements"
-    :is="element.tag"
-    v-bind="element.attributes"
-    >{{ element.elements }}</component>
-  </template>
-  <component
-    v-else-if="configuration.tag"
-    :is="configuration.tag"
-    v-bind="{...filteredAttributes, ...bindAttributes, ...castedAttributes}"
+      v-for="(element, index) in configuration.elements"
+      :key="`tag-${level}-${index}`"
+      :is="element.tag"
+      v-bind="element.attributes"
     >
-
+      {{ element.elements }}
+    </component>
+  </template>
+  <component v-else-if="configuration.tag"
+      :is="configuration.tag"
+      v-bind="{
+        ...filteredAttributes,
+        ...bindAttributes,
+        ...castedAttributes
+      }"
+    >
     <template v-if="isArray(configuration.elements)">
       <ue-recursive-stuff
         v-for="(_configuration, i) in configuration.elements"
@@ -29,30 +33,26 @@
       {{ applyCasting(configuration.elements) }}
     </template>
 
-
-      <template v-for="(slotConf,slotName) in slots"
-        :key="`tag-${level}-slot-${slotName}`"
-        v-slot:[`${slotName}`]>
-        <!-- {{ $log(slotName, slotConf) }} -->
-          <ue-recursive-stuff
-            :level="level+1"
-            :configuration="slotConf"
-          />
-      </template>
-      <!-- <ue-recursive-stuff
-        v-for="(_configuration, i) in configuration.slots"
-        :key="`tag-${level}-${i}`"
+    <template v-for="(slotConf,slotName) in slots"
+      :key="`tag-${level}-slot-${slotName}`"
+      v-slot:[`${slotName}`]>
+      <ue-recursive-stuff
         :level="level+1"
-        :configuration="_configuration"
-      /> -->
-
-
+        :configuration="slotConf"
+      />
+    </template>
+    <!-- <ue-recursive-stuff
+      v-for="(_configuration, i) in configuration.slots"
+      :key="`tag-${level}-${i}`"
+      :level="level+1"
+      :configuration="_configuration"
+    /> -->
   </component>
 </template>
 
 <script>
-import { reduce, get, cloneDeep } from 'lodash-es'
 import { computed, ref } from 'vue'
+import { reduce, get, cloneDeep, isArray, isString, isNumber } from 'lodash-es'
 
 export default {
   props: {
@@ -69,9 +69,7 @@ export default {
     bindData: {
       type: Object,
       default () {
-        return {
-
-        }
+        return {}
       }
     },
   },
@@ -87,16 +85,19 @@ export default {
         return props.configuration.slots
       else
         return {}
-      })
+    })
+
     function isTextable (value) {
-      return __isString(value) || __isNumber(value)
+      return isString(value) || isNumber(value)
     }
-    function isArray (value) {
-      return Array.isArray(value, (value))
-    }
+    // function isArray (value) {
+    //   return Array.isArray(value, (value))
+    // }
+
     function hasSlot(value) {
       return value.hasOwnProperty('slots');
     }
+
     function isObject (value) {
       return __isObject(value)
     }
@@ -108,18 +109,27 @@ export default {
 
       if (matches) {
         let result = get(props.bindData, matches[1])
-        funcs.forEach((func) => {
-          result = window[func](result)
-        })
+
+        if(result !== undefined) {
+          try {
+            funcs.forEach((func) => {
+              if(window[func] && typeof window[func] === 'function')
+                result = window[func](result)
+            })
+          } catch (e) {
+            console.error(e)
+          }
+        }
         return result
       }
       return value
     }
+
     function castAttribute(object, key, value) {
       let _value = value
       let funcs = []
 
-      if (isArray(value) && __isString(value[0])) {
+      if (isArray(value) && isString(value[0])) {
         _value = value.shift()
         funcs = value
       }
@@ -134,12 +144,15 @@ export default {
         }
       }
     }
-    const castPattern = /\$([\w|.]+)/
+
+    const castPattern = /\$([\w|.|\_|\-]+)/
+
     const filteredAttributes = cloneDeep(props.configuration.attributes)
+
     const castedAttributes = computed(() => {
       const attrs = cloneDeep(props.configuration.attributes)
       return reduce(attrs, (o, v, k) => {
-        if (!(isArray(v) || __isString(v) || __isObject(v))) {
+        if (!(isArray(v) || isString(v) || isObject(v))) {
           return o
         }
 
@@ -148,26 +161,58 @@ export default {
         return o
       }, {})
     })
-    const bindAttributes = computed(() => {
-      const bindKey = props.configuration.bind
-      if (bindKey) {
-        const matches = bindKey.match(castPattern)
 
-        if (matches && !!props.bindData) {
-          const key = matches[1]
-          if (Object.prototype.hasOwnProperty.call(props.bindData, key)) {
-            return __data_get(props.bindData, key)
+    const bindAttributes = computed(() => {
+      const bindData = props.bindData
+      let boundAttributes = {}
+
+      if(!!bindData && isObject(bindData)) {
+
+        let configurationBindKeys = props.configuration.bind
+
+        if (configurationBindKeys) {
+
+          if(isString(configurationBindKeys)) {
+            configurationBindKeys = [configurationBindKeys]
           }
+
+          if(isArray(configurationBindKeys)) {
+
+            if (!!bindData) {
+
+              configurationBindKeys.forEach((key) => {
+                let attributeName = key
+                let matches = key.match(castPattern)
+
+                if(matches) {
+                  attributeName = matches[1]
+                }
+
+                let boundAttribute
+                if (Object.prototype.hasOwnProperty.call(bindData, attributeName)) {
+                  boundAttribute = __data_get(bindData, attributeName, undefined)
+                }else if (Object.prototype.hasOwnProperty.call(bindData, key)) {
+                  boundAttribute = __data_get(bindData, key, undefined)
+                }
+
+                if(boundAttribute !== undefined) {
+                  boundAttributes = {...boundAttributes, ...boundAttribute}
+                }
+              })
+            }
+          }
+
         }
       }
 
-      return {}
+      return boundAttributes
     })
 
     return {
       // directives: props.configuration.directives ? props.configuration.directives.map((v) => resolveDirective(v)) : [],
       isTextable,
       isArray,
+
       applyCasting,
       hasSlot,
       filteredAttributes,
