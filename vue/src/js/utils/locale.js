e7a8a1f6be0ebe9dff02208a3ec756e965436f67
OoBook <oguzhan@olmadikprojeler.com>
2025-03-29 16:13:08 +0300

    refactor(locale.js): simplify locale management by removing hardcoded translations
    - Replace hardcoded locale definitions with a dynamic function to retrieve available languages.
    - Utilize lodash's map function for improved code efficiency and readability.

--- vue/src/js/utils/locale.js
+++ vue/src/js/utils/locale.js
@@ -1,49 +1,9 @@
-import { Mandarin } from 'flatpickr/dist/l10n/zh.js'
-import { Russian } from 'flatpickr/dist/l10n/ru.js'
-import { French } from 'flatpickr/dist/l10n/fr.js'
-import { Polish } from 'flatpickr/dist/l10n/pl.js'
-import { German } from 'flatpickr/dist/l10n/de.js'
-import { Dutch } from 'flatpickr/dist/l10n/nl.js'
-import { Portuguese } from 'flatpickr/dist/l10n/pt.js'
-import { Turkish } from 'flatpickr/dist/l10n/tr.js'
+import { map } from 'lodash-es'
 
-export const locales = {
-  en: {
-    'date-fns': require('date-fns/locale/en')
-  },
-  'zh-Hans': {
-    'date-fns': require('date-fns/locale/zh_cn'),
-    flatpickr: Mandarin
-  },
-  ru: {
-    'date-fns': require('date-fns/locale/ru'),
-    flatpickr: Russian
-  },
-  fr: {
-    'date-fns': require('date-fns/locale/fr'),
-    flatpickr: French
-  },
-  pl: {
-    'date-fns': require('date-fns/locale/pl'),
-    flatpickr: Polish
-  },
-  de: {
-    'date-fns': require('date-fns/locale/de'),
-    flatpickr: German
-  },
-  nl: {
-    'date-fns': require('date-fns/locale/nl'),
-    flatpickr: Dutch
-  },
-  pt: {
-    'date-fns': require('date-fns/locale/pt'),
-    flatpickr: Portuguese
-  },
-  tr: {
-    'date-fns': require('date-fns/locale/tr'),
-    flatpickr: Turkish
-  }
+export const getTranslationLanguages = (input) => {
+  return map(window[import.meta.env.VUE_APP_NAME].STORE.languages.all, 'value')
 }
+
 
 export function getCurrentLocale () {
   return window[import.meta.env.VUE_APP_NAME].LOCALE
