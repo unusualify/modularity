bb77a92f84cff33049800a5003ba3d60e38ed446
OoBook <oguzhan@olmadikprojeler.com>
2025-04-04 15:20:59 +0300

    refactor(cache.js): update cache mutation constants for consistency
    - Rename cache mutation constants to follow a more consistent naming convention.
    - Improve code clarity and maintainability by standardizing the format of constant names.

--- vue/src/js/store/mutations/cache.js
+++ vue/src/js/store/mutations/cache.js
@@ -1,9 +1,9 @@
 /* Cache */
-export const GET_CACHE = '__getCache'
-export const PUT_CACHE = '__putCache'
-export const PUSH_CACHE = '__pushCache'
-export const GET_LAST_CACHE = '__getLastCache'
-export const FORGET_CACHE = '__forgetCache'
+export const GET_CACHE = 'getCache'
+export const PUT_CACHE = 'putCache'
+export const PUSH_CACHE = 'pushCache'
+export const GET_LAST_CACHE = 'getLastCache'
+export const FORGET_CACHE = 'forgetCache'
 
 export default {
   GET_CACHE,
