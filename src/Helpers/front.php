e8a2fd7add5cd04551c373913eb9ab4e37af5f53
OoBook <oguzhan@olmadikprojeler.com>
2025-04-04 12:16:59 +0300

    feat(front.php): add getModularityDefaultUrls function for modularity URL retrieval
    - Introduce the getModularityDefaultUrls function to provide default URLs for modularity, including language routes and base permalinks.
    - Utilize Laravel's Route and Arr facades for improved URL handling and array manipulation.
    - Enhance modularity support by dynamically generating URLs based on application configuration.

--- src/Helpers/front.php
+++ src/Helpers/front.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
 if (! function_exists('getHost')) {
     /**
      * @param string $file
@@ -8,5 +11,24 @@ if (! function_exists('getHost')) {
     function getHost()
     {
         return parse_url(config('app.url'))['host'];
+    }
+}
+
+if (! function_exists('getModularityDefaultUrls')) {
+    /**
+     * Get the default urls for the modularity
+     *
+     * @return array
+     */
+    function getModularityDefaultUrls()
+    {
+        return [
+            'languages' => route(Route::hasAdmin('api.languages.index')),
+            'base_permalinks' => Arr::mapWithKeys(getLocales(), function ($locale, $key) {
+                extract(parse_url(config('app.url'))); // $scheme, $host
+
+                return [$locale => $host];
+            }),
+        ];
     }
 }
