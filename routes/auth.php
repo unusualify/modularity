a5ca2c96a17a8d8cc3876f33ac5fa2b61627fb34
celikerde <celikerde@itu.edu.tr>
2025-04-07 14:38:52 +0300

    feat: add OAuth routes for linking providers and handling callbacks

--- routes/auth.php
+++ routes/auth.php
@@ -26,6 +26,19 @@ if (modularityConfig('enabled.users-management')) {
     Route::post('login', 'LoginController@login')->name('login');
     Route::post('logout', 'LoginController@logout')->name('logout');
 
+    Route::get('login/oauth', 'LoginController@showPasswordForm')->name('login.oauth.showPasswordForm');
+    Route::post('login/oauth', 'LoginController@linkProvider')->name('login.oauth.linkProvider');
+
+
+    Route::get('/auth/{provider}/redirect', 'LoginController@redirectToProvider')->name('login.provider');
+    Route::get('/auth/{provider}/callback', 'LoginController@handleProviderCallback')->name('loginHandleCallbackProvider');
+
+    // #TODO add complete registration after email confirmatiosent
+    // Route::get('/completeRegistration', 'LoginController@completeRegisterForm')->name('completeRegistration.form');
+    // Route::post('/completeRegistration', 'LoginController@completeRegister')->name('completeRegistration');
+
+    //Route::get('/withoutLogin', 'LoginController@completeRegisterForm');
+
     Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.reset.link');
     Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.reset.email');
     Route::get('password/reset/success', 'ResetPasswordController@success')->name('password.reset.success');
