d81078fbdd57db9cdec9e2ebf30dd6843d9eab99
OoBook <oguzhan@olmadikprojeler.com>
2025-04-08 12:26:40 +0300

    feat(Metric.vue): add Metric component for displaying values with customizable styles
    - Introduce a new Metric component that accepts props for value, label, and various styling options.
    - Implement computed properties for dynamic class generation based on props, enhancing flexibility in presentation.
    - Include a scoped style for smooth transitions, improving the user interface experience.

---
+++ vue/src/js/components/Metric.vue
@@ -0,0 +1,102 @@
+<script setup>
+  import { computed } from 'vue';
+
+  const props = defineProps({
+    value: {
+      type: [Number, String],
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: null,
+    },
+    cardColor: {
+      type: String,
+      default: null,
+    },
+    labelColor: {
+      type: String,
+      default: null,
+    },
+    valueClass: {
+      type: String,
+      default: '',
+    },
+    labelClass: {
+      type: String,
+      default: '',
+    },
+    dense: {
+      type: Boolean,
+      default: false,
+    },
+    noInline: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+      default: null,
+    },
+  });
+
+  // Compute classes based on provided props
+  const cardClasses = computed(() => {
+    return {
+      'd-inline-block': !props.noInline,
+      'metric-card': true,
+      'text-center': true,
+      'py-1 px-2': props.dense,
+      'py-2 px-4': !props.dense
+    };
+  });
+
+  const valueClasses = computed(() => {
+    return [
+      'font-weight-bold',
+      props.dense ? 'text-h5' : 'text-h4',
+      props.color ? `text-${props.color}` : '',
+      props.valueClass,
+    ];
+  });
+
+  const labelClasses = computed(() => {
+    return [
+      props.color ? `text-${props.color}` : '',
+      props.labelColor ? `text-${props.labelColor}` : '',
+      props.dense ? 'text-subtitle-2' : 'text-subtitle',
+      props.labelClass
+    ];
+  });
+
+</script>
+
+<template>
+  <v-card
+    :color="cardColor"
+    :class="cardClasses"
+  >
+    <v-card-text>
+      <div :class="valueClasses">
+        {{ value }}
+      </div>
+      <div :class="labelClasses">
+        <v-icon
+          v-if="icon"
+          :icon="icon"
+
+        />
+        {{ label }}
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped lang="sass">
+  .metric-card
+    transition: all 0.2s
+</style>
