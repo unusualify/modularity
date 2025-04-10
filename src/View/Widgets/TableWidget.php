1761572fa555711c97c8892b631b5188989dced7
OoBook <oguzhan@olmadikprojeler.com>
2025-04-10 23:43:48 +0300

    feat(TableWidget.php): introduce a new widget for displaying tabular data
    - Add the `TableWidget` class to manage and render tables in a modular view.
    - Define default attributes and layout configurations, enhancing usability and appearance.
    - Implement the `hydrateAttributes` method to process table data and integrate dynamic endpoints, improving data handling within the widget.

---
+++ src/View/Widgets/TableWidget.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Unusualify\Modularity\View\Widgets;
+
+use Unusualify\Modularity\Module;
+use Unusualify\Modularity\View\ModularityWidget;
+use Unusualify\Modularity\Traits\Allowable;
+class TableWidget extends ModularityWidget
+{
+    use Allowable;
+
+    public $tag = 'ue-table';
+
+    public $widgetTag = 'v-col';
+
+    public $widgetCol = [
+        'cols' => 12,
+        'lg' => 6,
+        'xl' => 4,
+    ];
+
+    public $attributes = [
+        'class' => 'h-100',
+
+        'createOnModal' => false,
+        'editOnModal' => false,
+
+        'fullWidthWrapper' => true,
+        'fillHeight' => false,
+
+        'hideHeaders' => true,
+        'hideBorderRow' => true,
+        'hideDefaultFooter' => false,
+        'hideSearchField' => true,
+
+        'rowActionsType' => 'dropdown',
+        'roundedRows' => true,
+
+        'showSelect' => false,
+        'style' => '',
+        'class' => 'elevation-2',
+
+        'tableOptions' => [
+            'page' => 1,
+            'sortBy' => [],
+            'search' => null,
+            'groupBy' => [],
+            'itemsPerPage' => 5,
+            // 'tableType' => 'dashboard',
+        ],
+        'noFullScreen' => true,
+
+        'tableClasses' => 'elevation-2',
+        'toolbarOptions' => [
+            'color' => 'transparent', // rgb(255,255,255,1) or utility colors like white, purple
+            'border' => false, // false, 'xs', 'sm', 'md', 'lg', 'xl'.
+            'rounded' => false, // This can be 0, xs, sm, true, lg, xl, pill, circle, and shaped. string | number | boolean
+            'collapse' => false, // false, true,
+            'density' => 'default', // prominent, comfortable, compact, default
+            'elevation' => 0, // string or number refers to elevation
+            'image' => '', // image link for the background of the toolbar
+            // 'height' => '90',
+        ],
+    ];
+
+
+    public function hydrateAttributes($attributes)
+    {
+        $attributes = parent::hydrateAttributes($attributes);
+
+        $attributes = array_merge_recursive_preserve(
+            modularityConfig('default_table_attributes'),
+            $attributes
+        );
+
+        if (isset($attributes['_routeName']) && isset($attributes['_module']) && $attributes['_module'] instanceof Module) {
+
+            $routeEndpoints = $attributes['_module']->getRouteMainUris(
+                routeName: $attributes['_routeName'],
+                withoutNamePrefix: true,
+                modelBindingValue: ':id'
+            );
+
+            $attributes['endpoints'] = array_merge(
+                getModularityDefaultUrls(),
+                $routeEndpoints,
+            );
+        }
+
+        if(isset($attributes['columns'])) {
+            $newColumns = $this->getAllowableItems(
+                $attributes['columns'],
+                searchKey: 'allowedRoles',
+                orClosure: fn($item, $user) => $user->isSuperAdmin(),
+            );
+
+            $newColumns = configure_table_columns($newColumns);
+
+            $attributes['columns'] = hydrate_table_columns_translations($newColumns);
+        }
+
+        return $attributes;
+    }
+
+}
