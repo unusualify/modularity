05b6d15be05686fbae4f720f7093d993995cdb8d
OoBook <oguzhan@olmadikprojeler.com>
2025-04-10 23:43:37 +0300

    feat(BoardInformationWidget.php): add a new widget for displaying board information
    - Introduce the `BoardInformationWidget` class to manage and render board information in a modular view.
    - Define default attributes and layout configurations for the widget, enhancing its usability and appearance.
    - Implement the `hydrateAttributes` method to process card data and integrate connectors, improving data handling within the widget.

---
+++ src/View/Widgets/BoardInformationWidget.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Unusualify\Modularity\View\Widgets;
+
+use Illuminate\Support\Arr;
+use Unusualify\Modularity\View\ModularityWidget;
+
+class BoardInformationWidget extends ModularityWidget
+{
+
+    public $tag = 'ue-board-information-plus';
+
+    public $widgetTag = 'v-col';
+
+    public $widgetCol = [
+        'cols' => 12,
+        'lg' => 6,
+        'xl' => 4,
+    ];
+
+    public $attributes = [
+        'class' => 'elevation-2',
+        'container' => [
+            'color' => '',
+            'elevation' => 2,
+            'class' => 'h-100',
+        ],
+        'cardAttribute' => [
+            'variant' => 'outlined',
+            'borderRadius' => '14px',
+            'border' => 'sm',
+            'borderColor' => 'rgb(var(--v-theme-primary))',
+            'titleClass' => 'text-subtitle-2',
+            'titleColor' => 'grey',
+            'infoClass' => 'text-h4 pa-0',
+            'infoColor' => 'text-primary',
+            'class' => 'px-4 py-6 h-100',
+            'infoLineHeight' => '1',
+            'infoFontWeight' => '700',
+        ],
+    ];
+
+    public function hydrateAttributes($attributes)
+    {
+        $attributes = parent::hydrateAttributes($attributes);
+
+        if(isset($attributes['cards'])) {
+            $cards = [];
+            foreach($attributes['cards'] as $card) {
+                if(is_array($card) && Arr::isAssoc($card) && isset($card['connector'])) {
+                    $data = init_connector($card['connector']);
+                    $card['data'] = $data;
+                    $cards[] = $card;
+                }
+            }
+            $attributes['cards'] = $cards;
+        }
+
+        return $attributes;
+    }
+
+}
