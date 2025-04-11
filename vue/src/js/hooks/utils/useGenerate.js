4813f6c7adb89e4cd0a8e8efd2775ecb66668ed6
OoBook <oguzhan@olmadikprojeler.com>
2025-04-11 18:44:41 +0300

    refactor(useGenerate.js): enhance button properties generation with responsive support
    - Introduce computed property generatedButtonProps to dynamically adjust button properties based on screen size using Vuetify's useDisplay.
    - Update generateButtonProps to ensure consistent handling of icon and density properties, improving the overall button configuration logic.

--- vue/src/js/hooks/utils/useGenerate.js
+++ vue/src/js/hooks/utils/useGenerate.js
@@ -1,17 +1,46 @@
 // hooks/utils/useGenerate.js
 import { computed } from 'vue'
+import { useDisplay } from 'vuetify'
 
 export default function useGenerate(props, context) {
+
+  const { smAndUp } = useDisplay()
+
+  const generatedButtonProps = computed(() => {
+
+    if(!props)
+      return {}
+
+    const defaultButtonProps = generateButtonProps(props)
+    const hasIcon = defaultButtonProps.icon
+      || defaultButtonProps.prependIcon
+      || defaultButtonProps.appendIcon
+      || props.icon
+      || props.prependIcon
+      || props.appendIcon
+
+    return {
+      ...defaultButtonProps,
+
+      // size: props.size ?? 'default',
+      // rounded: props.forceLabel ? null : true,
+      icon: hasIcon && !smAndUp.value ? hasIcon : defaultButtonProps.icon,
+      density: (hasIcon && !smAndUp.value) ? 'compact' : (props.density ?? 'comfortable'),
+      rounded: hasIcon ? false : (defaultButtonProps.rounded ?? false),
+    }
+  })
 
   const generateButtonProps = (action) => {
 
     let extraProps = {}
+
     if(action.href){
       extraProps['onClick'] = (e) => {
         e.preventDefault()
         window.open(action.href, action.target ?? '_blank')
       }
     }
+
     return {
       ...(action.componentProps ?? {}),
       ...extraProps,
@@ -22,11 +51,12 @@ export default function useGenerate(props, context) {
       density: action.density ?? 'comfortable',
       size: action.size ?? 'default',
       disabled: action.disabled ?? action.componentProps?.disabled ?? false,
-      rounded: action.forceLabel ? null : true,
+      rounded: action.forceLabel ? null : false,
     }
   }
 
   return {
-    generateButtonProps
+    generateButtonProps,
+    generatedButtonProps
   }
 }
