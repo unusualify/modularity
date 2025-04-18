501075902ef1606053e234066fc9cf70d4baf2c3
OoBook <oguzhan@olmadikprojeler.com>
2025-04-18 18:17:49 +0300

    feat(AssignmentStatus.php): introduce AssignmentStatus enum for task management
    - Create a new enum, AssignmentStatus, to represent various states of assignments: COMPLETED, PENDING, REJECTED, and CANCELLED.
    - Implement methods for label, color, iconColor, icon, timeIntervalDescription, and timeClasses to enhance the representation and handling of assignment statuses in the application.

---
+++ src/Entities/Enums/AssignmentStatus.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Unusualify\Modularity\Entities\Enums;
+
+enum AssignmentStatus: string
+{
+    case COMPLETED = 'completed';
+    case PENDING = 'pending';
+    case REJECTED = 'rejected';
+    case CANCELLED = 'cancelled';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::COMPLETED => __('Completed'),
+            self::PENDING => __('Pending'),
+            self::REJECTED => __('Rejected'),
+            self::CANCELLED => __('Cancelled'),
+        };
+    }
+
+    public function color(): string
+    {
+        return match($this) {
+            self::COMPLETED => 'text-success',
+            self::PENDING => 'text-warning',
+            self::REJECTED => 'text-error',
+            self::CANCELLED => 'text-grey',
+        };
+    }
+
+    public function iconColor(): string
+    {
+        return match($this) {
+            self::COMPLETED => 'success',
+            self::PENDING => 'info',
+            self::REJECTED => 'error',
+            self::CANCELLED => 'grey',
+        };
+    }
+
+    public function icon(): string
+    {
+        return match($this) {
+            self::COMPLETED => 'mdi-check-circle-outline',
+            self::PENDING => 'mdi-clock-outline',
+            self::REJECTED => 'mdi-close-circle-outline',
+            self::CANCELLED => 'mdi-close-circle-outline',
+        };
+    }
+
+    public function timeIntervalDescription(): string
+    {
+        return match($this) {
+            self::PENDING => __('Until'),
+            self::REJECTED => __('Rejected'),
+            self::CANCELLED => __('Cancelled'),
+            self::COMPLETED => __('Completed'),
+        };
+    }
+
+    public function timeClasses(): string
+    {
+        return match($this) {
+            self::PENDING => 'font-weight-bold text-blue-darken-1',
+            self::REJECTED => 'font-weight-bold text-error',
+            self::CANCELLED => 'font-weight-bold text-warning',
+            self::COMPLETED => 'font-weight-bold text-success',
+        };
+    }
+
+}
