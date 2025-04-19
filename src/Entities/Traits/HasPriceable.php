32b1a162b75ab1b5651e898cea08d10788ddf0a1
OoBook <oguzhan@olmadikprojeler.com>
2025-04-19 12:49:14 +0300

    feat(HasPriceable.php): enhance pricing attributes and methods
    - Introduce new attributes for price calculations, including vat_percentage, vat_amount, has_discount, raw_discount, sub_total, and total_discount.
    - Update existing methods to reflect new attribute names and improve price retrieval logic.
    - Add new methods for formatted price outputs, enhancing the display of pricing information.
    - Implement query scopes for ordering by currency price and base price, improving query flexibility.

--- src/Entities/Traits/HasPriceable.php
+++ src/Entities/Traits/HasPriceable.php
@@ -28,10 +28,24 @@ trait HasPriceable
     {
         $this->append(
             'base_price_raw', // price excluding vat
-            'base_price_total', // price including vat
+            'base_price_vat_percentage', // price vat percentage
+            'base_price_vat_amount', // price vat amount
+            'base_price_has_discount', // price has discount
+            'base_price_raw_discount', // price raw discount
+            'base_price_sub_total', // price sub total
+            'base_price_total_discount', // price total discount
+            'base_price_total', // price total
+
             'base_price_formatted', // price excluding vat formatted (+ VAT)
-            'base_price_without_vat_formatted', // price excluding vat formatted
-            'base_price_total_formatted' // price including vat formatted
+
+            'base_price_raw_formatted', // price excluding vat formatted
+            'base_price_vat_percentage_formatted', // price vat percentage formatted
+            'base_price_vat_amount_formatted', // price vat amount formatted
+            'base_price_discount_percentage_formatted', // price discount percentage formatted
+            'base_price_raw_discount_formatted', // price raw discount formatted
+            'base_price_sub_total_formatted', // price sub total formatted
+            'base_price_total_discount_formatted', // price total discount formatted
+            'base_price_total_formatted' // price total formatted
         );
     }
 
@@ -51,14 +65,57 @@ trait HasPriceable
     protected function basePriceRaw(): Attribute
     {
         return Attribute::make(
-            get: fn ($value) => $this->basePrice ? $this->basePrice->price_excluding_vat : null,
+            // get: fn ($value) => $this->basePrice ? $this->basePrice->price_excluding_vat : null,
+            get: fn ($value) => $this->basePrice ? $this->basePrice->raw_price : null,
+        );
+    }
+
+    protected function basePriceVatPercentage(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->basePrice ? $this->basePrice->vat_percentage : null,
+        );
+    }
+
+    protected function basePriceVatAmount(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->basePrice ? $this->basePrice->vat_amount : null,
+        );
+    }
+
+    protected function basePriceHasDiscount(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->basePrice ? $this->basePrice->has_discount : false,
+        );
+    }
+
+    protected function basePriceRawDiscount(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->basePrice ? $this->basePrice->raw_discount : null,
+        );
+    }
+
+    protected function basePriceSubTotal(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->basePrice ? $this->basePrice->sub_total_price : null,
+        );
+    }
+
+    protected function basePriceTotalDiscount(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->basePrice ? $this->basePrice->total_discount : null,
         );
     }
 
     protected function basePriceTotal(): Attribute
     {
         return Attribute::make(
-            get: fn ($value) => $this->basePrice ? $this->basePrice->price_including_vat : null,
+            get: fn ($value) => $this->basePrice ? $this->basePrice->total_price : null,
         );
     }
 
@@ -71,11 +128,61 @@ trait HasPriceable
         );
     }
 
-    protected function basePriceWithoutVatFormatted(): Attribute
+    protected function basePriceRawFormatted(): Attribute
     {
         return Attribute::make(
             get: fn ($value) => $this->base_price_raw
                 ? \Oobook\Priceable\Facades\PriceService::formatAmount($this->base_price_raw)
+                : null,
+        );
+    }
+
+    protected function basePriceVatPercentageFormatted(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->basePrice && $this->basePrice->vat_percentage > 0 ? $this->basePrice->vat_percentage . '%' : '',
+        );
+    }
+
+    protected function basePriceVatAmountFormatted(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->base_price_vat_amount
+                ? \Oobook\Priceable\Facades\PriceService::formatAmount($this->base_price_vat_amount)
+                : null,
+        );
+    }
+
+    protected function basePriceDiscountPercentageFormatted(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->basePrice && $this->basePrice->discount_percentage > 0 ? $this->basePrice->discount_percentage . '%' : '',
+        );
+    }
+
+    protected function basePriceRawDiscountFormatted(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->base_price_raw_discount
+                ? \Oobook\Priceable\Facades\PriceService::formatAmount($this->base_price_raw_discount)
+                : null,
+        );
+    }
+
+    protected function basePriceSubTotalFormatted(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->base_price_sub_total
+                ? \Oobook\Priceable\Facades\PriceService::formatAmount($this->base_price_sub_total)
+                : null,
+        );
+    }
+
+    protected function basePriceTotalDiscountFormatted(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->base_price_total_discount
+                ? \Oobook\Priceable\Facades\PriceService::formatAmount($this->base_price_total_discount)
                 : null,
         );
     }
