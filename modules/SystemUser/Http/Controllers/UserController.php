d319cd53c7547965774ad3ffb5ca6fc4c8a98542
OoBook <oguzhan@olmadikprojeler.com>
2025-04-21 17:31:51 +0300

    feat(UserController.php): implement user creation with password reset notification
    - Added a new store method to handle user creation and send a password reset link upon successful registration.
    - Integrated session management for retaining form data and improved user redirection based on save type.
    - Enhanced user experience by providing appropriate success messages and redirecting to relevant routes after creation.

--- modules/SystemUser/Http/Controllers/UserController.php
+++ modules/SystemUser/Http/Controllers/UserController.php
@@ -3,6 +3,10 @@
 namespace Modules\SystemUser\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Session;
+use Unusualify\Modularity\Facades\Modularity;
 use Unusualify\Modularity\Http\Controllers\BaseController;
 
 class UserController extends BaseController
@@ -42,6 +46,60 @@ class UserController extends BaseController
             $app,
             $request
         );
+    }
+
+    public function store($parentId = null)
+    {
+        $this->formSchema = $this->createFormSchema($this->getConfigFieldsByRoute('inputs'));
+
+        $this->addWiths();
+
+        $this->addFormWiths();
+
+        $input = $this->validateFormRequest()->all();
+
+        $optionalParent = $this->nestedParentScopes();
+
+        $item = $this->repository->create($input + $optionalParent, $this->getPreviousRouteSchema());
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = Password::broker(Modularity::getAuthProviderName())->sendResetLink(
+            // $this->request->only('email'),
+            ['email' => $item->email],
+            function ($user, $token) {
+                // dd($user, $token, Password::RESET_LINK_SENT);
+
+                $user->sendGeneratePasswordNotification($token);
+
+                return Password::RESET_LINK_SENT;
+            }
+        );
+
+        activity()->performedOn($item)->log('created');
+
+        Session::put($this->routeName . '_retain', true);
+
+        if (isset($input['cmsSaveType']) && Str::endsWith($input['cmsSaveType'], '-close')) {
+            return $this->respondWithRedirect($this->getBackLink());
+        }
+
+        if (isset($input['cmsSaveType']) && Str::endsWith($input['cmsSaveType'], '-new')) {
+            return $this->respondWithRedirect(moduleRoute($this->routeName,
+                $this->routePrefix,
+                'create'
+            ));
+        }
+
+        return $this->request->ajax()
+            ? $this->respondWithSuccess(___('messages.save-success'))
+            : $this->respondWithRedirect(moduleRoute($this->routeName,
+                $this->routePrefix,
+                'edit',
+                [Str::singular(last(explode('.', $this->moduleName))) => $this->getItemIdentifier($item)]
+            ));
+
     }
 
     public function destroy($id, $submoduleId = null)
