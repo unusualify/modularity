8f2d97a9893ad35b599a80f5d7a51c04e91c7ded
OoBook <oguzhan@olmadikprojeler.com>
2025-04-21 17:32:47 +0300

    refactor(AuthorizableTrait.php): comment out hasAuthorization scope setting
    - Commented out the line that sets the 'hasAuthorization' scope in the filterAuthorizableTrait method for clarity and potential future use.

--- src/Repositories/Traits/AuthorizableTrait.php
+++ src/Repositories/Traits/AuthorizableTrait.php
@@ -12,6 +12,6 @@ trait AuthorizableTrait
     public function filterAuthorizableTrait($query, &$scopes)
     {
         // set, cast, unset or manipulate the scopes by using query and scopes
-        $scopes['hasAuthorization'] = true;
+        // $scopes['hasAuthorization'] = true;
     }
 }
