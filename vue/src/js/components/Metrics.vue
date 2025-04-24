66d74b9cff4642a5a224b4672b8e48c1a0c5b229
OoBook <oguzhan@olmadikprojeler.com>
2025-04-24 09:20:26 +0300

    feat(Metrics.vue): enhance metrics component with new props and functionality
    - Added `rowClass`, `metricWidth`, `minMetricWidth`, and `metricAttributes` props for improved customization of metrics display.
    - Introduced reactive state management for metrics and date range selection, enabling dynamic updates and user interaction.
    - Updated template structure to utilize new props and enhance layout flexibility, including a new date input for filtering metrics.
    - Refactored computed properties for better class management and streamlined metric rendering.

--- vue/src/js/components/Metrics.vue
+++ vue/src/js/components/Metrics.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { computed } from 'vue';
+  import { computed, ref, watch } from 'vue';
 
   const props = defineProps({
     title: {
@@ -26,6 +26,10 @@
       type: String,
       default: null
     },
+    rowClass: {
+      type: String,
+      default: null
+    },
     noInline: {
       type: Boolean,
       default: false
@@ -39,48 +43,38 @@
       default: null
     },
 
-    metricColor: {
-      type: String,
+    metricWidth: {
+      type: [String, Number],
       default: null
     },
-    metricCardColor: {
+    minMetricWidth: {
+      type: [String, Number],
+      default: 130
+    },
+
+    metricAttributes: {
+      type: Object,
+      default: () => ({})
+    },
+
+    endpoint: {
       type: String,
       default: null
-    },
-    metricLabelColor: {
-      type: String,
-      default: null
-    },
-    metricValueClass: {
-      type: String,
-      default: null
-    },
-    metricLabelClass: {
-      type: String,
-      default: null
-    },
-    metricNoInline: {
-      type: Boolean,
-      default: false
     }
   });
 
   const cardClasses = computed(() => {
     return [
-      'ue-metrics',
       !props.noInline ? 'd-inline-block' : '',
       'rounded-lg',
-      'overflow-hidden',
+      // 'overflow-hidden',
     ];
   });
 
   const headerClasses = computed(() => {
     return [
-      'ue-metrics__header',
       props.bgHeaderColor ? `bg-${props.bgHeaderColor}` : '',
       'd-flex justify-space-between align-center pa-4',
-      'rounded-t-lg',
-      'border-s-sm border-e-sm border-t-sm'
     ];
   });
 
@@ -95,22 +89,51 @@
   const filterClasses = computed(() => {
     return [
       props.filterColor ? `text-${props.filterColor}` : '',
-      'text-body-2',
-      'text-medium-emphasis'
+      // 'text-body-2 text-medium-emphasis',
+      'd-flex align-center'
+    ];
+  });
+
+  const rowClasses = computed(() => {
+    return [
+      'd-flex flex-wrap ga-5',
+      props.rowClass ? props.rowClass : ''
     ];
   });
 
   const defaultMetricAttributes = computed(() => {
     return {
-      ...(props.metricColor ? {color: props.metricColor} : {}),
-      ...(props.metricCardColor ? {cardColor: props.metricCardColor} : {}),
-      ...(props.metricLabelColor ? {labelColor: props.metricLabelColor} : {}),
-      ...(props.metricValueClass ? {valueClass: props.metricValueClass} : {}),
-      ...(props.metricLabelClass ? {labelClass: props.metricLabelClass} : {}),
-      ...(props.metricNoInline ? {noInline: props.metricNoInline} : {}),
+      ...(props.metricAttributes ?? {})
     };
   });
 
+  const metrics = ref(props.items);
+
+  const dateRangeModel = ref(null);
+  const dateRangeLoading = ref(false);
+
+  const refreshMetrics = () => {
+    dateRangeLoading.value = true;
+
+    axios.post(props.endpoint, {
+      date_range: dateRangeModel.value,
+      items: metrics.value
+    }).then(response => {
+      console.log(response.data);
+      if(response.data.variant === 'success') {
+        metrics.value = response.data.data;
+      }
+    }).catch(error => {
+      console.log(error);
+    }).finally(() => {
+      dateRangeLoading.value = false;
+    });
+  }
+  watch(dateRangeModel, (newValue, oldValue) => {
+    if(Array.isArray(newValue) && newValue.length > 1) {
+      refreshMetrics();
+    }
+  });
   // __log(defaultMetricAttributes.value);
 </script>
 
@@ -123,61 +146,60 @@
     <!-- Header with title and date -->
     <v-card-title :class="headerClasses">
       <div :class="titleClasses">{{ title }}</div>
-      <div v-if="date" :class="filterClasses">
-        {{ dateLabel }} {{ date }}
+      <div :class="filterClasses">
+        <v-date-input v-if="endpoint"
+          v-model="dateRangeModel"
+          :Xlabel="$t('')"
+          :validate-on="`submit blur`"
+          variant="outlined"
+          density="compact"
+          prepend-icon=""
+          append-inner-icon="$calendar"
+          persistent-placeholder
+          show-adjacent-months
+          required
+          hide-details
+          multiple="range"
+
+          :disabled="dateRangeLoading"
+
+          width="300"
+        >
+          <!-- <template v-slot:actions="{ save, cancel, isPristine }">
+            sss
+          </template> -->
+        </v-date-input>
+        <!-- {{ dateLabel }} {{ date }} -->
       </div>
     </v-card-title>
 
-    <!-- Metrics row -->
-    <div class="d-flex flex-wrap ue-metrics__row h-100">
-      <template
-        v-for="(metric, index) in items"
-        :key="index"
-        >
-        <ue-metric
-          :class="[
-            'ue-metrics__metric',
-            'flex-grow-1',
-          ]"
-          :style="{ width: `${100 / items.length}%`, minWidth: '120px' }"
-          elevation="0"
-          rounded="0"
-          dense
-          no-inline
-          v-bind="{
-            ...defaultMetricAttributes,
-            ...metric
-          }"
-        />
-      </template>
-    </div>
+    <v-card-text>
+      <!-- Metrics row -->
+      <div :class="rowClasses">
+        <template
+          v-for="(metric, index) in metrics"
+          :key="index"
+          >
+          <ue-metric
+            :style="[
+              metricWidth ? `width: ${metricWidth}` : '',
+              minMetricWidth ? `min-width: ${minMetricWidth}px` : '',
+            ]"
+
+            dense
+            no-inline
+            v-bind="{
+              ...defaultMetricAttributes,
+              ...metric
+            }"
+          />
+        </template>
+      </div>
+    </v-card-text>
+
   </v-card>
 </template>
 
 <style scoped lang="scss">
-  $border-width: 1px;
-  $border-color: rgba(0, 0, 0, 0.92);
 
-  .ue-metrics,
-  .ue-metrics__header {
-    border-color: $border-color !important;
-    border-right-color: $border-color !important;
-
-  }
-
-  .ue-metrics {
-    border-bottom: $border-width solid;
-
-    .ue-metrics__row {
-      border-left: $border-width solid $border-color !important;
-    }
-
-    .ue-metrics__metric {
-      border-top: $border-width solid;
-      border-block-start-color: $border-color !important;
-
-      border-right: $border-width solid $border-color !important;
-      border-block-end-color: $border-color !important;
-    }
-  }
 </style>
