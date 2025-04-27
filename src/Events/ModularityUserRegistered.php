883c3665c551fa0129dbbdf1df86cfcdd95faaa5
OoBook <oguzhan@olmadikprojeler.com>
2025-04-28 02:21:26 +0300

    feat(ModularityUserRegistered.php): create event class for user registration
    - Introduced ModularityUserRegistered event class to handle user registration events.
    - The class includes a property for the authenticated user and a constructor for instantiation, enhancing event-driven architecture in the application.

---
+++ src/Events/ModularityUserRegistered.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Unusualify\Modularity\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ModularityUserRegistered
+{
+    use SerializesModels;
+
+    /**
+     * The authenticated user.
+     *
+     * @var \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+}
