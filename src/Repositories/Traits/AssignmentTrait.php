3401ef3a5db846fca1984a6b843865172139dc1f
OoBook <oguzhan@olmadikprojeler.com>
2025-05-02 12:26:54 +0300

    refactor(AssignmentTrait, ProcessableTrait): remove unused methods and clean up code
    - Removed several unused methods from AssignmentTrait and ProcessableTrait to streamline the codebase.
    - This cleanup enhances maintainability and readability by eliminating redundant code and comments.

--- src/Repositories/Traits/AssignmentTrait.php
+++ src/Repositories/Traits/AssignmentTrait.php
@@ -29,98 +29,6 @@ trait AssignmentTrait
     }
 
     /**
-     * @param array $fields
-     * @return array
-     */
-    public function prepareFieldsBeforeCreateAssignmentTrait($fields)
-    {
-        // set, cast or unset related fields in order to prepare if necessary
-
-        return $fields;
-    }
-
-    /**
-     * @param \Unusualify\Modularity\Models\Model $object
-     * @param array $fields
-     * @return string[]
-     */
-    public function prepareFieldsBeforeSaveAssignmentTrait($object, $fields)
-    {
-        // set, cast or unset related fields in order to prepare if necessary
-
-        return $fields;
-    }
-
-    /**
-     * @param \Unusualify\Modularity\Models\Model $object
-     * @param array $fields
-     * @return void
-     */
-    public function afterUpdateBasicAssignmentTrait($object, $fields)
-    {
-        // do something after the updateBasic method of repository
-    }
-
-    /**
-     * @param \Unusualify\Modularity\Models\Model $object
-     * @param array $fields
-     * @return void
-     */
-    public function beforeSaveAssignmentTrait($object, $fields)
-    {
-        // do something before the create, update or duplicate methods of repository
-    }
-
-    /**
-     * @param \Unusualify\Modularity\Models\Model $object
-     * @param array $fields
-     * @return void
-     */
-    public function afterSaveAssignmentTrait($object, $fields)
-    {
-        // do something after the create, update or duplicate methods of repository
-    }
-
-    /**
-     * @param \Unusualify\Modularity\Models\Model $object
-     * @return void
-     */
-    public function afterDeleteAssignmentTrait($object)
-    {
-        // do something after the delete method of repository
-    }
-
-    /**
-     * @param \Unusualify\Modularity\Models\Model $object
-     * @return void
-     */
-    public function afterForceDeleteAssignmentTrait($object)
-    {
-        // do something after the forceDelete, bulkForceDelete methods of repository
-    }
-
-    /**
-     * @param \Unusualify\Modularity\Models\Model $object
-     * @return void
-     */
-    public function afterRestoreAssignmentTrait($object)
-    {
-        // do something after the restore, bulkRestore methods of repository
-    }
-
-    /**
-     * @param \Unusualify\Modularity\Models\Model $object
-     * @param array $fields
-     * @return \Unusualify\Modularity\Models\Model
-     */
-    public function hydrateAssignmentTrait($object, $fields)
-    {
-        // change, cast or set related fields in order to hydrate the object Model
-
-        return $object;
-    }
-
-    /**
      * @param \Unusualify\Modularity\Models\Model $object
      * @param array $fields
      * @param array $schema
@@ -140,19 +48,6 @@ trait AssignmentTrait
     }
 
     /**
-     * @param \Unusualify\Modularity\Models\Model $object
-     * @param array $fields
-     * @param array $schema
-     * @return array
-     */
-    public function getShowFieldsAssignmentTrait($object, $fields, $schema = [])
-    {
-        // set, cast, unset or manipulate the fields by using object, fields and schema
-
-        return $fields;
-    }
-
-    /**
      * @param \Illuminate\Database\Query\Builder $query
      * @param array $scopes
      * @return void
@@ -161,17 +56,6 @@ trait AssignmentTrait
     {
         // set, cast, unset or manipulate the scopes by using query and scopes
         $scopes['everAssignedToYourRoleOrHasAuthorization'] = true;
-    }
-
-    /**
-     * @param \Illuminate\Database\Query\Builder $query
-     * @param array $orders
-     * @return void
-     */
-    public function orderAssignmentTrait($query, &$orders)
-    {
-        // set, cast, unset or manipulate the orders by using query and orders
-        // $orders['order'] = true;
     }
 
     public function getAssignments($id)
