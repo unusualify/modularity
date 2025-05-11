47a8bef87237f9922fcd40a7cdcb47ee1063ae68
OoBook <oguzhan@olmadikprojeler.com>
2025-05-11 22:38:56 +0300

    refactor(Sidebar): streamline sidebar layout and improve user profile display
    - Updated the sidebar component to conditionally render user profile information based on guest status.
    - Enhanced the layout by reorganizing the structure of the profile section and ensuring consistent use of Vue slots for better maintainability.
    - Retained functionality for logout and about dialog while improving overall readability of the code.

--- vue/src/js/components/layouts/Sidebar.vue
+++ vue/src/js/components/layouts/Sidebar.vue
@@ -69,95 +69,97 @@
     </ue-navigation-group>
 
     <template v-slot:append>
-      <v-divider></v-divider>
-      <v-list class="">
-        <v-list-item
-          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-          :title="$store.getters.userProfile.name"
-          :subtitle="$store.getters.userProfile.email"
-          class="ue-sidebar__info-item"
-        >
-          <template v-slot:prepend="prependScope">
-            <v-avatar :image="$store.getters.userProfile.avatar_url"
-              @click="$openProfileDialog"/>
-          </template>
-          <template v-slot:append>
-            <v-btn
-              @click="profileMenuOpen = !profileMenuOpen"
-              :icon="profileMenuOpen ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-              size="small"
-              variant="text"
-            ></v-btn>
-          </template>
-        </v-list-item>
-
-        <v-expand-transition>
-          <ue-navigation-group
-            v-if="profileMenuOpen"
-            :items="profileMenu"
-            :profileMenu="true"
-            @activateMenu="handleMenu($event)"
+      <template v-if="!$store.getters.isGuest || true">
+        <v-divider></v-divider>
+        <v-list class="">
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            :title="$store.getters.userProfile.name"
+            :subtitle="$store.getters.userProfile.email"
+            class="ue-sidebar__info-item"
           >
-          </ue-navigation-group>
-        </v-expand-transition>
+            <template v-slot:prepend="prependScope">
+              <v-avatar :image="$store.getters.userProfile.avatar_url"
+                @click="$openProfileDialog"/>
+            </template>
+            <template v-slot:append>
+              <v-btn
+                @click="profileMenuOpen = !profileMenuOpen"
+                :icon="profileMenuOpen ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+                size="small"
+                variant="text"
+              ></v-btn>
+            </template>
+          </v-list-item>
 
-        <ue-logout-modal :csrf="$csrf()">
-          <template v-slot:activator="{ props }">
-            <v-tooltip text="Logout" location="top" :disabled="!(rail && !isHoverable)">
-              <template v-slot:activator="tooltipActivator">
-                <div v-bind="tooltipActivator.props">
-                  <v-list-item prepend-icon="mdi-logout" v-bind="props">
-                    {{ $t("authentication.logout") }}
-                  </v-list-item>
-                </div>
-              </template>
+          <v-expand-transition>
+            <ue-navigation-group
+              v-if="profileMenuOpen"
+              :items="profileMenu"
+              :profileMenu="true"
+              @activateMenu="handleMenu($event)"
+            >
+            </ue-navigation-group>
+          </v-expand-transition>
 
-            </v-tooltip>
-          </template>
-        </ue-logout-modal>
+          <ue-logout-modal :csrf="$csrf()">
+            <template v-slot:activator="{ props }">
+              <v-tooltip text="Logout" location="top" :disabled="!(rail && !isHoverable)">
+                <template v-slot:activator="tooltipActivator">
+                  <div v-bind="tooltipActivator.props">
+                    <v-list-item prepend-icon="mdi-logout" v-bind="props">
+                      {{ $t("authentication.logout") }}
+                    </v-list-item>
+                  </div>
+                </template>
 
-        <!-- About Dialog -->
-        <v-dialog ref="aboutDialog" max-width="500" v-if="$store.getters.versions && !$store.getters.isClient && !$store.getters.isSuperAdmin">
-          <template v-slot:activator="{ props: activatorProps }">
-            <v-list-item prepend-icon="mdi-information" v-bind="activatorProps">
-              {{ $t("About") }}
-            </v-list-item>
-          </template>
+              </v-tooltip>
+            </template>
+          </ue-logout-modal>
 
-          <template v-slot:default="{ isActive }">
-            <v-card :title="$t('About')">
-              <v-card-text>
-                <div v-for="(version, key) in $store.getters.versions" :key="key" class="d-flex align-center my-1">
-                  {{ $headline(key) }}:
-                  <v-chip variant="outlined" color="primary" class="ml-2">
-                    {{ version }}
-                  </v-chip>
-                </div>
-                <div v-if="$store.getters.isSuperAdmin" v-for="key in ['appName', 'appEnv', 'appDebug']" :key="key" class="d-flex align-center my-1">
-                  {{ key === 'appDebug' ? 'Debug Mode' : $headline(key) }}:
-                  <v-chip variant="outlined" :color="key === 'appDebug' ? $store.getters[key] ? 'success' : 'error' : 'primary'" class="ml-2">
-                    {{ key === 'appDebug' ? $store.getters[key] ? 'Active' : 'Inactive' : $store.getters[key] }}
-                  </v-chip>
-                </div>
-                <!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
-              </v-card-text>
+          <!-- About Dialog -->
+          <v-dialog ref="aboutDialog" max-width="500" v-if="$store.getters.versions && !$store.getters.isClient && !$store.getters.isSuperAdmin">
+            <template v-slot:activator="{ props: activatorProps }">
+              <v-list-item prepend-icon="mdi-information" v-bind="activatorProps">
+                {{ $t("About") }}
+              </v-list-item>
+            </template>
 
-              <v-divider></v-divider>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  variant="outlined"
-                  :text="$t('Close')"
-                  @click="isActive.value = false"
-                ></v-btn>
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-dialog>
+            <template v-slot:default="{ isActive }">
+              <v-card :title="$t('About')">
+                <v-card-text>
+                  <div v-for="(version, key) in $store.getters.versions" :key="key" class="d-flex align-center my-1">
+                    {{ $headline(key) }}:
+                    <v-chip variant="outlined" color="primary" class="ml-2">
+                      {{ version }}
+                    </v-chip>
+                  </div>
+                  <div v-if="$store.getters.isSuperAdmin" v-for="key in ['appName', 'appEnv', 'appDebug']" :key="key" class="d-flex align-center my-1">
+                    {{ key === 'appDebug' ? 'Debug Mode' : $headline(key) }}:
+                    <v-chip variant="outlined" :color="key === 'appDebug' ? $store.getters[key] ? 'success' : 'error' : 'primary'" class="ml-2">
+                      {{ key === 'appDebug' ? $store.getters[key] ? 'Active' : 'Inactive' : $store.getters[key] }}
+                    </v-chip>
+                  </div>
+                  <!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. -->
+                </v-card-text>
 
-        <!-- Bottom Slot -->
-        <slot name="bottom"> </slot>
-      </v-list>
+                <v-divider></v-divider>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    variant="outlined"
+                    :text="$t('Close')"
+                    @click="isActive.value = false"
+                  ></v-btn>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-dialog>
+
+          <!-- Bottom Slot -->
+          <slot name="bottom"> </slot>
+        </v-list>
+      </template>
     </template>
 
     <!-- <template v-slot:append>
