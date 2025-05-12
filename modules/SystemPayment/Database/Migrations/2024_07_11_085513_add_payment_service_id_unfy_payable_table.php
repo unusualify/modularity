d982ee8271687d6edc25208f790f75d940fa2971
OoBook <oguz.bukcuoglu@gmail.com>
2025-05-12 16:08:57 +0300

    fix(migration): update payable table name and add foreign keys for payment service, price, and currency
    - Changed the table name from 'unfy_payments' to 'up_payments' in the migration file.
    - Added foreign key constraints for 'payment_service_id', 'price_id', and 'currency_id' to ensure referential integrity.

--- modules/SystemPayment/Database/Migrations/2024_07_11_085513_add_payment_service_id_unfy_payable_table.php
+++ modules/SystemPayment/Database/Migrations/2024_07_11_085513_add_payment_service_id_unfy_payable_table.php
@@ -17,8 +17,7 @@ return new class extends Migration
         $price = new Price;
         $priceTableName = $price->getTable();
 
-        Schema::table(config('payable.table', 'unfy_payments'), function (Blueprint $table) use ($priceTableName) {
-
+        Schema::table(config('payable.table', 'up_payments'), function (Blueprint $table) use ($priceTableName) {
             $table->foreignId('payment_service_id')->after('id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('price_id')->after('payment_service_id')->constrained($priceTableName);
             $table->foreignId('currency_id')->after('price_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
@@ -33,12 +32,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-
-        Schema::table(config('payable.table', 'unfy_payments'), function (Blueprint $table) {
+        Schema::table(config('payable.table', 'up_payments'), function (Blueprint $table) {
             $table->dropForeign(['payment_service_id']);
             $table->dropColumn('payment_service_id');
             $table->dropForeign(['price_id']);
             $table->dropColumn('price_id');
+            $table->dropForeign(['currency_id']);
             $table->dropColumn('currency_id');
         });
 
