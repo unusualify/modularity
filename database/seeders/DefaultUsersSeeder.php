5b2991f2dec01c31b2f428f932445963c3549472
OoBook <oguz.bukcuoglu@gmail.com>
2025-05-21 12:41:10 +0300

    refactor(DefaultUsersSeeder.php): clean up user seeder by removing commented-out entries
    - Removed unused and commented-out user entries from the DefaultUsersSeeder to improve code clarity and maintainability.
    - Retained only the active user data for better readability.

--- database/seeders/DefaultUsersSeeder.php
+++ database/seeders/DefaultUsersSeeder.php
@@ -37,36 +37,7 @@ class DefaultUsersSeeder extends Seeder
                 'language' => 'en',
                 'timezone' => 'Europe/London',
                 'password' => '$2y$10$./.YS6k9clh6IU1cOdJLcuqqnY1FkxzjAcJ5Wv8qoQj6nYGV.fjqy',
-            ],
-            [
-                'id' => 2,
-                'name' => 'B2press',
-                'company_id' => null,
-                'surname' => null,
-                'job_title' => null,
-                'email' => 'info@b2press.com',
-                'language' => 'en',
-                'timezone' => 'Europe/London',
-                'password' => '$2y$10$ldyuNCY./iLsXQRxslQs0ex8Bryf3VAVDK3.826plGqjhGqAgPqwS',
-            ],
-            // [
-            //     'id' => 5,
-            //     'name' => 'Oğuzhan',
-            //     'company_id' => 1,
-            //     'surname' => 'Bükçüoğlu',
-            //     'job_title' => 'Senior',
-            //     'email' => 'oguz.bukcuoglu@gmail.com',
-            //     'language' => 'English',
-            //     'timezone' => 'Europe/Istanbul',
-            //     'phone' => '+90 552 313 08 93',
-            //     'country' => 'Türkiye',
-            //     'email_verified_at' => NULL,
-            //     'password' => '$2y$10$NxJoxJ0bN3HheQG4mENCFuUuKbKQ0WV6Rh5s7VZUDB/KYSleZZqGm',
-            //     'remember_token' => NULL,
-            //     'created_at' => '2023-11-30 07:32:10',
-            //     'updated_at' => '2023-11-30 11:49:47',
-            //     'published' => 0,
-            // ],
+            ]
         ]);
 
     }
