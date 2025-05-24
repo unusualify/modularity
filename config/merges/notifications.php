a1ad565e4cb40653ec9cb238f789b43318b19c07
OoBook <oguz.bukcuoglu@gmail.com>
2025-05-25 00:27:11 +0300

    feat(Notification): update notification channels and add TaskAssignedToAuthorizableNotification
    - Updated default notification channels for stateable, chatable, assignable, and authorizable notifications to include 'database' and 'mail'.
    - Introduced a new notification class `TaskAssignedToAuthorizableNotification` to handle task assignment notifications for authorizable models, enhancing the notification system's capabilities.

--- config/merges/notifications.php
+++ config/merges/notifications.php
@@ -2,15 +2,15 @@
 
 return [
     'stateable' => [
-        'channels' => env('NOTIFICATIONS_STATEABLE_CHANNELS', ''),
+        'channels' => env('NOTIFICATIONS_STATEABLE_CHANNELS', 'database,mail'),
     ],
     'chatable' => [
-        'channels' => env('NOTIFICATIONS_CHATABLE_CHANNELS', ''),
+        'channels' => env('NOTIFICATIONS_CHATABLE_CHANNELS', 'database,mail'),
     ],
     'assignable' => [
-        'channels' => env('NOTIFICATIONS_ASSIGNABLE_CHANNELS', ''),
+        'channels' => env('NOTIFICATIONS_ASSIGNABLE_CHANNELS', 'database,mail'),
     ],
     'authorizable' => [
-        'channels' => env('NOTIFICATIONS_AUTHORIZABLE_CHANNELS', ''),
+        'channels' => env('NOTIFICATIONS_AUTHORIZABLE_CHANNELS', 'database,mail'),
     ],
 ];
