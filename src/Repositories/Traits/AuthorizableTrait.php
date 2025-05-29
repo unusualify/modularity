94c098d0dc7b87bd9e1104da39a6cdc03d94775b
OoBook <oguz.bukcuoglu@gmail.com>
2025-05-30 00:24:03 +0300

    feat(AuthorizableTrait): add method for table filters based on authorization
    - Introduced `getTableFiltersAuthorizableTrait` to generate filter options for authorized and unauthorized listings, enhancing data handling for authorization-related queries.
    - Improved flexibility by including a filter for user-specific authorizations, allowing for more tailored data retrieval.

--- src/Repositories/Traits/AuthorizableTrait.php
+++ src/Repositories/Traits/AuthorizableTrait.php
@@ -14,4 +14,36 @@ trait AuthorizableTrait
         // set, cast, unset or manipulate the scopes by using query and scopes
         // $scopes['hasAuthorization'] = true;
     }
+
+    public function getTableFiltersAuthorizableTrait($scope): array
+    {
+        $model = $this->getModel();
+
+        $tableFilters = [];
+
+        if ($model->hasAuthorizationUsage()) {
+            $tableFilters[] = [
+                'name' => ___('listing.filter.authorized'),
+                'slug' => 'authorized',
+                'methods' => 'getCountFor',
+                'params' => ['hasAnyAuthorization'],
+            ];
+
+            $tableFilters[] = [
+                'name' => ___('listing.filter.unauthorized'),
+                'slug' => 'unauthorized',
+                'methods' => 'getCountFor',
+                'params' => ['unauthorized'],
+            ];
+        }
+
+        $tableFilters[] = [
+            'name' => ___('listing.filter.your-authorizations'),
+            'slug' => 'your-authorizations',
+            'methods' => 'getCountFor',
+            'params' => ['isAuthorizedToYou'],
+        ];
+
+        return $tableFilters;
+    }
 }
