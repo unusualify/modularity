9dfca9fad7a1d61493cf24aab05fb237f18554cb
OoBook <oguz.bukcuoglu@gmail.com>
2025-06-02 14:58:35 +0300

    build(vue): update build artifacts for v0.31.0

---
+++ vue/dist/modularity/entries/core-form.IB2ncUf1.js
@@ -0,0 +1,2 @@
+import{_ as t}from"../js/vendor.j7I_0bfY.js";import{f as m,U as s,s as p}from"../js/form.CnsPr2QJ.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.6Tbq9Hrr.js";const o=p;o.registerModule("form",m);const r=t({store:o});r.use(s);r.mount("#admin");
+//# sourceMappingURL=core-form.IB2ncUf1.js.map
