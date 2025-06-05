bd0ed1e5b24a8be0b29f837ee869e79d5c5ecb7c
OoBook <oguz.bukcuoglu@gmail.com>
2025-06-05 10:50:56 +0300

    build(vue): update build artifacts for v0.33.0

---
+++ vue/dist/modularity/entries/core-dashboard.BSYYxQ7R.js
@@ -0,0 +1,2 @@
+import{_ as o}from"../js/vendor.j7I_0bfY.js";import{f as r,U as e,s as m}from"../js/form.uSr2OnFs.js";import{d as s,U as p}from"../js/datatable.DIwjEDQ2.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.6Tbq9Hrr.js";const t=m;t.registerModule("datatable",s);t.registerModule("form",r);const a=o({store:t});a.use(e);a.component("ue-datatable",p);a.mount("#admin");
+//# sourceMappingURL=core-dashboard.BSYYxQ7R.js.map
--- vue/dist/modularity/entries/core-form.CSlqk6Kr.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"../js/vendor.j7I_0bfY.js";import{f as m,U as s,s as p}from"../js/form.CtSmYZJA.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.6Tbq9Hrr.js";const o=p;o.registerModule("form",m);const r=t({store:o});r.use(s);r.mount("#admin");
-//# sourceMappingURL=core-form.CSlqk6Kr.js.map
