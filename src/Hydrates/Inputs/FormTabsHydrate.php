283bcba15d8c1b208db50eed8fa347bfe17b5003
OoBook <oguz.bukcuoglu@gmail.com>
2025-06-13 11:07:12 +0300

    feat(FormTabsHydrate): enhance hydration logic to support lazy loading
    - Updated the hydrate method to differentiate between eager and lazy loading configurations for input types.
    - Introduced logic to handle lazy loading for 'input-comparison-table' and other input types, improving data retrieval flexibility.
    - Consolidated eager and lazy loading arrays into a single structure for better management of relationships.

--- src/Hydrates/Inputs/FormTabsHydrate.php
+++ src/Hydrates/Inputs/FormTabsHydrate.php
@@ -26,20 +26,42 @@ class FormTabsHydrate extends InputHydrate
         $input['default'] = json_decode('{}');
 
         $eagers = [];
+        $lazy = [];
         foreach ($input['schema'] as $key => $_input) {
             if (isset($_input['noEager']) && $_input['noEager'] === true) {
                 continue;
             }
             if ($_input['type'] == 'input-comparison-table' && isset($_input['comparators'])) {
                 foreach ($_input['comparators'] as $relation => $conf) {
-                    $eagers[] = isset($conf['eager']) ? $conf['eager'] : $input['name'] . '.' . $relation;
+                    if (isset($conf['lazy'])) {
+                        $lazy[] = is_array($conf['lazy']) ? $conf['lazy'] : explode(',', $conf['lazy']);
+                    } else {
+                        $eagers[] = isset($conf['eager'])
+                            ? (is_array($conf['eager']) ? $conf['eager'] : explode(',', $conf['eager']))
+                            : [$input['name'] . '.' . $relation];
+                    }
                 }
             }
             if (in_array($_input['type'], ['checklist', 'input-checklist', 'select', 'combobox', 'autocomplete'])) {
-                $eagers[] = isset($_input['eager']) ? $_input['eager'] : $input['name'] . '.' . $_input['name'];
+                if (isset($_input['lazy'])) {
+                    $lazy[] = is_array($_input['lazy']) ? $_input['lazy'] : explode(',', $_input['lazy']);
+                } else {
+                    $eagers[] = isset($_input['eager'])
+                        ? (is_array($_input['eager']) ? $_input['eager'] : explode(',', $_input['eager']))
+                        : $input['name'] . '.' . $_input['name'];
+                }
             }
         }
-        $input['eagers'] = $eagers;
+
+        $input['eagers'] = array_reduce($eagers, function ($acc, $item) {
+            $acc = array_merge($acc, $item);
+            return $acc;
+        }, []);
+
+        $input['lazy'] = array_reduce($lazy, function ($acc, $item) {
+            $acc = array_merge($acc, $item);
+            return $acc;
+        }, []);
 
         return $input;
     }
