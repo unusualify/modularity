a960c813bb9d34b3cef24fa3dfdcfc1f33abd419
OoBook <oguz.bukcuoglu@gmail.com>
2025-06-20 11:58:36 +0300

    feat(StateableScopes): add scope methods for state filtering
    - Introduced scopeIsStateables method to filter queries based on multiple state codes.
    - Added scopeIsStateablesCount method to count the number of records matching the specified state codes.

--- src/Entities/Scopes/StateableScopes.php
+++ src/Entities/Scopes/StateableScopes.php
@@ -4,6 +4,18 @@ namespace Unusualify\Modularity\Entities\Scopes;
 
 trait StateableScopes
 {
+    public function scopeIsStateables($query, $codes)
+    {
+        return $query->whereHas('state', function ($q) use ($codes) {
+            $q->whereIn($q->getModel()->getTable() . '.code', $codes);
+        });
+    }
+
+    public function scopeIsStateablesCount($query, $codes)
+    {
+        return $query->isStateables($codes)->count();
+    }
+
     public function scopeIsStateable($query, $code)
     {
         return $query->whereHas('state', function ($q) use ($code) {
