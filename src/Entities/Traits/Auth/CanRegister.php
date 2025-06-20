a92613e94316a24a117e6c74daefd2d265201e62
celikerde <celikerde@itu.edu.tr>
2025-06-20 12:54:28 +0300

    feat(auth): add traits for user registration and redirection
    - Introduce CanRegister trait to handle user registration email retrieval and notification sending.
    - Add RedirectsUsers trait to manage post-registration and login redirect paths, enhancing user flow.

---
+++ src/Entities/Traits/Auth/CanRegister.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Unusualify\Modularity\Entities\Traits\Auth;
+
+use Unusualify\Modularity\Notifications\EmailVerification;
+
+
+trait CanRegister
+{
+    /**
+     * Get the e-mail address where password reset links are sent.
+     *
+     * @return string
+     */
+    public function getEmailForRegister()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendRegisterNotification($token)
+    {
+        $this->notify(new EmailVerification($token));
+    }
+}
