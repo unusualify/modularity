3c2672b5d58f3c8ac57a3dd97b79740f8a2f897b
celikerde <celikerde@itu.edu.tr>
2025-07-17 12:30:30 +0300

    chore (ManageUtilities.php): clean up getViewLayoutVariables method
    - Removed commented-out debug statements and unnecessary blank lines for improved code readability.
    - Streamlined the method to focus on dynamic page and header title generation based on the current action.

--- src/Http/Controllers/Traits/ManageUtilities.php
+++ src/Http/Controllers/Traits/ManageUtilities.php
@@ -178,9 +178,6 @@ trait ManageUtilities
              ] : [])
              + $this->getViewLayoutVariables();
 
-
-
-
         return array_replace_recursive($data, $customFormData);
     }
 
@@ -195,7 +192,6 @@ trait ManageUtilities
 
         public function getViewLayoutVariables()
     {
-        // dd(Str::plural('Country'));
         $currentRoute = Route::current();
         $currentActionMethod = $currentRoute->getActionMethod();
         $snakeRouteName = Str::snake($this->routeName);
@@ -204,11 +200,8 @@ trait ManageUtilities
         // Check for custom title from configuration first
         $customTitle = $this->tableAttributes['customTitle'] ?? null;
 
-        // dd($currentActionMethod);
-
         switch ($currentActionMethod) {
             case 'create':
-                dd('Create');
                 $pageTitle = trans_choice($translationRouteKey, 1);
                 $headerTitle = $customTitle ?: __('fields.new-item', ['item' => trans_choice('modules.' . snakeCase($this->routeName), 1)]);
                 break;
@@ -221,10 +214,8 @@ trait ManageUtilities
                 $headerTitle = $customTitle ?: __('fields.show-item', ['item' => trans_choice('modules.' . snakeCase($this->routeName), 1)]);
                 break;
             default:
-                // dd($customTitle);
                 $pageTitle = $customTitle ?: trans_choice($translationRouteKey, 0);
                 $headerTitle = $customTitle ?: trans_choice($translationRouteKey, 0);
-                // dd($pageTitle, $headerTitle, trans_choice($translationRouteKey, 0));
         }
 
         return [
