09f1f24c2ee70ffe5f8c436ad6626fc192664714
OoBook <oguz.bukcuoglu@gmail.com>
2025-07-19 04:33:19 +0300

    fix(RegisterController.php): update registration response handling
    - Changed the response status code for validation errors from 200 to 422 to better reflect the error state.
    - Moved the event triggering for ModularityUserRegistering to occur after validation checks.
    - Enhanced the success response to include a status message and redirect URLs for improved user experience.

--- src/Http/Controllers/RegisterController.php
+++ src/Http/Controllers/RegisterController.php
@@ -149,19 +149,19 @@ class RegisterController extends Controller
     {
         $validator = $this->validator($request->all());
 
-        event(new ModularityUserRegistering($request));
-
         if ($validator->fails()) {
             return $request->wantsJson()
                 ? new JsonResponse([
                     'errors' => $validator->errors(),
                     'message' => $validator->messages()->first(),
                     'variant' => MessageStage::WARNING,
-                ], 200)
+                ], 422)
                 : $request->validate($this->rules());
 
             return $res;
         }
+
+        event(new ModularityUserRegistering($request));
 
         $user = Company::create([
             'name' => $request['company'] ?? '',
@@ -180,7 +180,10 @@ class RegisterController extends Controller
 
         return $request->wantsJson()
             ? new JsonResponse([
+                'status' => 'success',
+                'message' => 'User registered successfully',
                 'redirector' => route(Route::hasAdmin('register.success')),
+                'login_page' => route(Route::hasAdmin('login')),
             ], 200)
             : $this->sendLoginResponse($request);
     }
