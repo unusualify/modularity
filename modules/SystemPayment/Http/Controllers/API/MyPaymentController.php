6c20d78d7299554c37825168bd2db88b5c10efae
OoBook <oguz.bukcuoglu@gmail.com>
2025-07-30 14:00:34 +0300

    feat(SystemPayment): introduce MyPayment module with CRUD functionality
    - Added MyPayment entity, repository, and controllers for managing payments.
    - Updated configuration and routes to include MyPayment.
    - Created views and language files for MyPayment, enhancing user interface and localization.
    - Implemented API resource transformer for MyPayment data representation.

---
+++ modules/SystemPayment/Http/Controllers/API/MyPaymentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Modules\SystemPayment\Http\Controllers\API;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Modules\SystemPayment\Repositories\MyPaymentRepository;
+use Modules\SystemPayment\Transformers\MyPaymentResource;
+
+class MyPaymentController extends Controller
+{
+
+    /**
+     * This resource repository
+     */
+    private $repository;
+
+
+    /**
+     * @param MyPaymentRepository $repository
+     */
+    public function __construct(MyPaymentRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     * @return Renderable
+     */
+    public function index(Request $request)
+    {
+        return new MyPaymentResource( $this->repository->paginate($request) );
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Renderable
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function show($id)
+    {
+
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Renderable
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
