99910da8a627574288e705d5af8edd9e65c6cedd
OoBook <oguz.bukcuoglu@gmail.com>
2025-07-31 22:57:42 +0300

    build(vue): update build artifacts for v0.41.0

---
+++ vue/dist/modularity/entries/core-auth.BBcoQFm2.js
@@ -0,0 +1,2 @@
+import{_ as t}from"../js/vendor.BacmXAK3.js";import{f as s,U as m,s as p}from"../js/form.CiGgWT3l.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.BoxWmjM7.js";const o=p;o.registerModule("form",s);const r=t({store:o});r.use(m);r.mount("#auth");
+//# sourceMappingURL=core-auth.BBcoQFm2.js.map
