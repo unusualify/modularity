b7d57fdbc3081396bf99f145749185c2a808318d
OoBook <oguz.bukcuoglu@gmail.com>
2025-08-18 23:24:41 +0300

    refactor(Price.php): enhance payment update logic and modularity integration
    - Modified the updateOrNewPayment method to accept an additional parameter for extra payloads.
    - Improved the modularity payload structure for better integration with payment processing.
    - Added functionality to save receipts for transfer payments using the Filepond facade.
    - Streamlined the merging of payloads to ensure clarity and maintainability.

--- modules/SystemPricing/Entities/Price.php
+++ modules/SystemPricing/Entities/Price.php
@@ -2,9 +2,12 @@
 
 namespace Modules\SystemPricing\Entities;
 
+use Illuminate\Support\Arr;
 use Modules\SystemPayment\Entities\Payment;
 use Modules\SystemPricing\Entities\Mutators\PriceMutators;
+use Unusualify\Modularity\Entities\Enums\PaymentStatus;
 use Unusualify\Modularity\Entities\Traits\ModelHelpers;
+use Unusualify\Modularity\Facades\Filepond;
 
 class Price extends \Oobook\Priceable\Models\Price
 {
@@ -78,14 +81,56 @@ class Price extends \Oobook\Priceable\Models\Price
         return $this->payments('COMPLETED');
     }
 
-    public function updateOrNewPayment($payload)
+    public function updateOrNewPayment($payload, $extraPayload = [])
     {
+        $defaultCurrency = $this->currency;
+        $orderId = uniqid('ORD');
+        $modularityPayload = array_merge([
+            'previous_url' => url()->previous(),
+            'datetime' => now()->format('Y-m-d H:i:s'),
+            'original_raw_amount' => $this->discounted_raw_amount,
+            'original_total_amount' => $this->total_amount,
+            'converted_raw_amount' => $this->discounted_raw_amount,
+            'converted_total_amount' => $this->total_amount,
+            'vat_percentage' => $this->vat_percentage,
+            'vat_multiplier' => $this->vat_multiplier,
+            'discount_percentage' => $this->discount_percentage,
+            'converted' => $this->discounted_raw_amount,
+            'original_currency' => $this->currency->iso_4217,
+            'original_currency_id' => $this->currency_id,
+            'converted_currency' => $defaultCurrency->iso_4217,
+            'converted_currency_id' => $defaultCurrency->id,
+            'exchange_rate' => 1,
+        ], $payload['parameters']['modularity'] ?? []);
+
+        $payload = array_merge([
+            'amount' => $this->total_amount,
+            'currency' => $defaultCurrency->iso_4217,
+            'currency_id' => $defaultCurrency->id,
+            'order_id' => $orderId,
+            'installment' => 1,
+            'status' => PaymentStatus::COMPLETED,
+            // 'payment_gateway' => $paymentService->key,
+            // 'payment_service_id' => $paymentService->id,
+            'parameters' => [
+                ...($payload['parameters'] ?? []),
+                'modularity' => $modularityPayload,
+            ],
+            'response' => [],
+        ], Arr::except($payload, ['parameters']));
+
         $payment = $this->payments()->whereIn('status', ['PENDING', 'FAILED'])->latest()->first();
 
         if($payment){
             $payment->update($payload);
+            $payment->refresh();
         } else {
             $payment = $this->payment()->create($payload);
+        }
+
+        // if it is a transfer payment, we need to save the receipts
+        if(isset($extraPayload['receipts'])){
+            Filepond::saveFile($payment, $extraPayload['receipts'], 'receipts');
         }
 
         return $payment;
