c8f3d8405facd17cb7a70251fc832c272d76b5c6
OoBook <oguz.bukcuoglu@gmail.com>
2025-08-20 15:57:38 +0300

    feat(component.php): add modularity modal and form service functions
    - Introduced `modularity_response_modal_body_component`, `modularity_modal_service`, and `modularity_modal_service_form` to facilitate modal rendering and form handling.
    - These functions enhance the modularity of the component system by providing reusable modal configurations and form setups.
    - Improved code organization and maintainability by encapsulating modal and form logic within dedicated functions.

---
+++ src/Helpers/component.php
@@ -0,0 +1,82 @@
+<?php
+
+use Unusualify\Modularity\View\Component;
+
+if (! function_exists('modularity_response_modal_body_component')) {
+    function modularity_response_modal_body_component($color = 'success', $icon = 'mdi-check-decagram-outline', string $title, string $description, $redirector = null, array $modalProps = [])
+    {
+        return Component::makeDiv()
+            ->setElements([
+                Component::makeDiv()
+                    ->setAttributes([
+                        'class' => 'd-flex justify-center',
+                    ])
+                    ->setElements([
+                        Component::makeVIcon()
+                            ->setAttributes([
+                                'icon' => $icon,
+                                'size' => 'x-large',
+                                'color' => $color,
+                            ]),
+                    ]),
+                Component::makeUeTitle()
+                    ->setAttributes([
+                        'tag' => 'h3',
+                        'type' => 'h3',
+                        'color' => $color,
+                        'weight' => 'regular',
+                        'transform' => 'capitalize',
+                        'justify' => 'center',
+                    ])
+                    ->setElements($title),
+                Component::makeUeTitle()
+                    ->setAttributes([
+                        'type' => 'body-2',
+                        'color' => 'grey-darken-1',
+                        'weight' => 'regular',
+                        'transform' => 'none',
+                        'justify' => 'center',
+                    ])
+                    ->setElements($description),
+            ])
+            ->render();
+    }
+}
+
+if (! function_exists('modularity_modal_service')) {
+    function modularity_modal_service(string $color = 'success', string $icon = 'mdi-check-decagram-outline', string $title, string $description, array $modalProps = [])
+    {
+        return [
+            'component' => 'ue-recursive-stuff',
+            'props' => [
+                'configuration' => modularity_response_modal_body_component($color, $icon, $title, $description, $modalProps),
+            ],
+            'modalProps' => $modalProps,
+        ];
+    }
+}
+
+if (! function_exists('modularity_modal_service_form')) {
+    function modularity_modal_service_form($schema, $actionUrl, $buttonText, array $model = [], array $modalProps = [], $formProps = [])
+    {
+        return [
+            'component' => 'ue-recursive-stuff',
+            'props' => [
+                'configuration' => Component::makeUeForm()
+                    ->setAttributes([
+                        ...$formProps,
+                        'hasSubmit' => true,
+                        'rowAttribute' => [
+                            'noGutters' => false,
+                        ],
+                        'schema' => $schema,
+                        'actionUrl' => $actionUrl,
+                        'buttonText' => $buttonText,
+                        'modelValue' => $model,
+                    ])
+                    ->render(),
+            ],
+            'modalProps' => $modalProps,
+        ];
+    }
+}
