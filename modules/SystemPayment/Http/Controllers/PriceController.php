0b43fe7f0fb66cf47b3b90dfb1369860c3245cf7
OoBook <oguz.bukcuoglu@gmail.com>
2025-09-03 18:16:53 +0300

    feat(PriceController): implement checkout method for payment processing
    - Added a new `checkout` method to handle payment processing, including validation of price ID and payment service compatibility.
    - Implemented currency conversion logic and integrated payment service selection based on user input.
    - Enhanced response handling for AJAX requests and added modal notifications for payment status.
    - Updated the `response` method to ensure payment status is marked as completed when appropriate.

--- modules/SystemPayment/Http/Controllers/PriceController.php
+++ modules/SystemPayment/Http/Controllers/PriceController.php
@@ -278,6 +278,187 @@ class PriceController extends Controller
         return $resp;
     }
 
+    public function checkout(Request $request)
+    {
+        $priceTable = (new Price)->getTable();
+        $request->validate([
+            'price_id' => 'required|exists:' . $priceTable . ',id',
+        ]);
+
+        $params = $request->all();
+        $price = Price::with('currency', 'vatRate')->find($params['price_id']);
+
+        $previousUrl = url()->previous();
+
+        if ($price->payment && $price->payment->status == PaymentStatus::COMPLETED) {
+            $modalService = modularity_modal_service('warning', 'mdi-alert-circle-outline', 'Paid Before', 'This price has already been paid! Please check the payment status.', [
+                'noCancelButton' => true,
+            ]);
+
+            if ($request->ajax()) {
+                return response()->json([
+                    'status' => MessageStage::ERROR,
+                    'message' => 'Payment already completed',
+                    'redirector' => merge_url_query($previousUrl ?? route('admin.dashboard'), [
+                        'modalService' => $modalService,
+                    ]),
+                ], 403);
+            }
+
+            return redirect(merge_url_query($previousUrl ?? route('admin.dashboard'), [
+                'modalService' => $modalService,
+            ]));
+        }
+
+        $user = Auth::user();
+        $company = $user->company;
+
+        $requestCurrencyIso4217 = null;
+        $paymentService = null;
+
+        if (! isset($params['payment_service'])) {
+            if ($request->ajax()) {
+                return response()->json([
+                    'status' => MessageStage::ERROR,
+                    'message' => 'Not compatible payment service',
+                ], 403);
+            }
+
+            return redirect()->back()->with('error', 'Not compatible payment service');
+        } else {
+            $requestCurrencyIso4217 = $params['payment_service']['currency']['iso_4217'];
+        }
+
+        $rawAmount = $price->discounted_raw_amount;
+        $totalAmount = $price->total_amount;
+
+        $currency = $price->currency;
+        $converted = false;
+        $exchangeRate = null;
+
+        if (Str::upper($currency->iso_4217) != Str::upper($requestCurrencyIso4217)) {
+            $converted = true;
+            $currency = Currency::where('iso_4217', $requestCurrencyIso4217)->first();
+
+            $rawAmount = CurrencyExchange::convertTo(
+                $rawAmount,
+                mb_strtoupper($requestCurrencyIso4217),
+                decimals: 0,
+                round: 'round'
+            );
+            $exchangeRate = CurrencyExchange::getExchangeRate(mb_strtoupper($requestCurrencyIso4217));
+            $totalAmount = intval($rawAmount * (1 + $price->vat_multiplier));
+        }
+
+        if (isset($params['payment_service']) && $params['payment_service']['payment_method'] == -1) {
+            $paymentCurrency = PaymentCurrency::find($currency->id);
+            $paymentService = $paymentCurrency->paymentService;
+
+            if (! $paymentService) {
+                throw new \Exception('Payment service not found for currency ' . $paymentCurrency->iso_4217);
+            }
+        } elseif (isset($params['payment_service'])) {
+            $paymentService = PaymentService::find($params['payment_service']['payment_method']);
+        }
+
+        $payable = new Payable($paymentService->key);
+
+        if (!$paymentService->serviceClass::hasBuiltInForm()) {
+            if ($request->ajax()) {
+                return response()->json([
+                    'status' => MessageStage::ERROR,
+                    'message' => 'Payment service does not support built-in form',
+                ], 403);
+            }
+
+            return redirect()->back()->with('error', 'Payment service does not support built-in form');
+        }
+
+        $orderId = uniqid('ORD');
+        $modularityPayload = [
+            'previous_url' => url()->previous(),
+            'datetime' => now()->format('Y-m-d H:i:s'),
+            'original_raw_amount' => $price->discounted_raw_amount,
+            'original_total_amount' => $price->total_amount,
+            'converted_raw_amount' => $rawAmount,
+            'converted_total_amount' => $totalAmount,
+            'vat_percentage' => $price->vat_percentage,
+            'vat_multiplier' => $price->vat_multiplier,
+            'discount_percentage' => $price->discount_percentage,
+            'converted' => $converted,
+            'original_currency' => $price->currency->iso_4217,
+            'original_currency_id' => $price->currency_id,
+            'converted_currency' => $currency->iso_4217,
+            'converted_currency_id' => $currency->id,
+            'exchange_rate' => $exchangeRate,
+        ];
+
+        Session::put('payable_payment_service', $paymentService->key);
+
+        $payload = [
+            'amount' => $totalAmount,
+            'currency' => $currency->iso_4217,
+            'order_id' => $orderId,
+            'installment' => $request->get('installment') ?? '1',
+
+            'payment_group' => 'PRODUCT',
+            'locale' => app()->getLocale(),
+
+            'card_name' => $params['payment_service']['credit_card']['card_name'],
+            'card_no' => str_replace(' ', '', $params['payment_service']['credit_card']['card_number']),
+            'card_month' => (string) $params['payment_service']['credit_card']['card_month'],
+            'card_year' => (string) $params['payment_service']['credit_card']['card_year'],
+            'card_cvv' => $params['payment_service']['credit_card']['card_cvv'],
+
+            'user_id' => $user->id,
+            'user_name' => $user->name,
+            'user_surname' => $user->surname,
+            'user_gsm' => $user->phone,
+            'user_email' => $user->email,
+            'user_ip' => $request->ip(),
+            'user_last_login_date' => Carbon::now()->format('Y-m-d H:i:s'),
+            'user_registration_date' => $user->created_at->format('Y-m-d H:i:s'),
+
+            'company_name' => $company ? $company->name : null,
+            'user_address' => $company ? $company->address : null,
+            'user_city' => $company ? $company->city : null,
+            'user_country' => $company ? $company->country : null,
+            'user_zip_code' => $company ? $company->zip_code : null,
+
+            'basket_id' => uniqid(),
+            'items' => [
+                [
+                    'id' => '1',
+                    'name' => 'test',
+                    'category1' => 'testcat1',
+                    'category2' => 'testcat2',
+                    'price' => $price->price_excluding_vat,
+                    'type' => 'VIRTUAL',
+                ],
+            ],
+
+            'modularity' => $modularityPayload,
+        ];
+
+        $paymentPayload = [
+            'price_id' => $price->id,
+            'payment_service_id' => $paymentService->id,
+            'currency_id' => $currency->id,
+        ];
+
+        if ($price->payment && in_array($price->payment->status, [PaymentStatus::PENDING, PaymentStatus::FAILED])) {
+            $paymentPayload['id'] = $price->payment->id;
+        }
+
+        $resp = $payable->checkout($payload, paymentPayload: $paymentPayload);
+
+        if($request->ajax()) {
+            return $resp;
+        }
+
+        return $resp;
+    }
+
     public function response(Request $request)
     {
         $color = 'error';
@@ -301,6 +482,12 @@ class PriceController extends Controller
                 'confirmText' => __('Continue'),
             ];
 
+            if($payment->status !== PaymentStatus::COMPLETED) {
+                $payment->update([
+                    'status' => PaymentStatus::COMPLETED,
+                ]);
+            }
+
             if ($payment) {
                 PaymentCompleted::dispatch($payment);
                 // $newPrice = $payment->price->replicate();
@@ -323,52 +510,19 @@ class PriceController extends Controller
 
         $modularityPayload = $payment ? $payment->parameters->modularity ?? new \stdClass : new \stdClass;
 
+        if($request->ajax()) {
+            return response()->json([
+                'variant' => $color,
+                'message' => $description,
+                'timeout' => 1000,
+                'redirector' => merge_url_query($modularityPayload->previous_url ?? route('admin.dashboard'), [
+                    'modalService' => modularity_modal_service($color, $icon, $title, $description, $modalProps),
+                ]),
+            ]);
+        }
+
         return redirect(merge_url_query($modularityPayload->previous_url ?? route('admin.dashboard'), [
             'modalService' => modularity_modal_service($color, $icon, $title, $description, $modalProps),
         ]));
-    }
-
-    protected function createModalBody(string $color, $icon, string $title, string $description, array $modalProps = [])
-    {
-        return Component::makeDiv()
-            ->setElements([
-                Component::makeVIcon()
-                    ->setAttributes([
-                        'icon' => $icon,
-                        'size' => 'x-large',
-                        'color' => $color,
-                    ]),
-                Component::makeUeTitle()
-                    ->setAttributes([
-                        'tag' => 'h3',
-                        'type' => 'h3',
-                        'color' => $color,
-                        'weight' => 'regular',
-                        'transform' => 'capitalize',
-                        'justify' => 'center',
-                    ])
-                    ->setElements($title),
-                Component::makeUeTitle()
-                    ->setAttributes([
-                        'type' => 'body-2',
-                        'color' => 'grey-darken-1',
-                        'weight' => 'regular',
-                        'transform' => 'none',
-                        'justify' => 'center',
-                    ])
-                    ->setElements($description),
-            ])
-            ->render();
-    }
-
-    protected function createModalService(string $color, $icon, string $title, string $description, array $modalProps = [])
-    {
-        return [
-            'component' => 'ue-recursive-stuff',
-            'props' => [
-                'configuration' => $this->createModalBody($color, $icon, $title, $description, $modalProps),
-            ],
-            'modalProps' => $modalProps,
-        ];
     }
 }
