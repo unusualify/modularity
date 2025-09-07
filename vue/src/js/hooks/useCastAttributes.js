8513a0745541deb744b298af1fd338bc564521a9
OoBook <oguz.bukcuoglu@gmail.com>
2025-09-07 14:59:23 +0300

    refactor(useCastAttributes): enhance attribute casting logic and introduce matching functions
    - Refactored the attribute casting logic to improve readability and maintainability.
    - Introduced new functions for matching attributes, standard attributes, and eval attributes to streamline the casting process.
    - Updated the castObjectAttribute function to utilize the new matching functions, enhancing its functionality and error handling.

--- vue/src/js/hooks/useCastAttributes.js
+++ vue/src/js/hooks/useCastAttributes.js
@@ -10,40 +10,22 @@ export default function useCastAttributes () {
   const AttributePattern = /\$([\w\d\.\*\_]+)/
   const EvalPattern = /^\$\((.*)\)\$/
 
-  const castObjectAttribute = (value, ownerItem) => {
-    if(!isString(value))
-      return value
+  const matchAttribute = (value) => {
+    return isMatchingPattern(value)
+  }
 
+  const matchStandardAttribute = (value) => {
+    return AttributePattern.test(value)
+  }
+
+  const matchEvalAttribute = (value) => {
+    return EvalPattern.test(value)
+  }
+
+  const castStandardAttribute = (value, ownerItem, options = {}) => {
     let returnValue = value
-    let matches
 
-    if(isMatchingPattern(value)){
-      returnValue = replacePattern(value, ownerItem)
-    } else if(EvalPattern.test(value)){
-      let matches = value.match(EvalPattern)
-      let evalText = matches[1]
-
-      let evalParts = evalText.split(' ').map((v) => {
-        if(AttributePattern.test(v)) {
-          let evalPartMatches = v.match(AttributePattern)
-          let evalPart = evalPartMatches[1]
-
-          let evalPartCastedValue = __data_get(ownerItem, evalPart, undefined)
-
-          if(evalPartCastedValue !== undefined) {
-            return evalPartCastedValue
-          }
-        }
-
-        return v
-      })
-
-      try {
-        return eval(evalParts.join(' '))
-      } catch (e) {
-        console.error('Error in eval', e)
-      }
-    } else if(AttributePattern.test(value)){
+    if(matchStandardAttribute(value)){
       let matches = value.match(AttributePattern)
       let notation = matches[1]
       let quoted = __preg_quote(matches[0])
@@ -52,9 +34,11 @@ export default function useCastAttributes () {
       let newParts = []
       for(const j in parts){
         let part = parts[j]
-        if(part === '*'){
+        if(part === '*' ){
           let _id = ownerItem.id
-          newParts.push(`*id=${_id}`)
+          if(!(__isset(options.clearAsterisk) && options.clearAsterisk)){
+            newParts.push(`*id=${_id}`)
+          }
         }else{
           newParts.push(part)
         }
@@ -86,7 +70,7 @@ export default function useCastAttributes () {
         }
 
         if(_value && isString(_value)){
-          let remainingQuote = '\\w\\s' + __preg_quote('çşıİğüö.,;?|:_')
+          let remainingQuote = '\\w\\s' + __preg_quote('çşıİğüö.,;?|:_-=<>/"\'')
           let pattern = new RegExp( String.raw`^([${remainingQuote}]+)?(${quoted})([${remainingQuote}]+)?$`)
 
           if(value.match(pattern)){
@@ -103,6 +87,81 @@ export default function useCastAttributes () {
           returnValue = _value
         }
       }
+    }
+
+    return returnValue
+  }
+
+  const castEvalAttribute = (value, ownerItem) => {
+
+    let returnValue = value
+
+    if(matchEvalAttribute(value)){
+      let matches = value.match(EvalPattern)
+      let evalText = matches[1]
+
+      let evalParts = evalText.split(' ').map((v) => {
+        return castAttribute(v, ownerItem)
+      })
+
+      try {
+        returnValue = eval(evalParts.join(' '))
+      } catch (e) {
+        console.error('Error in eval', e)
+      }
+    }
+
+    return returnValue
+
+  }
+
+  const castAttribute = (value, ownerItem) => {
+    if(matchAttribute(value)){
+      return replacePattern(value, ownerItem)
+    } else if(matchEvalAttribute(value)){
+      return castEvalAttribute(value, ownerItem)
+    } else if(matchStandardAttribute(value)){
+      return castStandardAttribute(value, ownerItem)
+    }
+
+    return value
+  }
+
+  const castObjectAttribute = (value, ownerItem, options = {}) => {
+    if(!isString(value))
+      return value
+
+    let returnValue = value
+    let matches
+
+    if(matchAttribute(value)){
+      returnValue = replacePattern(value, ownerItem)
+    } else if(matchEvalAttribute(value)){
+      let matches = value.match(EvalPattern)
+      let evalText = matches[1]
+
+      let evalParts = evalText.split(' ').map((v) => {
+        if(AttributePattern.test(v)) {
+          let evalPartMatches = v.match(AttributePattern)
+          let evalPart = evalPartMatches[1]
+
+          let evalPartCastedValue = __data_get(ownerItem, evalPart, undefined)
+
+          if(evalPartCastedValue !== undefined) {
+            return evalPartCastedValue
+          }
+        }
+
+        return v
+      })
+
+      try {
+        return eval(evalParts.join(' '))
+      } catch (e) {
+        console.error('Error in eval', e)
+      }
+    } else if(matchStandardAttribute(value)){
+      return castStandardAttribute(value, ownerItem, options)
     }
 
     return returnValue
@@ -128,6 +187,12 @@ export default function useCastAttributes () {
   }
 
   return {
+    matchAttribute,
+    matchStandardAttribute,
+    matchEvalAttribute,
+    castAttribute,
+    castStandardAttribute,
+    castEvalAttribute,
     castObjectAttribute,
     castObjectAttributes
   }
