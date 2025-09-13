445c99f78b6dd377a75020f1afef39a5ae42e25e
OoBook <oguz.bukcuoglu@gmail.com>
2025-09-14 02:28:28 +0300

    fix(Payment): handle payment gateway retrieval in serviceClass method
    - Updated the serviceClass method to initialize the paymentGateway variable correctly, ensuring it retrieves the payment service key within the try block.
    - This change addresses potential issues with uninitialized variables and improves error handling for service class retrieval.

--- modules/SystemPayment/Entities/Payment.php
+++ modules/SystemPayment/Entities/Payment.php
@@ -98,8 +98,9 @@ class Payment extends \Unusualify\Payable\Models\Payment
     protected function serviceClass(): Attribute
     {
         $serviceClass = null;
-        $paymentGateway = $this->paymentService->key;
+        $paymentGateway = null;
         try {
+            $paymentGateway = $this->paymentService->key;
             $serviceClass = \Unusualify\Payable\Payable::getServiceClass($paymentGateway);
         } catch (\Exception $e) {
             if ($e->getMessage() == 'Service class not found for slug: ' . $paymentGateway && $this->paymentService->transferrable) {
