38d4bf0c9289b63ec89f08f6344083b83376c478
OoBook <oguz.bukcuoglu@gmail.com>
2025-10-01 13:35:50 +0300

    refactor(useCache): update store access and improve code clarity
    - Replaced the useStore hook with direct Store imports for accessing Vuex store.
    - Cleaned up getter and mutation calls to enhance readability and maintainability.
    - Updated computed property to reflect changes in store access.

--- vue/src/js/hooks/useCache.js
+++ vue/src/js/hooks/useCache.js
@@ -1,33 +1,31 @@
 // useCache.js
-import { useStore } from 'vuex'
+import { computed } from 'vue'
 import { CACHE } from '@/store/mutations'
-import store from '@/store'
+import Store from '@/store'
 
 export default function useCache() {
-  const store = useStore()
-
   const get = (key, defaultValue = null) => {
-    return store.getters[CACHE.GET_CACHE](key) ?? defaultValue
+    return Store.getters[CACHE.GET_CACHE](key) ?? defaultValue
   }
 
   const put = (key, value) => {
-    store.commit(CACHE.PUT_CACHE, {key, value})
+    Store.commit(CACHE.PUT_CACHE, {key, value})
   }
 
   const push = (key, value) => {
-    store.commit(CACHE.PUSH_CACHE, {key, value})
+    Store.commit(CACHE.PUSH_CACHE, {key, value})
   }
 
   const last = (key, defaultValue = null) => {
-    return store.getters[CACHE.GET_LAST_CACHE](key) ?? defaultValue
+    return Store.getters[CACHE.GET_LAST_CACHE](key) ?? defaultValue
   }
 
   const forget = (key) => {
-    store.commit(CACHE.FORGET_CACHE, key)
+    Store.commit(CACHE.FORGET_CACHE, key)
   }
 
   const has = (key) => {
-    return store.getters[CACHE.HAS_CACHE](key)
+    return Store.getters[CACHE.HAS_CACHE](key)
   }
 
   return {
@@ -37,6 +35,6 @@ export default function useCache() {
     last,
     forget,
 
-    states: computed(() => store.state.cache),
+    states: computed(() => Store.state.cache),
   }
 }
