71cdc0509fc1ac852b3b4a0ee09387a696c08d70
OoBook <oguz.bukcuoglu@gmail.com>
2025-10-06 04:48:43 +0300

    feat(PaymentRepository): integrate CreatorTrait for enhanced payment record management
    - Added CreatorTrait to PaymentRepository, enabling improved handling of creator-related functionalities within payment records.
    - This enhancement supports better data integrity and role-based access control for payment-related operations.

--- modules/SystemPayment/Repositories/PaymentRepository.php
+++ modules/SystemPayment/Repositories/PaymentRepository.php
@@ -4,12 +4,13 @@ namespace Modules\SystemPayment\Repositories;
 
 use Modules\SystemPayment\Entities\Payment;
 use Unusualify\Modularity\Repositories\Repository;
+use Unusualify\Modularity\Repositories\Traits\CreatorTrait;
 use Unusualify\Modularity\Repositories\Traits\FilepondsTrait;
 use Unusualify\Modularity\Repositories\Traits\SpreadableTrait;
 
 class PaymentRepository extends Repository
 {
-    use FilepondsTrait, SpreadableTrait;
+    use FilepondsTrait, SpreadableTrait, CreatorTrait;
 
     public function __construct(Payment $model)
     {
