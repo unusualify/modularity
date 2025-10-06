65ac20933aa080fd2e25771bc4b4f59f09958a48
OoBook <oguz.bukcuoglu@gmail.com>
2025-10-06 04:48:31 +0300

    feat(CreatorHydrate): implement CreatorHydrate class for custom input handling
    - Added CreatorHydrate class to manage input hydration for a custom creator field.
    - Defined default requirements and implemented logic to manipulate input schema structure, enhancing data handling for creator-related inputs.
    - Updated CreatorTrait with methods to manage form fields and prepend schema for creator functionality, improving role-based access control and data integrity.

--- src/Repositories/Traits/CreatorTrait.php
+++ src/Repositories/Traits/CreatorTrait.php
@@ -2,6 +2,8 @@
 
 namespace Unusualify\Modularity\Repositories\Traits;
 
+use Illuminate\Support\Facades\Auth;
+
 trait CreatorTrait
 {
     /**
@@ -13,5 +15,42 @@ trait CreatorTrait
     public function filterCreatorTrait($query, &$scopes)
     {
         $scopes['hasAccessToCreation'] = true;
+    }
+
+    /**
+     * @param \Unusualify\Modularity\Models\Model $object
+     * @param array $fields
+     * @param array $schema
+     * @return array
+     */
+    public function getFormFieldsCreatorTrait($object, $fields, $schema = [])
+    {
+        // set, cast, unset or manipulate the fields by using object, fields and schema
+        if(isset($schema['custom_creator_id'])){
+            $creatorInput = $schema['custom_creator_id'];
+            $isAllowed = true;
+
+            if(isset($creatorInput['allowedRoles'])){
+                $allowedRoles = $creatorInput['allowedRoles'];
+                if(!(Auth::check() && Auth::user()->hasRole($allowedRoles))){
+                    $isAllowed = false;
+                }
+            }
+
+            if($isAllowed && $object->creator()->exists()){
+                $fields['custom_creator_id'] = $object?->creator?->id;
+            }
+        }
+
+        return $fields;
+    }
+
+    public function prependFormSchemaCreatorTrait($scope = [])
+    {
+        return [
+            (object)[
+                'type' => 'creator',
+            ],
+        ];
     }
 }
