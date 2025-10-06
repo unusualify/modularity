aa16e991f53b36983c132f4d3b11141c5a483a9f
OoBook <oguz.bukcuoglu@gmail.com>
2025-10-07 00:30:04 +0300

    build(vue): update build artifacts for v0.48.0

---
+++ vue/dist/modularity/entries/core-auth.WO6KcUvC.js
@@ -0,0 +1,2 @@
+import{af as t}from"../js/vendor.CTlBbFOA.js";import{a as r,s as p}from"../js/UEConfig.B4qKqKZG.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.BJtVVQgM.js";const s=p,o=t({store:s});o.use(r);o.mount("#auth");
+//# sourceMappingURL=core-auth.WO6KcUvC.js.map
---
+++ vue/dist/modularity/entries/core-form.BJsJGy9V.js
@@ -0,0 +1,2 @@
+import{af as t}from"../js/vendor.CTlBbFOA.js";import{a as r,s as m}from"../js/UEConfig.B4qKqKZG.js";import"../js/awesome-phonenumber.CPpBdrPW.js";import"../js/fine-uploader.C8t-Jhyu.js";import"../js/vuetify.BJtVVQgM.js";const p=m,o=t({store:p});o.use(r);o.mount("#admin");
+//# sourceMappingURL=core-form.BJsJGy9V.js.map
