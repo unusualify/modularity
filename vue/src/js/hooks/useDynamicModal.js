53dfb2c398307640aed4907ca229b9360c45093e
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-06-27 20:36:05 +0300

    fix(useDynamicModal.js): inject modalService with default null to prevent errors when not installed

--- vue/src/js/hooks/useDynamicModal.js
+++ vue/src/js/hooks/useDynamicModal.js
@@ -3,7 +3,7 @@ import { inject } from 'vue'
  * Composition‐API hook
  */
 export default function useDynamicModal() {
-  const service = inject('modalService')
+  const service = inject('modalService', null)
 
   if (!service) {
     throw new Error('[ModalService] not installed. Did you forget `app.use(ModalService)`?')
