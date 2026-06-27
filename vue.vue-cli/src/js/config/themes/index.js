bbe154cf65c9b0f0d14dee3e8d8ba1e9ba520b41
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-06-27 23:00:42 +0300

    chore(cleanup): remove unused configuration files and dependencies from vue.vue.cli project

--- vue.vue-cli/src/js/config/themes/index.js
+++
@@ -1,3 +0,0 @@
-export { default as unusual } from './unusual'
-export { default as b2press } from './b2press'
-export { default as jakomeet } from './jakomeet'
--- vue.vue-cli/src/js/mixins/randKey.js
+++
@@ -1,7 +0,0 @@
-export default {
-  data: function () {
-    return {
-      randKey: Date.now() + Math.floor(Math.random() * 9999)
-    }
-  }
-}
