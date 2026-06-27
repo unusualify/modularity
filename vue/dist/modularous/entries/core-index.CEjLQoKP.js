e2e872e563ea963299a0fdf41abec392b8f60dad
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-06-28 00:21:14 +0300

    build(vue): update build artifacts for v12.2.0

---
+++ vue/dist/modularous/entries/core-index.CEjLQoKP.js
@@ -0,0 +1,2 @@
+import{bs as t}from"../js/vendor.D8DUI8cv.js";import{s as r,U as m}from"../js/UEConfig.Bzlqha1E.js";import"../js/fine-uploader.Dwa_ZJgp.js";import"../js/vuetify.S0i6YVIT.js";import"../js/tiptap.CqDFYeA0.js";import"../js/awesome-phonenumber.BNGr885O.js";const p=r,o=t({store:p});o.use(m);o.mount("#admin");
+//# sourceMappingURL=core-index.CEjLQoKP.js.map
