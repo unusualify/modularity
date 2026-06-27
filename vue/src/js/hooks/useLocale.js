fbf7d725c6976fc60764a283457dc8f9eb1fdcd7
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-06-27 20:36:22 +0300

    fix(useLocale.js): replace direct store import with useStore hook for improved reactivity

--- vue/src/js/hooks/useLocale.js
+++ vue/src/js/hooks/useLocale.js
@@ -4,12 +4,12 @@
  */
 import { computed, ref, } from 'vue'
 import { useStore } from 'vuex'
-import store from '@/store'
 import { LANGUAGE } from '@/store/mutations'
 
 const RTL_LOCALES = ['ar', 'arc', 'dv', 'fa', 'ha', 'he', 'khw', 'ks', 'ku', 'ps', 'ur', 'yi']
 
 export default function useLocale (props = {}) {
+  const store = useStore()
   const _locale = ref(props.locale ?? null)
 
   const currentLocale = computed(() => store.state.language?.active ?? null)
