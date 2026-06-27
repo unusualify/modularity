f28ee7965c6ada4a13d364d6ad4bcd28a9e3b89e
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-06-27 17:52:46 +0300

    feat(Traitify): enhance traitsMethods to support snake_case method names

--- src/Traits/Traitify.php
+++ src/Traits/Traitify.php
@@ -20,6 +20,10 @@ trait Traitify
         $uniqueTraits = array_unique(array_map('class_basename', $traits));
 
         $methods = array_map(function (string $trait) use ($method) {
+            // if $methodName is snake_case, combining it and make it snake_case
+            if(str_contains($method, '_')) {
+                return $method . '_' . snakeCase($trait);
+            } 
             return $method . $trait;
         }, $uniqueTraits);
 
