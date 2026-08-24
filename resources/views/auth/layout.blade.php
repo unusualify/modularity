b7ec545ed72fb9def1cb5b6844ff73e5696bf3da
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-08-24 11:34:06 +0300

    refactor(AuthLayout): simplify form structure and enhance styling consistency
    - Updated the layout of the authentication form by removing unnecessary template slots and comments.
    - Introduced a ':border="false"' prop to the <ue-form> for consistent styling.
    - Streamlined the form's structure to improve readability and maintainability.

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -25,12 +25,7 @@
         >
             @section('content')
                 @if(!isset($taskState))
-                    <ue-form v-bind='@json($formAttributes)'>
-                        {{-- <template v-slot:submit="submitScope">
-                            <v-btn block dense type="submit" :disabled="!submitScope.validForm" :loading="submitScope.loading">
-                                @{{ submitScope.buttonDefaultText.toUpperCase() }}
-                            </v-btn>
-                        </template> --}}
+                    <ue-form :border="false" v-bind='@json($formAttributes)'>
                         @foreach( ($formSlots ?? []) as $slotName => $configuration)
                             <template v-slot:{{ $slotName }}="slotScope">
                                 <ue-recursive-stuff
@@ -52,11 +47,6 @@
                             :bindData='slotScope'
                         />
                     </template>
-                    {{-- <template v-slot:bottom1 >
-                        <ue-recursive-stuff
-                            :configuration='@json($configuration)'
-                        />
-                    </template> --}}
                 @endforeach
             @stop
             @yield('content')
