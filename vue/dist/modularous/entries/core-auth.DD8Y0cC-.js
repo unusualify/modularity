8bf5ebfe0e7c87bdf01e72494926ce849f052452
Oğuzhan Bükçüoğlu <oguz.bukcuoglu@gmail.com>
2026-08-31 03:03:17 +0300

    build(vue): update build artifacts for v12.9.0

---
+++ vue/dist/modularous/entries/core-auth.DD8Y0cC-.js
@@ -0,0 +1,2 @@
+import{bs as t}from"../js/vendor.B9yN4JkX.js";import{U as r,s as p}from"../js/UEConfig.BL-P4w6u.js";import"../js/fine-uploader.A-czSBGM.js";import"../js/vuetify.BJzLMZL-.js";import"../js/tiptap.NSyloqdq.js";import"../js/awesome-phonenumber.CPySbNGi.js";const s=p,o=t({store:s});o.use(r);o.mount("#auth");
+//# sourceMappingURL=core-auth.DD8Y0cC-.js.map
---
+++ vue/dist/modularous/entries/core-dashboard.Bc_redxP.js
@@ -0,0 +1,2 @@
+import{bs as t}from"../js/vendor.B9yN4JkX.js";import{s as r,U as m}from"../js/UEConfig.BL-P4w6u.js";import"../js/fine-uploader.A-czSBGM.js";import"../js/vuetify.BJzLMZL-.js";import"../js/tiptap.NSyloqdq.js";import"../js/awesome-phonenumber.CPySbNGi.js";const p=r,o=t({store:p});o.use(m);o.mount("#admin");
+//# sourceMappingURL=core-dashboard.Bc_redxP.js.map
